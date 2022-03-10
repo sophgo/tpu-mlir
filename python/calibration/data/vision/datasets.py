233fdc17e7fcb9656b69e7bd1cbf48794e37f72b
man.lu <man.lu@sophgo.com>
2022-03-10 09:53:54 +0000

    add image pre-processing
    - add pytest
    - add image loader
    
    Change-Id: I9934b691526b3b4ea1482627d910ddd6491bbad8

---
+++ python/calibration/data/vision/datasets.py
@@ -0,0 +1,97 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+# coding: utf-8
+# pylint: disable=
+"""Dataset container."""
+__all__ = [
+    "ImageFolderDataset",
+]
+
+import os
+import warnings
+import cv2
+
+
+class ImageFolderDataset:
+    """A dataset for loading image files stored in a folder structure.
+
+    like::
+
+        root/car/0001.jpg
+        root/car/xxxa.jpg
+        root/car/yyyb.jpg
+        root/bus/123.jpg
+        root/bus/023.jpg
+        root/bus/wwww.jpg
+
+    Parameters
+    ----------
+    root : str
+        Path to root directory.
+    flag : {0, 1}, default 1
+        If 0, always convert loaded images to greyscale (1 channel).
+        If 1, always convert loaded images to colored (3 channels).
+    transform : callable, default None
+        A function that takes data and label and transforms them::
+
+            transform = lambda data, label: (data.astype(np.float32)/255, label)
+
+    Attributes
+    ----------
+    synsets : list
+        List of class names. `synsets[i]` is the name for the integer label `i`
+    items : list of tuples
+        List of all images in (filename, label) pairs.
+    """
+
+    def __init__(self, root, flag=1, transform=None):
+        self._root = os.path.expanduser(root)
+        self._flag = flag
+        self._transform = transform
+        self._exts = [".jpg", ".jpeg", ".png"]
+        self._list_images(self._root)
+
+    def _list_images(self, root):
+        self.items = []
+
+        for folder in sorted(os.listdir(root)):
+            path = os.path.join(root, folder)
+            if not os.path.isdir(path):
+                warnings.warn(
+                    "Ignoring %s, which is not a directory." % path, stacklevel=3
+                )
+                continue
+            for filename in sorted(os.listdir(path)):
+                filename = os.path.join(path, filename)
+                ext = os.path.splitext(filename)[1]
+                if ext.lower() not in self._exts:
+                    warnings.warn(
+                        "Ignoring %s of type %s. Only support %s"
+                        % (filename, ext, ", ".join(self._exts))
+                    )
+                    continue
+                self.items.append(filename)
+
+    def __getitem__(self, idx):
+        img = cv2.imread(self.items[idx], self._flag)
+        if self._transform is not None:
+            return self._transform(img)
+        return img
+
+    def __len__(self):
+        return len(self.items)
