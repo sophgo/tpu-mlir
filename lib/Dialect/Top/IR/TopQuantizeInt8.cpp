8153ca66c16e1e538945ea57e9920e7793e99e16
pengchao.hu <pengchao.hu@sophgo.com>
2022-03-17 11:26:58 +0800

    refine weight op create
    Change-Id: I6b320e346ad8513d2f03e921e690a13317c91a21

--- lib/Dialect/Top/IR/TopQuantizeInt8.cpp
+++ lib/Dialect/Top/IR/TopQuantizeInt8.cpp
@@ -53,6 +53,8 @@ const double qmax8bit = 127.0;
 Value top::ConvOp::quantize_int8() {
   auto op = getOperation();
   OpBuilder builder(op);
+  std::vector<Value> operands;
+  operands.push_back(input());
   std::vector<NamedAttribute> attrs;
   int64_t n, ic, ih, iw, oc, oh, ow, g, kh, kw, ins_h, ins_w, sh, sw, pt, pb,
       pl, pr, dh, dw;
@@ -66,6 +68,7 @@ Value top::ConvOp::quantize_int8() {
   auto filter_max = findMaxabs(filter_fp32->data(), filter_fp32->size());
   int rshift = calRightShiftNum(filter_max, th_input, th_output, 8);
   rshift = std::max(rshift, 0);
+  Value new_bias = bias();
   if (with_bias) {
     auto biasOp = cast<top::WeightOp>(bias().getDefiningOp());
     auto bias_fp32 = biasOp.read<float>();
@@ -88,9 +91,8 @@ Value top::ConvOp::quantize_int8() {
     auto bias_type = bias().getType().cast<RankedTensorType>();
     auto new_type =
         RankedTensorType::get(bias_type.getShape(), builder.getIntegerType(16));
-    biasOp.write("int16", *bias_int16, new_type);
-    // ufw_net.layers()[layer_id]->blobs()[1]->setFixToFloatScale(1.0f /
-    // bias_scale);
+    new_bias = WeightOp::create(op, "bias_int16", *bias_int16,
+                                new_type);
   }
   attrs.push_back(
       builder.getNamedAttr("rshift", builder.getI64IntegerAttr(rshift)));
@@ -101,13 +103,10 @@ Value top::ConvOp::quantize_int8() {
   auto filter_type = filter().getType().cast<RankedTensorType>();
   auto new_type =
       RankedTensorType::get(filter_type.getShape(), builder.getI8Type());
-  filterOp.write("int8", *filter_int8, new_type);
-
-  std::vector<Value> operands;
-  const int nInputs = op->getNumOperands();
-  for (auto i = 0; i < nInputs; ++i) {
-    operands.push_back(op->getOperand(i));
-  }
+  auto new_filter =
+      WeightOp::create(op, "filter_int8", *filter_int8, new_type);
+  operands.push_back(new_filter);
+  operands.push_back(new_bias);
   for (auto &attr : op->getAttrs()) {
     attrs.push_back(attr);
   }
