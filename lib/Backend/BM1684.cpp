cb469db8e14c8b7f4f4a705624424f733d824c20
pengchao.hu <pengchao.hu@sophgo.com>
2022-03-23 11:30:31 +0800

    add bm1684 codegen pass
    Change-Id: I07af0388f28c9bbc98eab3121c48d5ee55239e2a

---
+++ lib/Backend/BM1684.cpp
@@ -0,0 +1,219 @@
+#include "sophgo/Backend/BM1684.h"
+#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/Format.h"
+
+using namespace sophgo::backend;
+
+template <typename FPtrTy>
+FPtrTy BM1684::CastToFPtr(const char *symbolName) {
+  assert(DL.isValid());
+  auto fPtr = DL.getAddressOfSymbol(symbolName);
+  if (fPtr == nullptr) {
+    llvm::errs() << "can't find symbol: " << symbolName;
+    llvm_unreachable(symbolName);
+  }
+  return reinterpret_cast<FPtrTy>(fPtr);
+}
+
+#define CAST_FUNCTION(name) dl_##name = CastToFPtr<name>(#name)
+
+BM1684::BM1684() {
+  std::string Err;
+  DL = llvm::sys::DynamicLibrary::getPermanentLibrary("libbackend_1684.so", &Err);
+  if (DL.isValid() == false) {
+    llvm_unreachable(Err.c_str());
+  }
+  CAST_FUNCTION(cmodel_init);
+  CAST_FUNCTION(cmodel_deinit);
+  CAST_FUNCTION(create_cmd_id_node);
+  CAST_FUNCTION(destroy_cmd_id_node);
+  CAST_FUNCTION(set_cmd_id_cycle);
+  CAST_FUNCTION(get_cmd_id_cycle);
+  CAST_FUNCTION(reset_cmd_id);
+  CAST_FUNCTION(allow_store_cmd);
+  CAST_FUNCTION(forbid_store_cmd);
+  CAST_FUNCTION(use_atomic_cmodel);
+  CAST_FUNCTION(forbid_atomic_cmodel);
+  CAST_FUNCTION(tensor_align_move_gen_cmd);
+  CAST_FUNCTION(general_matrix_move_gen_cmd);
+  CAST_FUNCTION(nodechip_conv_forward_local);
+  CAST_FUNCTION(nodechip_winograd_forward_local);
+  CAST_FUNCTION(nodechip_pooling_fix8b_forward_local);
+  CAST_FUNCTION(nodechip_conv_forward_local_fix8b);
+  CAST_FUNCTION(nodechip_conv_forward_local_fix16b);
+  CAST_FUNCTION(nodechip_winograd_forward_local_fix8b);
+  CAST_FUNCTION(nodechip_winograd_double_buffer_forward_local_fix8b);
+  CAST_FUNCTION(nodechip_deconv_forward_local);
+  CAST_FUNCTION(nodechip_deconv_fix16b_forward_local);
+  CAST_FUNCTION(nodechip_deconv_fix8b_forward_local);
+  CAST_FUNCTION(nodechip_pooling_forward_local);
+  CAST_FUNCTION(nodechip_upsample_forward_local);
+  CAST_FUNCTION(nodechip_upsample_fix8b_forward_local);
+  CAST_FUNCTION(nodechip_lrn_forward_local);
+  CAST_FUNCTION(nodechip_lrn_fix8b_forward_local);
+  CAST_FUNCTION(nodechip_batchnorm_layer_local);
+  CAST_FUNCTION(nodechip_bnscale_fix8b_forward_local);
+  CAST_FUNCTION(nodechip_scale_forward_local);
+  CAST_FUNCTION(nodechip_eltwise_forward_local);
+  CAST_FUNCTION(nodechip_eltwise_fix8b_forward_local);
+  CAST_FUNCTION(nodechip_fc_forward_local);
+  CAST_FUNCTION(nodechip_prelu_forward_local_v2);
+  CAST_FUNCTION(nodechip_relu_forward_local);
+  CAST_FUNCTION(nodechip_prelu_forward_local_fix8b_v3);
+  CAST_FUNCTION(nodechip_relu_forward_local_fix16b);
+  CAST_FUNCTION(nodechip_reorg_forward_local);
+  CAST_FUNCTION(nodechip_reorg_forward_fix8b_local);
+  CAST_FUNCTION(nodechip_permute_forward_local);
+  CAST_FUNCTION(nodechip_permute_fix8b_forward_local);
+  CAST_FUNCTION(nodechip_normalize_forward_local);
+  CAST_FUNCTION(nodechip_normalize_fix8b_forward_local);
+  CAST_FUNCTION(nodechip_active_forward_local);
+  CAST_FUNCTION(nodechip_mulshift_fix8b_forward_local);
+  CAST_FUNCTION(nodechip_concat_md);
+  CAST_FUNCTION(nodechip_concat_md_fix8b);
+  CAST_FUNCTION(nodechip_stride_slice_forward_local);
+  CAST_FUNCTION(nodechip_stride_slice_forward_local_fix8b);
+  CAST_FUNCTION(nodechip_pooling3d_local);
+  CAST_FUNCTION(nodechip_conv3d_local);
+  CAST_FUNCTION(nodechip_conv3d_fix8b_local);
+  CAST_FUNCTION(nodechip_deconv3d_local);
+  CAST_FUNCTION(nodechip_fc_forward_parallel);
+  CAST_FUNCTION(nodechip_fc_fix8b_forward_parallel);
+  CAST_FUNCTION(nodechip_pooling_forward_parallel_with_data_split);
+  CAST_FUNCTION(nodechip_pooling_fix8b_forward_parallel_with_data_split);
+  CAST_FUNCTION(nodechip_pooling_tf_fix8b_forward_parallel_with_data_split);
+  CAST_FUNCTION(nodechip_sort_per_dim);
+  CAST_FUNCTION(nodechip_sort_per_dim_fix8b);
+  CAST_FUNCTION(nodechip_index_select);
+  CAST_FUNCTION(nodechip_index_select_fix8b);
+  CAST_FUNCTION(nodechip_psroipooling_forward_with_datasplit);
+  CAST_FUNCTION(nodechip_psroipooling_fix8b_forward_with_datasplit);
+  CAST_FUNCTION(nodechip_roi_pooling_forward);
+  CAST_FUNCTION(nodechip_roi_pooling_forward_fix8b);
+  CAST_FUNCTION(nodechip_crop);
+  CAST_FUNCTION(nodechip_crop_fix8b);
+  CAST_FUNCTION(nodechip_upsample_forward_parallel_with_data_split);
+  CAST_FUNCTION(nodechip_upsample_forward_parallel_fix8b);
+  CAST_FUNCTION(nodechip_upsample_mask_forward);
+  CAST_FUNCTION(nodechip_multiregion_forward_parallel);
+  CAST_FUNCTION(nodechip_deconv_forward_parallel_with_data_split);
+  CAST_FUNCTION(nodechip_deconv_fix16b_forward_parallel);
+  CAST_FUNCTION(nodechip_deconv_fix8b_forward_parallel);
+  CAST_FUNCTION(nodechip_depthwise_forward_parallel_with_dilation);
+  CAST_FUNCTION(nodechip_conv_forward_parallel_with_data_split);
+  CAST_FUNCTION(nodechip_winograd_forward_parallel_with_data_split);
+  CAST_FUNCTION(nodechip_winograd_forward_parallel_fix8b_with_data_split);
+  CAST_FUNCTION(nodechip_lstm_forward_parallel);
+  CAST_FUNCTION(nodechip_scale_forward);
+  CAST_FUNCTION(nodechip_eltwise_forward);
+  CAST_FUNCTION(nodechip_eltwise_fix8b_forward_parallel);
+  CAST_FUNCTION(nodechip_prelu_forward);
+  CAST_FUNCTION(nodechip_prelu_forward_fix8b);
+  CAST_FUNCTION(nodechip_relu_forward_fix16b);
+  CAST_FUNCTION(nodechip_permute_forward);
+  CAST_FUNCTION(nodechip_permute_fix8b_forward);
+  CAST_FUNCTION(nodechip_normalize_forward);
+  CAST_FUNCTION(nodechip_normalize_fix8b_forward);
+  CAST_FUNCTION(nodechip_slice_forward);
+  CAST_FUNCTION(nodechip_softmax_forward_parallel);
+  CAST_FUNCTION(nodechip_active_forward_parallel);
+  CAST_FUNCTION(nodechip_active_forward_parallel_fix8b);
+  CAST_FUNCTION(nodechip_relu_forward_32bit_parallel);
+  CAST_FUNCTION(nodechip_batchnorm_forward_inference_parallel);
+  CAST_FUNCTION(nodechip_batchnorm_forward_inference_parallel_v2);
+  CAST_FUNCTION(nodechip_bnscale_forward_parallel_fix8b_with_src_storage_mode);
+  CAST_FUNCTION(nodechip_lrn_forward_parallel);
+  CAST_FUNCTION(nodechip_lrn_fix8b_forward_parallel);
+  CAST_FUNCTION(nodechip_depthwise_fix8b_forward_parallel);
+  CAST_FUNCTION(nodechip_conv_forward_parallel_fix8b_with_data_split);
+  CAST_FUNCTION(nodechip_conv_forward_parallel_fix16b_with_data_split);
+  CAST_FUNCTION(nodechip_mulshift_fix8b_forward);
+  CAST_FUNCTION(nodechip_global_conv_data_split_fix8b);
+  CAST_FUNCTION(nodechip_rpnproposal_forward);
+  CAST_FUNCTION(nodechip_shuffle_channel_forward);
+  CAST_FUNCTION(nodechip_shuffle_channel_fix8b_forward);
+  CAST_FUNCTION(nodechip_topk);
+  CAST_FUNCTION(nodechip_topk_fix8b);
+  CAST_FUNCTION(nodechip_lut_v2);
+  CAST_FUNCTION(nodechip_cumsum);
+  CAST_FUNCTION(nodechip_arg);
+  CAST_FUNCTION(nodechip_arg_local);
+  CAST_FUNCTION(nodechip_arg_fix8b);
+  CAST_FUNCTION(nodechip_stride_slice_md);
+  CAST_FUNCTION(nodechip_stride_slice_fix8b);
+  CAST_FUNCTION(nodechip_split_tf_md);
+  CAST_FUNCTION(nodechip_split_tf_fix8b_md);
+  CAST_FUNCTION(nodechip_interp_forward_parallel);
+  CAST_FUNCTION(nodechip_interp_forward_fix8b_parallel);
+  CAST_FUNCTION(nodechip_reverse_forward_v2);
+  CAST_FUNCTION(nodechip_reorg_forward_v2);
+  CAST_FUNCTION(nodechip_reorg_forward_fix8b);
+  CAST_FUNCTION(nodechip_adaptive_pooling_forward);
+  CAST_FUNCTION(nodechip_yolo);
+  CAST_FUNCTION(nodechip_memset);
+  CAST_FUNCTION(nodechip_channel_shift_forward);
+  CAST_FUNCTION(nodechip_channel_shift_forward_fix8b);
+  CAST_FUNCTION(nodechip_interleave);
+  CAST_FUNCTION(nodechip_interleave_fix8b);
+  CAST_FUNCTION(nodechip_interleave_local);
+  CAST_FUNCTION(nodechip_interleave_fixpoint_local);
+  CAST_FUNCTION(nodechip_stride_calculate_forward);
+  CAST_FUNCTION(nodechip_stridecalc_forward_global);
+  CAST_FUNCTION(nodechip_stride_calculate_forward_fix8b);
+  CAST_FUNCTION(nodechip_stridecalc_forward_local);
+  CAST_FUNCTION(nodechip_stridecalc_fix8b_forward_global);
+  CAST_FUNCTION(nodechip_stridecalc_fix8b_forward_local);
+  CAST_FUNCTION(nodechip_bitwise_forward_global);
+  CAST_FUNCTION(nodechip_bitwise_forward_local);
+  CAST_FUNCTION(nodechip_arith_shift_forward);
+  CAST_FUNCTION(nodechip_arith_shift_forward_local_v2);
+  CAST_FUNCTION(nodechip_reshape_fix8b);
+  CAST_FUNCTION(nodechip_pooling3d_forward_parallel);
+  CAST_FUNCTION(nodechip_pooling3d_fix8b_forward_parallel);
+  CAST_FUNCTION(nodechip_max_pooling_with_mask_forward_v2);
+  CAST_FUNCTION(nodechip_conv3d_parallel);
+  CAST_FUNCTION(nodechip_deconv3d);
+  CAST_FUNCTION(nodechip_conv3d_fix8b_parallel);
+  CAST_FUNCTION(nodechip_conv3d_add_parallel);
+  CAST_FUNCTION(nodechip_unfold);
+  CAST_FUNCTION(nodechip_gru);
+  CAST_FUNCTION(nodechip_pytorch_lstm);
+  CAST_FUNCTION(nodechip_matrix_band_part);
+  CAST_FUNCTION(nodechip_global_memcpy_ex);
+  CAST_FUNCTION(nodechip_lut_local_v2);
+  CAST_FUNCTION(nodechip_eltwise_binary_ex_forward_local);
+  CAST_FUNCTION(nodechip_eltwise_binary_ex_forward);
+  CAST_FUNCTION(nodechip_serial_number_gen);
+  CAST_FUNCTION(nodechip_pad_fix8b);
+  CAST_FUNCTION(nodechip_pad);
+  CAST_FUNCTION(nodechip_pad_local);
+  CAST_FUNCTION(nodechip_pad_fix8b_local);
+  CAST_FUNCTION(nodechip_concat_local_v2);
+  CAST_FUNCTION(nodechip_concat_fix8b_local_v2);
+
+  // setup
+  dl_cmodel_init(0, CMODEL_GLOBAL_MEM_SIZE);
+  cmdid_node = dl_create_cmd_id_node();
+  dl_reset_cmd_id(cmdid_node);
+  set_command_issue_flag(true);
+}
+
+BM1684::~BM1684() {
+  dl_destroy_cmd_id_node(cmdid_node);
+  dl_cmodel_deinit(0);
+}
+
+void BM1684::reset_cmd_id_node() {
+  dl_reset_cmd_id(cmdid_node);
+}
+
+void BM1684::set_command_issue_flag(bool value) {
+  really_issue_command = value;
+  if(really_issue_command) {
+    dl_allow_store_cmd();
+    dl_use_atomic_cmodel();
+  } else {
+    dl_forbid_store_cmd();
+    dl_forbid_atomic_cmodel();
+  }
+}
---
+++ lib/Dialect/Tpu/IR/TpuCodegenInt8BM1684.cpp
@@ -0,0 +1,172 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+// Also available under a BSD-style license. See LICENSE.
+//
+//===----------------------------------------------------------------------===//
+
+#include "sophgo/Dialect/Top/IR/TopOps.h"
+#include "sophgo/Dialect/Tpu/IR/TpuOps.h"
+#include "sophgo/Backend/BM1684.h"
+#include "sophgo/Interfaces/CodegenInterface.h"
+#include "sophgo/Support/MathUtils.h"
+#include "sophgo/Support/Helper/Quant.h"
+#include "sophgo/Support/Helper/Module.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinOps.h"
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/IR/TypeUtilities.h"
+#include "mlir/Support/LLVM.h"
+#include "llvm/ADT/StringMap.h"
+#include "llvm/Support/Casting.h"
+
+using namespace mlir;
+using namespace sophgo;
+using namespace sophgo::helper;
+using namespace sophgo::backend;
+
+#define ALIGN(x, a) ((((x) + (a)-1) / (a)) * (a))
+
+typedef enum {
+  STORE_MODE_1N = 0,
+  STORE_MODE_2N = 1,
+  STORE_MODE_4N = 2,
+} STORE_MODE_T;
+
+void tpu::ConvOp::codegen_int8_bm1684() {
+  int64_t n, ic, ih, iw, oc, oh, ow, g, kh, kw, ins_h, ins_w, sh, sw, pt, pb,
+      pl, pr, dh, dw;
+  bool is_dw, with_bias, relu;
+  parseParam(n, ic, ih, iw, oc, oh, ow, g, kh, kw, ins_h, ins_w, sh, sw, pt, pb,
+             pl, pr, dh, dw, is_dw, with_bias, relu);
+  if (is_dw) {
+    BM1684::instance().dl_nodechip_depthwise_fix8b_forward_parallel(
+        Module::getAddress(input()), Module::getAddress(output()),
+        Module::getAddress(filter()),
+        with_bias ? Module::getAddress(bias()) : 0, n, ic, ih, iw, kh, kw, pt,
+        pb, pl, pr, sh, sw, ins_h, ins_w, rshift(), with_bias ? 1 : 0, 0, 1, 1,
+        1, 1, relu ? 1 : 0, BM1684::instance().get_cmd_id_node());
+  } else {
+    auto weight_addr = Module::getAddress(filter());
+    auto bias_offset = ALIGN(ic / g, 4) * kh * kw;
+    BM1684::instance().dl_nodechip_conv_forward_parallel_fix8b_with_data_split(
+        Module::getAddress(input()), Module::getAddress(output()),
+        Module::getAddress(filter()),
+        with_bias ? Module::getAddress(bias()) : 0, n, ic, ih, iw, g, oc, kh,
+        kw, dh, dw, pt, pb, pl, pr, sh, sw, with_bias ? 1 : 0, 0, relu ? 1 : 0,
+        0, 1, 0, 0, rshift(), 1, 1, 1, 3, 0, 0, 0, 0, 0,
+        BM1684::instance().get_cmd_id_node());
+  }
+}
+
+typedef enum {
+  FcPerLayerShift = 0,
+  FcPerLayerScale = 1,
+  FcPerChannelScale = 2,
+} FcQScale;
+
+typedef struct {
+  float perlayer_scale;
+  int if_asymmetic;
+  int weight_offset;
+  int output_offset;
+  int if_bias_float;
+} FcQParams;
+
+void tpu::MatMulOp::codegen_int8_bm1684() {
+  int64_t batch, M, K, N;
+  bool with_bias;
+  parseParam(batch, M, K, N, with_bias);
+  int res_16b = 0;
+  int out_sign = 1;
+  int opd0_sign = 1;
+  int opd1_sign = 1;
+  int using_bias = with_bias ? 1 : 0;
+  int bias_sign = 1;
+  int if_relu = do_relu();
+  FcQParams quant_param{0, 0, 0, 0, 0};
+  BM1684::instance().dl_nodechip_fc_fix8b_forward_parallel(
+      Module::getAddress(input()), Module::getAddress(right()),
+      with_bias ? Module::getAddress(bias()) : 0, Module::getAddress(output()),
+      0, M, K, N, 0, using_bias, 1, 1, 1, rshift(), 0, do_relu() ? 1 : 0, 1,
+      isa<top::WeightOp>(right().getDefiningOp()) ? 0 : 1, 1, 0, FcPerLayerShift, &quant_param,
+      BM1684::instance().get_cmd_id_node());
+}
+
+void tpu::AvgPoolOp::codegen_int8_bm1684() {
+  int64_t n, c, ih, iw, oh, ow, kh, kw, sh, sw, pt, pb, pl, pr, pad_value;
+  bool is_global, count_include_pad;
+  parseParam(n, c, ih, iw, oh, ow, kh, kw, sh, sw, pt, pb, pl, pr, pad_value,
+             is_global, count_include_pad);
+  BM1684::instance().dl_nodechip_pooling_fix8b_forward_parallel_with_data_split(
+      Module::getAddress(input()), Module::getAddress(output()), n, c, ih, iw,
+      kh, kw, pt, pb, pl, pr, sh, sw, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 1,
+      do_relu() ? 1 : 0, BM1684::instance().get_cmd_id_node());
+}
+
+void tpu::MaxPoolOp::codegen_int8_bm1684() {
+  int64_t n, c, ih, iw, oh, ow, kh, kw, sh, sw, pt, pb, pl, pr, pad_value;
+  bool is_global, count_include_pad;
+  parseParam(n, c, ih, iw, oh, ow, kh, kw, sh, sw, pt, pb, pl, pr, pad_value,
+             is_global, count_include_pad);
+  BM1684::instance().dl_nodechip_pooling_fix8b_forward_parallel_with_data_split(
+      Module::getAddress(input()), Module::getAddress(output()), n, c, ih, iw,
+      kh, kw, pt, pb, pl, pr, sh, sw, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1,
+      do_relu() ? 1 : 0, BM1684::instance().get_cmd_id_node());
+}
+
+void tpu::ReshapeOp::codegen_int8_bm1684() {
+  auto in_addr = Module::getAddress(input());
+  auto out_addr = Module::getAddress(output());
+  if (in_addr == out_addr) {
+    return;
+  }
+  int64_t in, ic, ih, iw, on, oc, oh, ow;
+  Module::getNCHW(input(), in, ic, ih, iw);
+  Module::getNCHW(output(), on, oc, oh, ow);
+  BM1684::instance().dl_nodechip_reshape_fix8b(
+      in_addr, out_addr, in, ic, ih, iw, on, oc, oh, ow, STORE_MODE_4N,
+      STORE_MODE_4N, BM1684::instance().get_cmd_id_node());
+}
+
+void tpu::AddOp::codegen_int8_bm1684() {
+  int input_num = inputs().size();
+  assert(input_num == 2);
+  int64_t n, c, h, w;
+  Module::getNCHW(output(), n, c, h, w);
+  int op_code = 1; // (0: Product; 1: Sum; 2: Max)
+
+  std::vector<float> coeff_v;
+  if (coeff().hasValue()) {
+    for (auto data : coeff().getValue()) {
+      coeff_v.push_back(data.cast<FloatAttr>().getValueAsDouble());
+    }
+  } else {
+    coeff_v = {0, 0};
+  }
+  std::vector<int> rshift_v;
+  for (auto data : rshifts()) {
+    rshift_v.push_back(data.cast<IntegerAttr>().getInt());
+  }
+
+  BM1684::instance().dl_nodechip_eltwise_fix8b_forward_parallel(
+      Module::getAddress(inputs()[0]),     // u64    bottom_A_global_addr,
+      Module::getAddress(inputs()[1]),     // u64    bottom_B_global_addr,
+      Module::getAddress(output()),        // u64    top_global_addr,
+      n,                                   // int    tensor_n,
+      c,                                   // int    tensor_c,
+      h,                                   // int    tensor_h,
+      w,                                   // int    tensor_w,
+      op_code,                             // int    op_code,
+      coeff_v[0],                          // int    scale_A,
+      coeff_v[1],                          // int    scale_B,
+      1,                                   // int    sign_A,
+      1,                                   // int    sign_B,
+      rshift_v[0],                         // int    rshift_A,
+      rshift_v[1],                         // int    rshift_B,
+      do_relu() ? 1 : 0,                   // int    do_relu(),
+      BM1684::instance().get_cmd_id_node() // CMD_ID_NODE *id_node
+  );
+}
