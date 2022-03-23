efec8bf920479d8849dba0e73d48f4f56ad347a9
man.lu <man.lu@sophgo.com>
2022-03-23 09:35:29 +0000

    add TFLite -> Top converter
    - add TFLiteReader(a graph iterator)
    - using quant.uniform type to represent tflite Quantization()
    - convert TensorFlow format nhwc -> nchw
    - add pad and softmax operations
    - merge pad to computation function
    - add resnet50_int8.tflite model and test
    
    Change-Id: I9825792af76919dc06a6c9161e8a8f2d63d9c5d5

---
+++ regression/run_tflite.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ex
+
+
+mkdir -p tmp
+pushd tmp
+model_transform.py \
+    --model_type tflite \
+    --model_name resnet50 \
+    --input_shapes [[4,3,224,224]] \
+    --model_def  ../resnet50_quant_int8.tflite \
+    --mlir resnet50.mlir
