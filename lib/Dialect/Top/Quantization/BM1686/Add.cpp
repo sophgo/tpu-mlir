091580c9944a3533c96caa2c71ec497bc51cc824
jason.wang <jason.wang@sophgo.com>
2022-05-07 13:54:07 +0800

    add only rightshift the add output and fp32 compute
    Change-Id: Ib4da701c49df06697fb93c7cdce1cce5996fe322

--- lib/Dialect/Top/Quantization/BM1686/Add.cpp
+++ lib/Dialect/Top/Quantization/BM1686/Add.cpp
@@ -14,18 +14,21 @@ Value top::AddOp::quantize_int8_bm1686() {
   const int nInputs = op->getNumOperands();
   std::vector<int64_t> rshift_v(nInputs);
   std::vector<int64_t> multiplier_v(nInputs);
-  std::vector<double> coeff_v(nInputs, 1.0);
+  std::vector<double> coeff_param_v(nInputs, 1.0);
+  std::vector<int64_t> coeff_v(nInputs, 1);
   auto th_output_min = Quant::getMin(output());
   auto th_output_max = Quant::getMax(output());
 
   if (coeff().hasValue()) {
     int idx = 0;
     for (auto v : coeff().getValue()) {
-      coeff_v[idx++] = v.cast<FloatAttr>().getValueAsDouble();
+      coeff_param_v[idx++] = v.cast<FloatAttr>().getValueAsDouble();
     }
   }
 
-  float bias = 0;
+  int alg_type = 0; //0:normal, 1:only rightshift the output //设为1略微提升精度，但会导致backend_api_eltwise_fixed_global assert失败, 因为对某分支的multiplier左移会导致coeff_v大于128
+  double bias = 0;
+  int max_shifti = -32;
   for (int i = 0; i < nInputs; i++) {
     auto input = op->getOperand(i);
     operands.push_back(input);
@@ -34,24 +37,38 @@ Value top::AddOp::quantize_int8_bm1686() {
     double scale = (th_input_max - th_input_min) / (127 - (-128));
     int64_t zeropoint = std::round(-th_input_min / scale) - 128;
     int scalei, shifti;
-    float alpha = (th_input_max - th_input_min) /
+    double alpha = (th_input_max - th_input_min) /
                             (th_output_max - th_output_min);
     bias += alpha*zeropoint;
-    get_scale_and_shift(coeff_v[i] * alpha,
-                        scalei, shifti, 8);
-    coeff_v[i] = (double)scalei;
-    rshift_v[i] = shifti; //
+    get_scale_and_shift(coeff_param_v[i] * alpha,
+                        scalei, shifti, 8); //改为32，cos反而略降
+    coeff_v[i] = scalei;
+    rshift_v[i] = shifti;
+    if (shifti > max_shifti)
+      max_shifti = shifti;
+    //printf("i:%d scalei:%d, shifti:%d\n", i, scalei, shifti);
   }
 
+  if (alg_type) {
+    for (int i = 0; i < nInputs; i++) {
+      coeff_v[i] = coeff_v[i]<<(max_shifti - rshift_v[i]); //这里左移会导致coeff_v大于128
+      //printf("i:%d, max_shifti:%d - rshift_v:%d\n", i, max_shifti, rshift_v[i]);
+      rshift_v[i] = 0;
+    }
+    rshift_v[0] = max_shifti;
+    bias = bias*std::pow(2, max_shifti);
+  }
+
+  //printf("quantize rectified_bias:%f\n", bias);
   std::vector<NamedAttribute> attrs;
   attrs.push_back(builder.getNamedAttr("name", nameAttr()));
   attrs.push_back(builder.getNamedAttr("do_relu", do_reluAttr()));
   attrs.push_back(
-      builder.getNamedAttr("coeff", builder.getF64ArrayAttr(coeff_v)));
+      builder.getNamedAttr("multipliers", builder.getI64ArrayAttr(coeff_v)));
   attrs.push_back(
       builder.getNamedAttr("rshifts", builder.getI64ArrayAttr(rshift_v)));
   attrs.push_back(
-      builder.getNamedAttr("rectified_bias", builder.getSI32IntegerAttr(std::round(bias))));
+      builder.getNamedAttr("rectified_bias", builder.getF64FloatAttr(bias)));
   auto newOp = builder.create<tpu::AddOp>(op->getLoc(), output().getType(),
                                           ArrayRef<Value>{operands},
                                           ArrayRef<NamedAttribute>{attrs});
