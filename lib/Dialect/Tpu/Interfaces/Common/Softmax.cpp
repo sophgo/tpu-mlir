30a6dc1375a3caaaeb26bf0eb0c9a7bccb367072
man.lu <man.lu@sophgo.com>
2022-05-17 04:12:38 +0000

    add resnet50 tflite tpu inference
    - fix bug in image::normalize (keep the Dtype unchanged)
    
    - tflite interpreter
      - The "experimental_preserve_all_tensors" should be set True if we want to
        inspect the intermediate tensors used during computation.
    
    - add TFLiteInterpreter pytest
    
    - merge conv input zero_point to bias
    
    - add layout/represent data to Interpreter
    
    - fix npy read bug: sophgo-mlir using row-major but some npy contains col-major
      array.
    
    - add softmax
    
    Change-Id: I49c3a42764a9230951f3e4c9866725fbce12637b

--- lib/Dialect/Tpu/Interfaces/Common/Softmax.cpp
+++ lib/Dialect/Tpu/Interfaces/Common/Softmax.cpp
@@ -3,40 +3,39 @@
 #include "sophgo/Support/Helper/Module.h"
 
 using namespace sophgo;
-using namespace sophgo::helper;
 using namespace mlir;
 
-LogicalResult tpu::SoftmaxOp::init(InferenceParameter &p) {
-  auto pooling = new Pooling();
-  auto dt = getDnnlType(input());
+LogicalResult tpu::SoftmaxOp::init(InferenceParameter &p) { return success(); }
 
-  int izp = 0;
-  auto dtype = input().getType().cast<RankedTensorType>().getElementType();
-  if (dtype.isa<quant::UniformQuantizedType>()) {
-    izp = dtype.cast<quant::UniformQuantizedType>().getZeroPoint();
-  }
-  p.handle = (void *)pooling;
-  return success();
-}
-
-void tpu::SoftmaxOp::deinit(InferenceParameter &p) {
-  if (p.handle != nullptr) {
-    auto pooling = (Pooling *)p.handle;
-    delete pooling;
-    p.handle = nullptr;
-  }
-  return;
-}
+void tpu::SoftmaxOp::deinit(InferenceParameter &p) {}
 
 LogicalResult tpu::SoftmaxOp::inference(InferenceParameter &p) {
-  if (p.handle == nullptr) {
-    return failure();
+  auto input_shape = Module::getShape(input());
+  size_t softmax_cont = input_shape.back();
+  size_t batch_cont = Module::getNumElements(input()) / softmax_cont;
+  std::vector<float> max_in_each_batch(batch_cont);
+  for (size_t i = 0; i < batch_cont; ++i) {
+    float max = p.inputs[0][i * softmax_cont];
+    for (size_t j = 0; j < softmax_cont; ++j)
+      if (p.inputs[0][i * softmax_cont + j] > max)
+        max = p.inputs[0][i * softmax_cont + j];
+    max_in_each_batch[i] = max;
   }
-  auto pooling = (Pooling *)p.handle;
-  pooling->run();
-  if (do_relu()) {
-    relu(p.outputs[0], p.outputs[0], Module::getNumElements(output()),
-         Module::getStorageType(output()));
+  std::memcpy(p.outputs[0], p.inputs[0],
+              Module::getNumElements(input()) * sizeof(float));
+  std::vector<float> exp_sum_in_each_batch(batch_cont, 0);
+  for (size_t i = 0; i < batch_cont; ++i) {
+    for (size_t j = 0; j < softmax_cont; ++j) {
+      auto v = p.outputs[0][i * softmax_cont + j];
+      v = std::exp(v - max_in_each_batch[i]);
+      p.outputs[0][i * softmax_cont + j] = v;
+      exp_sum_in_each_batch[i] += v;
+    }
+  }
+  for (size_t i = 0; i < batch_cont; ++i) {
+    for (size_t j = 0; j < softmax_cont; ++j) {
+      p.outputs[0][i * softmax_cont + j] /= exp_sum_in_each_batch[i];
+    }
   }
   return success();
 }
