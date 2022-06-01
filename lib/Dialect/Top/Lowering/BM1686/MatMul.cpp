a2c4ffa5f66a3cea698f53047a5e0a8d7d152bb2
jason.wang <jason.wang@sophgo.com>
2022-06-01 17:39:22 +0800

    fix fp16 infer error
    Change-Id: Ie3feea46951767b237623172b98f3f58893886e1

--- lib/Dialect/Top/Lowering/BM1686/MatMul.cpp
+++ lib/Dialect/Top/Lowering/BM1686/MatMul.cpp
@@ -12,7 +12,7 @@
 #include "sophgo/Dialect/Tpu/IR/TpuOps.h"
 #include "sophgo/Support/MathUtils.h"
 #include "sophgo/Support/Helper/Quant.h"
-#include "sophgo/Support/fp16_bf16.h"
+#include "sophgo/Support/fp16.h"
 
 using namespace mlir;
 using namespace sophgo;
@@ -125,12 +125,11 @@ Value top::MatMulOp::lowering_fp(llvm::StringRef mode) {
     auto filterOp = cast<top::WeightOp>(right().getDefiningOp());
     auto filter_f32 = filterOp.read<float>();
     auto filter_ui16 = std::make_shared<std::vector<uint16_t>>(filter_f32->size());
-    unsigned int* filter_uint32 = (unsigned int*)filter_f32->data();
     for (int i = 0; i < filter_f32->size(); i++) {
       if (mode == Quant::Type::F16) {
-        filter_ui16->data()[i] = float_to_fp16_uint16_nvidia(filter_f32->data()[i]);
+        filter_ui16->data()[i] = fp16_alt_from_fp32_value(filter_f32->data()[i]);
       } else {
-        filter_ui16->data()[i] = (uint16_t)((filter_uint32[i] & 0xFFFF0000)>>16);
+        filter_ui16->data()[i] = float_to_bf16_uint16_simple(filter_f32->data()[i]);
       }
     }
     auto filter_type = right().getType().cast<RankedTensorType>();
@@ -146,9 +145,12 @@ Value top::MatMulOp::lowering_fp(llvm::StringRef mode) {
       auto biasOp = cast<top::WeightOp>(bias().getDefiningOp());
       auto bias_fp32 = biasOp.read<float>();
       auto bias_ui16 = std::make_shared<std::vector<uint16_t>>(bias_fp32->size());
-      unsigned int* bias_uint32 = (unsigned int*)bias_fp32->data();
       for (int i = 0; i < bias_fp32->size(); i++) {
-        bias_ui16->data()[i] = (uint16_t)((bias_uint32[i] & 0xFFFF0000)>>16);
+        if (mode == Quant::Type::F16) {
+          bias_ui16->data()[i] = fp16_alt_from_fp32_value(bias_fp32->data()[i]);
+        } else {
+          bias_ui16->data()[i] = float_to_bf16_uint16_simple(bias_fp32->data()[i]);
+        }
       }
 
       auto bias_type = bias().getType().cast<RankedTensorType>();
