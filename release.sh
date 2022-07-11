9057f2a3f00eb2e47f2db9afb8937740da7ac518
man.lu <man.lu@sophgo.com>
2022-07-11 05:58:10 +0000

    add release script
    Change-Id: I8a752a824b246e49104b7f7bae3f3d3df9f1a975

---
+++ release.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+set -e
+
+source envsetup.sh
+rm -rf ${INSTALL_PATH}
+source build.sh RELEASE
+
+mlir_version="$(grep MLIR_VERSION ${BUILD_PATH}/CMakeCache.txt | cut -d "=" -f2)"
+release_archive="./tpu-mlir_${mlir_version}"
+
+rm -rf ${release_archive}*
+cp -rf ${INSTALL_PATH} ${release_archive}
+
+cp -rf ${PROJECT_ROOT}/regression ${release_archive}
+
+# build a envsetup.sh
+__envsetupfile=${release_archive}/envsetup.sh
+rm -f __envsetupfile
+
+echo "Create ${__envsetupfile}" 1>&2
+more > "${__envsetupfile}" <<'//MY_CODE_STREAM'
+#!/bin/bash
+# set environment variable
+TPUC_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+export PATH=${TPUC_ROOT}/bin:$PATH
+export PATH=${TPUC_ROOT}/python/tools:$PATH
+export PATH=${TPUC_ROOT}/python/utils:$PATH
+export LD_LIBRARY_PATH=$TPUC_ROOT/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=${TPUC_ROOT}/python:$PYTHONPATH
+export NNMODELS_PATH=${TPUC_ROOT}/../nnmodels
+export REGRESSION_PATH=${TPUC_ROOT}/regression
+//MY_CODE_STREAM
+
+# generate readme.md
+echo "Create readme.md" 1>&2
+more > "${release_archive}/readme.md" <<'//MY_CODE_STREAM'
+For test
+
+1. set environment
+
+``` bash
+source ./envsetup.sh
+```
+
+2. run regression
+
+``` bash
+cd regression
+ ./run.sh
+```
+
+After run regression test, all the bmodels will be in regression_out.
+
+//MY_CODE_STREAM
+
+
+
+
+tar -cvzf "tpu-mlir_${mlir_version}.tar.gz" ${release_archive}
+rm -rf ${release_archive}
