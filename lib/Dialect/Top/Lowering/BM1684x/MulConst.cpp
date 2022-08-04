d2bf77a118df57b28acb8aaec7bfabe816ccca0b
zhaoyang.wang <zhaoyang.wang@sophgo.com>
2022-08-04 15:47:27 +0800

    [MLIR] MulConst support asymmetric
    Change-Id: Id4e26b48886da05d29e53eb196d68bd4fc74fc77

--- lib/Dialect/Top/Lowering/BM1684x/MulConst.cpp
+++ lib/Dialect/Top/Lowering/BM1684x/MulConst.cpp
@@ -17,6 +17,45 @@ using namespace tpu_mlir;
 using namespace tpu_mlir::helper;
 using namespace mlir;
 
+static Value asym_transfer(Value in, Value out, double const_val) {
+  double in_scale, out_scale;
+  int64_t in_zp, out_zp;
+  Quant::getScaleAndZeroPoint(in, in_scale, in_zp, 1);
+  Quant::getScaleAndZeroPoint(out, out_scale, out_zp, 1);
+  if (in_scale == out_scale && in_zp == out_zp) {
+    return in;
+  }
+  auto in_shape = Module::getShape(in);
+  auto out_type = Quant::getQuantInt8Type(out, 1);
+  auto ele_type = out_type.cast<RankedTensorType>().getElementType();
+  auto new_type = RankedTensorType::get(in_shape, ele_type);
+
+  auto op = out.getDefiningOp();
+  OpBuilder builder(op);
+  auto in_name = Module::getName(in.getDefiningOp());
+  auto out_name = Module::getName(op);
+  auto new_name = in_name + "_to_" + out_name;
+  int multiplier, rshift;
+  get_scale_and_shift(in_scale / out_scale * const_val, multiplier, rshift, 8);
+
+  std::vector<NamedAttribute> attrs;
+  attrs.push_back(
+        builder.getNamedAttr("name", builder.getStringAttr(new_name)));
+  attrs.push_back(builder.getNamedAttr(
+        "multiplier", builder.getI64IntegerAttr(multiplier)));
+  attrs.push_back(
+        builder.getNamedAttr("rshift", builder.getI64IntegerAttr(rshift)));
+  attrs.push_back(
+        builder.getNamedAttr("quant_mode", builder.getI64IntegerAttr(2)));
+  auto in_type = in.getType().cast<RankedTensorType>();
+  auto none = Module::getNoneOp(op);
+  builder.setInsertionPointAfterValue(in);
+  auto rqOp = builder.create<tpu::RequantOp>(op->getLoc(), new_type,
+                                             ValueRange{in, none},
+                                             ArrayRef<NamedAttribute>{attrs});
+  return rqOp.output();
+}
+
 Value top::MulConstOp::lowering_int8_bm1684x(bool asymmetric) {
   auto op = getOperation();
   const int nInputs = op->getNumOperands();
@@ -48,7 +87,17 @@ Value top::MulConstOp::lowering_int8_bm1684x(bool asymmetric) {
         ArrayRef<NamedAttribute>{attrs});
     return newOp.output();
   } else {
-    llvm_unreachable("MulConstOp asymmetric use FP32");
+    OpBuilder builder(op);
+    auto new_input = asym_transfer(input(), output(), const_val().convertToDouble());
+    std::vector<NamedAttribute> attrs;
+    for (auto &attr : op->getAttrs()) {
+      attrs.push_back(attr);
+    }
+    auto newType = Quant::getQuantInt8Type(output(), asymmetric);
+    auto newOp = builder.create<tpu::MulConstOp>(
+        op->getLoc(), newType, ValueRange{new_input},
+        ArrayRef<NamedAttribute>{attrs});
+    return newOp.output();
   }
 }
 
