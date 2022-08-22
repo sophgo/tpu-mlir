465e92dee5a235d4789360624cefb57323ee8090
jone.zhang <jone.zhang@sophgo.com>
2022-08-22 15:35:26 +0800

    [tpu-mlir] refine Softmax quant lowering&inference
    Change-Id: I026de0a6822f54cbf300c21d962092543af1af5e

--- lib/Dialect/Top/Lowering/BM1684x/Softmax.cpp
+++ lib/Dialect/Top/Lowering/BM1684x/Softmax.cpp
@@ -38,13 +38,27 @@ Value top::SoftmaxOp::lowering_quant_bm1684x() {
   if (Quant::isUniformQuantized(input(), output()) == false) {
     llvm_unreachable("input output should be quantized");
   }
-  return lowering_common<tpu::SoftmaxOp>(getOperation(), output().getType());
-  // // use f32
-  // Builder builder(getContext());
-  // auto in_f32 = do_cast(input(), builder.getF32Type(), false);
-  // auto op = getOperation();
-  // op->setOperand(0, in_f32);
-  // auto type = output().getType();
-  // auto v =  lowering_common_float<tpu::SoftmaxOp>(op);
-  // return do_cast(v, type, true);
+  auto op = getOperation();
+  OpBuilder builder(getContext());
+  const int nInputs = op->getNumOperands();
+  int64_t zeropoint;
+  double i_scale;
+  Quant::getScaleAndZeroPoint(input(), i_scale, zeropoint, true);
+  SmallVector<double> table(256);
+  double beta_v = betaAttr().getValueAsDouble() ;
+  double scale = -i_scale * beta_v;
+  for (int i = 0; i < 256; ++i) {
+    table[i] = std::exp(scale * i);
+  }
+
+  builder.setInsertionPointAfter(op);
+  std::vector<NamedAttribute> attrs;
+  attrs.push_back(builder.getNamedAttr("name", nameAttr()));
+  attrs.push_back(builder.getNamedAttr("axis", axisAttr()));
+  attrs.push_back(builder.getNamedAttr(
+      "table", builder.getF64ArrayAttr(table)));
+  auto newOp = builder.create<tpu::SoftmaxOp>(output().getLoc(), output().getType(),
+                                              ValueRange{op->getOperand(0)},
+                                              ArrayRef<NamedAttribute>{attrs});
+  return newOp.output();
 }
