2856644ee1e02c1389f0c39791974fb586902c60
xueru.chen <xueru.chen@sophgo.com>
2022-08-25 12:20:25 +0800

    support Conv1d and Conv3d
    Change-Id: Ic65b034c14fd5aca9dec62a418aebde22348eb28

---
+++ lib/Dialect/Tpu/Interfaces/BM1684x/Conv3D.cpp
@@ -0,0 +1,373 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (c) 2020-2030 by Sophgo Technologies Inc. All rights reserved.
+//
+// Licensed under the Apache License v2.0.
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information.
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+#include "tpu_mlir/Backend/BM168x/BM1684x.h"
+#include "tpu_mlir/Dialect/Tpu/IR/TpuOps.h"
+#include "tpu_mlir/Support/Dnnl/Conv.h"
+#include "tpu_mlir/Support/Helper/Module.h"
+#include "tpu_mlir/Support/Helper/Quant.h"
+#include "tpu_mlir/Support/MathUtils.h"
+
+using namespace mlir;
+using namespace tpu_mlir;
+using namespace tpu_mlir::helper;
+using namespace tpu_mlir::backend;
+
+// ======================================
+// WeightReorderInterface
+// ======================================
+
+template <typename T>
+static void filter_reorder(std::shared_ptr<std::vector<T>> &filter,
+                           std::vector<int64_t> &shape) {
+  int32_t oc = shape[0];
+  int32_t ic = shape[1];
+  int32_t kd = shape[2];
+  int32_t kh = shape[3];
+  int32_t kw = shape[4];
+  auto type_bytes = sizeof(T);
+  int32_t IC_PARALLEL = 64 / type_bytes;
+  auto kernel_hw = kh * kw;
+  int32_t new_ic = ceiling_func(ic * kd, IC_PARALLEL);
+  int32_t new_hw = kernel_hw * IC_PARALLEL;
+  auto filter_new = std::make_shared<std::vector<T>>(oc * new_ic * new_hw, 0);
+  for (int oc_idx = 0; oc_idx < oc; oc_idx++) {
+    for (int ic_idx = 0; ic_idx < new_ic; ic_idx++) {
+      for (int k_idx = 0; k_idx < kernel_hw; k_idx++) {
+        for (int inner = 0; inner < IC_PARALLEL; inner++) {
+          if (ic_idx * IC_PARALLEL + inner >= ic * kd)
+            break;
+          int orig_offset = oc_idx * ic * kd * kh * kw +
+                            (ic_idx * IC_PARALLEL + inner) * kernel_hw + k_idx;
+          int trans_offset = oc_idx * new_ic * new_hw + ic_idx * new_hw +
+                             k_idx * IC_PARALLEL + inner;
+          filter_new->at(trans_offset) = filter->at(orig_offset);
+        }
+      }
+    }
+  }
+  filter = filter_new;
+  shape = {1, oc, new_ic, kh * kw, IC_PARALLEL};
+}
+
+void tpu::Conv3DOp::weight_reorder_int8_bm1684x() {
+  conv_attr_t attr = {0};
+  parseParam(&attr);
+  // filter
+  auto filterOp = filter().getDefiningOp<top::WeightOp>();
+  auto filter_i8 = filterOp.read<int8_t>();
+  std::vector<int64_t> filter_shape = {attr.oc, attr.ic / attr.groups, attr.kd,
+                                       attr.kh, attr.kw};
+  // (oc, ic, kd, kh, kw) -> (oc, (ic*kt)/64, kh, kw, 64)
+  filter_reorder(filter_i8, filter_shape);
+
+  auto op = getOperation();
+  OpBuilder builder(getContext());
+  auto elem_type = Module::getStorageType(filter());
+  auto filter_type = RankedTensorType::get(filter_shape, elem_type);
+  auto new_filter =
+      top::WeightOp::create(op, "reordered", *filter_i8, filter_type);
+  op->setOperand(1, new_filter);
+
+  // bias op
+  if (attr.has_bias) {
+    llvm::SmallVector<int64_t> bias_shape = {1, attr.oc, 1, 1, 1};
+    auto new_type =
+        RankedTensorType::get(bias_shape, Module::getStorageType(bias()));
+    bias().setType(new_type);
+  }
+}
+
+void tpu::Conv3DOp::weight_reorder_bf16_bm1684x() {
+  conv_attr_t attr = {0};
+  parseParam(&attr);
+  auto filterOp = filter().getDefiningOp<top::WeightOp>();
+  if (attr.is_dw || attr.groups > 1) {
+    llvm_unreachable("depthwise should support !!");
+  }
+  auto filter_u16 = filterOp.read<uint16_t>();
+  std::vector<int64_t> filter_shape = {attr.oc, attr.ic, attr.kd, attr.kh,
+                                       attr.kw};
+  filter_reorder(filter_u16, filter_shape);
+
+  auto op = getOperation();
+  OpBuilder builder(getContext());
+  auto filter_type = Module::getStorageType(filter());
+  auto new_filter_type = RankedTensorType::get(filter_shape, filter_type);
+  auto newFilterOp =
+      top::WeightOp::create(op, "reordered", *filter_u16, new_filter_type);
+  op->setOperand(1, newFilterOp);
+
+  // bias op
+  if (attr.has_bias) {
+    auto biasOp = bias().getDefiningOp<top::WeightOp>();
+    llvm::SmallVector<int64_t> bias_shape = {1, attr.oc, 1, 1, 1};
+    auto new_type =
+        RankedTensorType::get(bias_shape, Module::getStorageType(bias()));
+    bias().setType(new_type);
+  }
+}
+
+void tpu::Conv3DOp::weight_reorder_f16_bm1684x() {
+  weight_reorder_bf16_bm1684x();
+}
+
+void tpu::Conv3DOp::weight_reorder_f32_bm1684x() {
+  conv_attr_t attr = {0};
+  parseParam(&attr);
+  auto op = getOperation();
+  auto out_type = Module::getStorageType(output());
+  // filter reorder
+  auto filterOp = filter().getDefiningOp<top::WeightOp>();
+  int64_t filter_shape[5];
+  if (out_type.isF32()) {
+    filter_shape[0] = 1;
+    filter_shape[1] = attr.oc;
+    filter_shape[2] = attr.ic / attr.groups;
+    filter_shape[3] = attr.kd * attr.kh * attr.kw;
+    filter_shape[4] = 1;
+    auto new_type = RankedTensorType::get(filter_shape, out_type);
+    filter().setType(new_type);
+  } else {
+    dump();
+    llvm_unreachable("op type not support");
+  }
+
+  // bias op
+  if (attr.has_bias) {
+    auto biasOp = bias().getDefiningOp<top::WeightOp>();
+    llvm::SmallVector<int64_t> bias_shape = {1, attr.oc, 1, 1, 1};
+    auto new_type = RankedTensorType::get(bias_shape, out_type);
+    bias().setType(new_type);
+  }
+}
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+typedef struct {
+  uint64_t input_global_addr;
+  uint64_t weight_global_addr;
+  uint64_t bias_global_addr;
+  uint64_t output_global_addr;
+  int32_t input_shape[5]; // (n, ic, it, ih, iw)
+  int32_t groups;
+  int32_t output_c;
+  int32_t kernel[3];
+  int32_t stride[3];
+  int32_t dilation[3];
+  int32_t pad[6];
+  int32_t has_bias;
+  int32_t input_dtype;
+  int32_t weight_dtype;
+  int32_t bias_dtype;
+  int32_t output_dtype;
+  int32_t do_relu;
+  float relu_limit;
+  uint64_t kzp_global_addr;
+  uint64_t pad_global_addr;
+  bool kzp_is_const;
+  bool pad_is_const;
+  int32_t kzp_val;
+  int32_t pad_val;
+  int32_t kzp_dtype;
+} conv3d_global_spec_t;
+
+typedef struct conv_local_param {
+  uint32_t input_local_addr;
+  uint32_t weight_local_addr;
+  uint32_t bias_local_addr;
+  uint32_t buffer_local_addr;
+  uint32_t output_local_addr;
+  int32_t input_shape[5]; // (id, n, ic, ih, iw)
+  int32_t groups;
+  int32_t output_c;
+  int32_t kernel[3];   // (kd, kh, kw)
+  int32_t stride[3];   // (sd, sh, sw)
+  int32_t dilation[3]; // (dd, dh, dw)
+  int32_t pad[6];      // (df, db, ht, hb, wl, wr)
+  int32_t has_bias;
+  int32_t input_dtype;
+  int32_t weight_dtype;
+  int32_t bias_dtype;
+  int32_t output_dtype;
+  int32_t do_relu;
+  float relu_limit;
+  uint32_t kzp_local_addr;
+  uint32_t pad_local_addr;
+  bool kzp_is_const;
+  bool pad_is_const;
+  int32_t kzp_val;
+  int32_t pad_val;
+  int32_t kzp_dtype;
+} conv3d_local_spec_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+void tpu::Conv3DOp::codegen_global_bm1684x() {
+  conv_attr_t attr = {0};
+  parseParam(&attr);
+  conv3d_global_spec_t spec;
+  memset(&spec, 0, sizeof(spec));
+  spec.input_global_addr = Module::getAddress(input());
+  spec.weight_global_addr = Module::getAddress(filter());
+  spec.output_global_addr = Module::getAddress(output());
+  if (attr.has_bias) {
+    spec.has_bias = 1;
+    spec.bias_global_addr = Module::getAddress(bias());
+    spec.bias_dtype = BM168x::getDataType(bias());
+  }
+  auto shape = Module::getShape(input());
+  for (size_t i = 0; i < shape.size(); ++i) {
+    spec.input_shape[i] = shape[i];
+  }
+  spec.groups = attr.groups;
+  spec.output_c = attr.oc;
+  spec.kernel[0] = attr.kd;
+  spec.kernel[1] = attr.kh;
+  spec.kernel[2] = attr.kw;
+  spec.stride[0] = attr.sd;
+  spec.stride[1] = attr.sh;
+  spec.stride[2] = attr.sw;
+  spec.dilation[0] = attr.dd;
+  spec.dilation[1] = attr.dh;
+  spec.dilation[2] = attr.dw;
+  spec.pad[0] = attr.pdf;
+  spec.pad[1] = attr.pdb;
+  spec.pad[2] = attr.pht;
+  spec.pad[3] = attr.phb;
+  spec.pad[4] = attr.pwl;
+  spec.pad[5] = attr.pwr;
+  spec.input_dtype = BM168x::getDataType(input());
+  spec.weight_dtype = BM168x::getDataType(filter());
+  spec.output_dtype = BM168x::getDataType(output());
+  spec.do_relu = attr.do_relu;
+  spec.relu_limit = attr.relu_limit;
+  if (Quant::isUniformQuantized(input())) {
+    auto out_etype = Module::getStorageType(output());
+    spec.do_relu = out_etype.isUnsignedInteger(8);
+    auto in_qtype = Quant::getUniformQuantizedType(input());
+    spec.kzp_is_const = true;
+    spec.kzp_val = 0;
+    spec.kzp_dtype = spec.weight_dtype;
+    spec.pad_is_const = true;
+    spec.pad_val = in_qtype.getZeroPoint();
+  }
+  BM1684x::instance().call_global_func("backend_api_conv3d_global", &spec,
+                                       sizeof(spec));
+}
+
+// ======================================
+// LocalGenInterface
+// ======================================
+
+int64_t tpu::Conv3DOp::getBufferSize_bm1684x(
+    int64_t in_lmem_bytes, int64_t out_lmem_bytes, int64_t in_nslice,
+    int64_t in_hslice, int64_t out_nslice, int64_t out_hslice) {
+  conv_attr_t attr = {0};
+  parseParam(&attr);
+  int64_t sz = 0;
+  const int64_t npu_num = BM1684x::NPU_NUM;
+
+  int32_t oc_per_npu = 0;
+  for (int32_t i = 0; i < attr.groups; i++) {
+    oc_per_npu =
+        std::max((int64_t)oc_per_npu,
+                 ceiling_func(i * attr.groups % npu_num + attr.oc / attr.groups,
+                              npu_num));
+  }
+  auto in_type = input().getType();
+  auto out_type = output().getType();
+  // output start npu id must be same with weight start npu id
+  if ((in_type.isF16() || in_type.isBF16()) && !out_type.isF32() &&
+      attr.kd > 1) {
+    sz += (oc_per_npu *
+           align_up(out_hslice * attr.ow,
+                    BM1684x::instance().get_eu_num(sizeof(float))) *
+           sizeof(float));
+  }
+
+  // input must start from npu 0
+  if ((in_type.isF16() || in_type.isBF16()) && attr.groups > 1) {
+    sz += ceiling_func((int64_t)attr.ic / attr.groups, npu_num) *
+          align_up(in_hslice * attr.iw,
+                   BM1684x::instance().get_eu_num(sizeof(int16_t))) *
+          sizeof(int16_t);
+  }
+  if ((in_type.isInteger(8)) && attr.groups > 1) {
+    sz += ceiling_func((int64_t)attr.ic / attr.groups, npu_num) *
+          align_up(in_hslice * attr.iw,
+                   BM1684x::instance().get_eu_num(sizeof(int8_t))) *
+          sizeof(int8_t);
+  }
+  return sz;
+}
+
+void tpu::Conv3DOp::codegen_local_bm1684x(int64_t n_step, int64_t h_step) {
+  conv_attr_t attr = {0};
+  parseParam(&attr);
+  auto op = getOperation();
+  auto input_spec = BM1684x::get_input_spec(op);
+  auto output_spec = BM1684x::get_output_spec(op);
+  auto gi = getGroupInfo(n_step, h_step);
+  auto in_gi = LocalGenInterface::getGroupInfo(input(), n_step, h_step);
+  conv3d_local_spec_t spec;
+  memset(&spec, 0, sizeof(spec));
+  spec.input_local_addr = in_gi.out_addr;
+  spec.weight_local_addr = LocalGenInterface::getGroupInfo(filter()).out_addr;
+  if (attr.has_bias) {
+    spec.has_bias = true;
+    spec.bias_local_addr = LocalGenInterface::getGroupInfo(bias()).out_addr;
+    spec.bias_dtype = BM168x::getDataType(bias());
+  }
+  spec.buffer_local_addr = gi.buffer_addr;
+  spec.output_local_addr = gi.out_addr;
+  spec.input_shape[0] = attr.id;
+  spec.input_shape[1] = in_gi.n_slice;
+  spec.input_shape[2] = attr.ic;
+  spec.input_shape[3] = in_gi.h_slice;
+  spec.input_shape[4] = attr.iw;
+  spec.groups = attr.groups;
+  spec.output_c = attr.oc;
+  spec.kernel[0] = attr.kd;
+  spec.kernel[1] = attr.kh;
+  spec.kernel[2] = attr.kw;
+  spec.stride[0] = attr.sd;
+  spec.stride[1] = attr.sh;
+  spec.stride[2] = attr.sw;
+  spec.dilation[0] = attr.dd;
+  spec.dilation[1] = attr.dh;
+  spec.dilation[2] = attr.dw;
+  spec.pad[0] = attr.pdf;
+  spec.pad[1] = attr.pdb;
+  spec.pad[2] = in_gi.h_idx == 0 ? attr.pht : 0;
+  spec.pad[3] = in_gi.h_idx + in_gi.h_slice >= attr.ih ? attr.phb : 0;
+  spec.pad[4] = attr.pwl;
+  spec.pad[5] = attr.pwr;
+  spec.input_dtype = BM168x::getDataType(input());
+  spec.weight_dtype = BM168x::getDataType(filter());
+  spec.output_dtype = BM168x::getDataType(output());
+  spec.do_relu = attr.do_relu;
+  spec.relu_limit = attr.relu_limit;
+  if (Quant::isUniformQuantized(input())) {
+    auto out_etype = Module::getStorageType(output());
+    spec.do_relu = out_etype.isUnsignedInteger(8);
+    auto in_qtype = Quant::getUniformQuantizedType(input());
+    spec.kzp_is_const = true;
+    spec.kzp_val = 0;
+    spec.kzp_dtype = spec.weight_dtype;
+    spec.pad_is_const = true;
+    spec.pad_val = in_qtype.getZeroPoint();
+  }
+  BM1684x::instance().call_local_func("backend_api_conv3d_local", &spec,
+                                      sizeof(conv3d_local_spec_t));
+}
---
+++ lib/Dialect/Tpu/Interfaces/Common/Conv1D.cpp
@@ -0,0 +1,133 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (c) 2020-2030 by Sophgo Technologies Inc. All rights reserved.
+//
+// Licensed under the Apache License v2.0.
+// See http://www.apache.org/licenses/LICENSE-2.0 for license information.
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+#include "tpu_mlir/Dialect/Tpu/IR/TpuOps.h"
+#include "tpu_mlir/Support/Dnnl/Dnnl.h"
+#include "tpu_mlir/Support/Float16.h"
+#include "tpu_mlir/Support/Helper/Module.h"
+#include "tpu_mlir/Support/Helper/Quant.h"
+#include "tpu_mlir/Support/MathUtils.h"
+
+using namespace tpu_mlir;
+using namespace tpu_mlir::helper;
+using namespace mlir;
+
+void tpu::Conv1DOp::parseParam(void *param) {
+  conv_attr_t *p = (conv_attr_t *)param;
+  memset(p, 0, sizeof(conv_attr_t));
+  p->id = p->od = p->kd = p->sd = p->dd = 1;
+  p->iw = p->ow = p->kw = p->sw = p->dw = 1;
+  auto i_s = input().getType().cast<RankedTensorType>().getShape();
+  auto o_s = output().getType().cast<RankedTensorType>().getShape();
+  p->do_relu = this->do_relu();
+  p->relu_limit = relu_limit().convertToDouble();
+  p->has_bias = with_bias();
+  p->n = i_s[0];
+  p->ic = i_s[1];
+  p->ih = i_s[2];
+  p->oc = o_s[1];
+  p->oh = o_s[2];
+  auto kernel = Module::getI64Array(kernel_shape());
+  p->kh = kernel->at(0);
+  auto pads_v = Module::getI64Array(pads());
+  p->pht = pads_v->at(0);
+  p->phb = pads_v->at(1);
+  if (Quant::isUniformQuantized(input())) {
+    p->pad_value = Quant::getUniformQuantizedType(input()).getZeroPoint();
+  }
+  auto strides_v = Module::getI64Array(strides());
+  p->sh = strides_v->at(0);
+  auto dilation = Module::getI64Array(dilations(), 1, 1);
+  p->dh = dilation->at(0);
+  auto ins = Module::getI64Array(inserts(), 1, 0);
+  p->ins_h = ins->at(0);
+  assert(p->ins_h == 0 && p->ins_w == 0);
+  p->groups = group();
+  p->is_dw = (p->oc == p->ic && p->oc == p->groups && p->groups > 1);
+}
+LogicalResult tpu::Conv1DOp::init(InferenceParameter &p) {
+  auto conv = new Conv();
+  conv_attr_t attr = {0};
+  parseParam(&attr);
+
+  conv->setup(p.inputs[0], p.inputs[1], p.inputs[2], p.outputs[0], attr);
+  p.handle = (void *)conv;
+  return success();
+}
+
+void tpu::Conv1DOp::deinit(InferenceParameter &p) {
+  if (p.handle != nullptr) {
+    auto conv = (Conv *)p.handle;
+    delete conv;
+    p.handle = nullptr;
+  }
+}
+
+LogicalResult tpu::Conv1DOp::inference(InferenceParameter &p) {
+  if (p.handle == nullptr) {
+    return failure();
+  }
+  auto conv = (Conv *)p.handle;
+  conv->run();
+  // requant
+  auto out_type = Module::getStorageType(output());
+  auto num_elem = Module::getNumElements(output());
+  if (out_type.isa<FloatType>()) {
+    if (out_type.isBF16()) {
+      f32_to_bf16(p.outputs[0], p.outputs[0], num_elem);
+    } else if (out_type.isF16()) {
+      f32_to_f16(p.outputs[0], p.outputs[0], num_elem);
+    }
+  } else if (Quant::isUniformQuantized(output())) {
+    int64_t n, c, h, w;
+    auto sType = Module::getStorageType(output());
+    Module::getNCHW(output(), n, c, h, w);
+    auto o_qtype = Quant::getUniformQuantizedType(output());
+    auto rshift_v = Module::getI64Array(rshift().getValue());
+    auto multiplier_v = Module::getI64Array(multiplier(), rshift_v->size(), 1);
+    bool per_axis = rshift_v->size() == c;
+    auto mode = quant_mode().hasValue() ? quant_mode().getValue() : 2;
+#pragma omp parallel for schedule(static, omp_schedule(c))
+    for (int ic = 0; ic < c; ic++) {
+      int64_t shift = per_axis ? rshift_v->at(ic) : rshift_v->at(0);
+      int64_t multi = per_axis ? multiplier_v->at(ic) : multiplier_v->at(0);
+      for (int in = 0; in < n; in++) {
+        for (int hw = 0; hw < h * w; hw++) {
+          int offset = (in * c + ic) * h * w + hw;
+          int64_t v = 0;
+          if (mode == 0) {
+            v = MultiplyByQuantizedMultiplier((int32_t)p.outputs[0][offset],
+                                              (int32_t)multi, (int32_t)shift) +
+                o_qtype.getZeroPoint();
+          } else {
+            v = applyMultiplierAndRShift(p.outputs[0][offset], multi, shift) +
+                o_qtype.getZeroPoint();
+          }
+          p.outputs[0][offset] = sType.isUnsignedInteger(8) ? Quant::to_uint8(v)
+                                                            : Quant::to_int8(v);
+        }
+      }
+    }
+  }
+
+  return success();
+}
+
+LogicalResult tpu::Conv1DOp::BackwardH(int64_t &in_idx, int64_t &in_slice,
+                                       int64_t out_idx, int64_t out_slice) {
+  conv_attr_t attr = {0};
+  parseParam(&attr);
+  int kh_with_dh = (attr.kh - 1) * attr.dh + 1;
+  in_slice = (out_slice - 1) * attr.sh +
+             (kh_with_dh >= attr.sh ? kh_with_dh : attr.sh);
+  in_idx = out_idx * attr.sh - attr.pht;
+  LocalGenInterface::fixSlice(in_idx, in_slice, attr.ih);
+  return success();
+}
