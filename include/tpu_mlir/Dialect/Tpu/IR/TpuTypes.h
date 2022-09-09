abbb93a7a0cf5015bbec3bda404a4ced3d2225ce
man.lu <man.lu@sophgo.com>
2022-09-09 14:57:43 +0000

    [doc] fix tpu_perf package name
    - Split the definition of tpu_type AttributeType out of the TPU for
      TOP layer to reuse this type.
    
    Change-Id: I95ae0be6842c002d0818879ac981ebca54c09b51

---
+++ include/tpu_mlir/Dialect/Tpu/IR/TpuTypes.h
@@ -0,0 +1,15 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
+//
+// TPU-MLIR is licensed under the 2-Clause BSD License except for the
+// third-party components.
+//
+//===----------------------------------------------------------------------===//
+
+#pragma once
+
+#include "mlir/IR/BuiltinTypes.h"
+#include "tpu_mlir/Dialect/Tpu/IR/TpuEnum.h.inc"
+#define GET_ATTRDEF_CLASSES
+#include "tpu_mlir/Dialect/Tpu/IR/TpuAttr.h.inc"
