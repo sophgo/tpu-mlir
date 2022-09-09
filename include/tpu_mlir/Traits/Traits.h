c92d791163b8362487dcaf4fd522c99d37585481
pengchao.hu <pengchao.hu@sophgo.com>
2022-09-09 10:58:54 +0800

    remove name trait
    Change-Id: I16c42d4c5167b2786461760150d41930d8de9e84

--- include/tpu_mlir/Traits/Traits.h
+++ include/tpu_mlir/Traits/Traits.h
@@ -31,22 +31,6 @@ template <typename ConcreteType>
 class SupportFuseRelu
     : public ::mlir::OpTrait::TraitBase<ConcreteType, SupportFuseRelu> {};
 
-// If a op has this trait, it should have nameAttr
-template <typename ConcreteType>
-class HasCommonAttributes
-    : public ::mlir::OpTrait::TraitBase<ConcreteType, HasCommonAttributes> {
-public:
-  static mlir::LogicalResult verifyTrait(mlir::Operation *op) {
-    // static constexpr llvm::StringRef commonAttrs[] = {"name"};
-    // for (auto attr : commonAttrs) {
-    //   if (!op->hasAttrOfType<mlir::StringAttr>(attr)) {
-    //     return op->emitError("expected operation to have attribute: " + attr);
-    //   }
-    // }
-    return mlir::success();
-  }
-};
-
 template <typename ConcreteType>
 class TpuTypeRestrict
     : public ::mlir::OpTrait::TraitBase<ConcreteType, TpuTypeRestrict> {
