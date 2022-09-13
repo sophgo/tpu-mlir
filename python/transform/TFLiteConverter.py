66af57dcbd6b0106f81baad835a3a3455c6663e1
pengchao.hu <pengchao.hu@sophgo.com>
2022-09-13 11:44:07 +0800

    remove redundant preprocess code
    Change-Id: Ieebf20204d037857f41e922f9db3d2b03cc3ac4e

--- python/transform/TFLiteConverter.py
+++ python/transform/TFLiteConverter.py
@@ -635,17 +635,7 @@ class TFLiteConverter(BaseConverter):
             if not self.preprocess_args or not image:
                 input_op = self.mlir.create_input_op(input.name, idx, **{})
             else:
-                preprocess_hint = {
-                    'mean': self.preprocess_args['mean'],
-                    'scale': self.preprocess_args['scale'],
-                    'pixel_format': self.preprocess_args["pixel_format"],
-                    'channel_format': self.preprocess_args["channel_format"],
-                    'pad_type': self.preprocess_args["pad_type"],
-                    'resize_dims': self.preprocess_args['resize_dims'],
-                    'keep_aspect_ratio': self.preprocess_args['keep_aspect_ratio'],
-                    'pad_value': self.preprocess_args['pad_value']
-                }
-                input_op = self.mlir.create_input_op(input.name, idx, **preprocess_hint)
+                input_op = self.mlir.create_input_op(input.name, idx, **self.preprocess_args)
             symbol_table.update({input.id: input_op})
 
         def add_operation(operation):
