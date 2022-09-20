1a48714e7e6e4d2d210ba5e65ef50122bdae6fe0
pengchao.hu <pengchao.hu@sophgo.com>
2022-09-20 12:00:52 +0800

    adjust quick start doc
    Change-Id: Id29519b8c814f7a0b457a7cffaa6e9d9b4a2487f

--- release_doc.sh
+++ release_doc.sh
@@ -3,7 +3,7 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-# generate tpu-mlir_quick_start_guide
+# generate tpu-mlir_quick_start
 # ------------------------------------------------------------------------------
 pushd ${DIR}/doc/quick_start
 make clean
@@ -11,7 +11,7 @@ make pdf
 make html
 popd
 
-# generate tpu-mlir_developer_manual
+# generate tpu-mlir_technical_manual
 # ------------------------------------------------------------------------------
 pushd ${DIR}/doc/developer_manual
 make clean
@@ -20,17 +20,22 @@ make html
 popd
 # ------------------------------------------------------------------------------
 
-mkdir -p ${DIR}/build/doc/quick_start
-cp -rf ${DIR}/doc/quick_start/build/tpu-mlir_quick_start_guide_zh.pdf \
-   ${DIR}/build/doc/quick_start/
-cp -rf ${DIR}/doc/quick_start/build/html ${DIR}/build/doc/quick_start/html
+mkdir -p ${DIR}/build/doc/quick_start_zh
+cp -rf ${DIR}/doc/quick_start/build_zh/tpu-mlir_quick_start_zh.pdf \
+   ${DIR}/build/doc/quick_start_zh/
+cp -rf ${DIR}/doc/quick_start/build_zh/html ${DIR}/build/doc/quick_start_zh
+
+mkdir -p ${DIR}/build/doc/quick_start_en
+cp -rf ${DIR}/doc/quick_start/build_en/tpu-mlir_quick_start_en.pdf \
+   ${DIR}/build/doc/quick_start_en/
+cp -rf ${DIR}/doc/quick_start/build_en/html ${DIR}/build/doc/quick_start_en
 
 mkdir -p ${DIR}/build/doc/developer_manual_zh
-cp -f ${DIR}/doc/developer_manual/build_zh/tpu-mlir_developer_manual_zh.pdf \
+cp -f ${DIR}/doc/developer_manual/build_zh/tpu-mlir_technical_manual_zh.pdf \
    ${DIR}/build/doc/developer_manual_zh/
 cp -rf ${DIR}/doc/developer_manual/build_zh/html ${DIR}/build/doc/developer_manual_zh/
 
 mkdir -p ${DIR}/build/doc/developer_manual_en
-cp -f ${DIR}/doc/developer_manual/build_en/tpu-mlir_developer_manual_en.pdf \
+cp -f ${DIR}/doc/developer_manual/build_en/tpu-mlir_technical_manual_en.pdf \
    ${DIR}/build/doc/developer_manual_en/
 cp -rf ${DIR}/doc/developer_manual/build_en/html ${DIR}/build/doc/developer_manual_en/
