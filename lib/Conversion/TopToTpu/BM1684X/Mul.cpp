58c7853ccfca9ae28882ff2dda6dc3d22bc107f2
man.lu <man.lu@sophgo.com>
2022-11-04 03:35:57 +0000

    BroadcastMul supports one of the inputs is constant
    Change-Id: I6224a5d682524d629bdc5ea572a50ea4fd6b9dbb

--- lib/Conversion/TopToTpu/BM1684X/Mul.cpp
+++ lib/Conversion/TopToTpu/BM1684X/Mul.cpp
@@ -24,21 +24,47 @@ void MulLowering::LoweringINT8(PatternRewriter &rewriter, top::MulOp op,
   }
   const int nInputs = op->getNumOperands();
   std::vector<Value> operands;
-  double scale;
-  int64_t zp_o;
-  double scale_o;
+  double scale = 1;
+  int64_t zp_o = 0;
+  double scale_o = 1;
   Quant::getScaleAndZeroPoint(op.output(), scale_o, zp_o, asymmetric);
 
   double scale_i;
   int64_t zp;
   for (int i = 0; i < nInputs; i++) {
     auto input = op->getOperand(i);
-    operands.push_back(input);
-    Quant::getScaleAndZeroPoint(input, scale_i, zp, asymmetric);
-    if (i == 0)
-      scale = scale_i;
-    else
-      scale *= scale_i;
+    if (auto constOp = dyn_cast<top::WeightOp>(input.getDefiningOp())) {
+      auto constF32 = constOp.read<float>();
+      float fmax, fmin;
+      findMinMax(constF32->data(), constF32->size(), &fmin, &fmax);
+      bool cSign = (fmin < 0);
+      float fqmax = cSign ? 127 : 255;
+      auto filter_type = input.getType().cast<RankedTensorType>();
+      auto new_type = RankedTensorType::get(filter_type.getShape(),
+                                            rewriter.getIntegerType(8, cSign));
+      scale_i = fmax / fqmax;
+      if (cSign) {
+        auto constI8 = std::make_shared<std::vector<int8_t>>(constF32->size());
+        std::transform(
+            constF32->begin(), constF32->end(), constI8->begin(),
+            [&](const float cf32) { return Quant::to_int8(cf32 / scale_i); });
+        auto new_filter =
+            top::WeightOp::create(constOp, "i8", *constI8, new_type);
+        operands.push_back(new_filter);
+      } else {
+        auto constU8 = std::make_shared<std::vector<uint8_t>>(constF32->size());
+        std::transform(
+            constF32->begin(), constF32->end(), constU8->begin(),
+            [&](const float cf32) { return Quant::to_uint8(cf32 / scale_i); });
+        auto new_filter =
+            top::WeightOp::create(constOp, "u8", *constU8, new_type);
+        operands.push_back(new_filter);
+      }
+    } else {
+      Quant::getScaleAndZeroPoint(input, scale_i, zp, asymmetric);
+      operands.push_back(input);
+    }
+    scale *= scale_i;
   }
 
   scale /= scale_o;
@@ -58,10 +84,22 @@ void MulLowering::LoweringINT8(PatternRewriter &rewriter, top::MulOp op,
 }
 
 void MulLowering::LoweringBF16(PatternRewriter &rewriter, top::MulOp op) const {
+  for (int i = 0, n = op.getNumOperands(); i < n; ++i) {
+    if (auto constOp =
+            dyn_cast<top::WeightOp>(op.getOperand(i).getDefiningOp())) {
+      op.setOperand(i, constOp.clone_bf16(op));
+    }
+  }
   lowering_common_bf16<tpu::MulOp>(rewriter, op);
 }
 
 void MulLowering::LoweringF16(PatternRewriter &rewriter, top::MulOp op) const {
+  for (int i = 0, n = op.getNumOperands(); i < n; ++i) {
+    if (auto constOp =
+            dyn_cast<top::WeightOp>(op.getOperand(i).getDefiningOp())) {
+      op.setOperand(i, constOp.clone_f16(op));
+    }
+  }
   lowering_common_f16<tpu::MulOp>(rewriter, op);
 }
 
