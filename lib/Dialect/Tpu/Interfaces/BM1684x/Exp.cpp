94f7cf122dc94b58b3ae08bcf6a043b04dc846db
Pzzzzz <hello-cd.plus@hotmail.com>
2022-11-21 20:15:11 +0800

    add ExpOp

---
+++ lib/Dialect/Tpu/Interfaces/BM1684x/Exp.cpp
@@ -0,0 +1,35 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
+//
+// TPU-MLIR is licensed under the 2-Clause BSD License except for the
+// third-party components.
+//
+//===----------------------------------------------------------------------===//
+
+#include "tpu_mlir/Backend/BM168x/BM1684x.h"
+#include "tpu_mlir/Dialect/Tpu/IR/TpuOps.h"
+#include "tpu_mlir/Support/Helper/Module.h"
+#include "tpu_mlir/Support/Helper/Quant.h"
+#include "tpu_mlir/Support/MathUtils.h"
+
+using namespace mlir;
+using namespace tpu_mlir;
+using namespace tpu_mlir::helper;
+using namespace tpu_mlir::backend;
+
+// =========================================
+// GlobalGenInterface
+// =========================================
+
+void tpu::ExpOp::codegen_global_bm1684x() {
+  active_global_spec_t spec;
+  memset(&spec, 0, sizeof(spec));
+  spec.common.active_type = ACTIVE_EXP;
+  auto op = getOperation();
+  auto input_spec = BM1684x::get_input_spec(op);
+  auto output_spec = BM1684x::get_output_spec(op);
+  BM1684x::instance().call_global_func("backend_api_active_global", &spec,
+                                       sizeof(spec), input_spec->data(),
+                                       output_spec->data());
+}
