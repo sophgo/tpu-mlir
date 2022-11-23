ae9fd5a7786fc9836bfac8320decf087fac7673e
shanglin.guo <shanglin.guo@sophgo.com>
2022-11-23 18:11:28 +0800

    [tpu-mlir] calculate the reduceOp's imm buffer size according to nodechip
    Change-Id: I2b46c37834da4bda0c7413c6609fadebc884654e

--- lib/Dialect/Tpu/Transforms/BM168x/GlobalBuffer.cpp
+++ lib/Dialect/Tpu/Transforms/BM168x/GlobalBuffer.cpp
@@ -59,8 +59,9 @@ public:
     auto type = Module::getStorageType(reduceOp.input());
     // add buffer
     /* if reduce n or c, need imm buffer. if reduce h/w, don't need imm buffer
-       if reduce c/h, c/w, n/h, n/w, will split it to 2 step at fronted, it will not go here.
-       if reduce c/h/w, n/h/w, need imm buffer */
+       if reduce c/h, c/w, n/w, will split it to 2 step at fronted, it will not go here.
+       if reduce c/h/w, n/h/w, need imm buffer .
+       Note: reduce max/mean and n/h, don't support it now (transpose need imm buffer) */
     auto axes_val = Module::getI64Array(reduceOp.axes());
     auto axis_num = axes_val->size();
     auto in_tensor = Module::getShape(reduceOp.input());
@@ -69,11 +70,36 @@ public:
       is_reduce[axes_val->at(i)] = 1;
     }
 
+    /* reducemax/mean and reduce n/w,c/h,c/w, will use max/avg pool to implement it.*/
     if ((axis_num == 1 && (is_reduce[0] || is_reduce[1]))
-        || (axis_num == 2 && (is_reduce[0] && is_reduce[1]))
         || (axis_num == 3 && (is_reduce[0] || is_reduce[1]))
         || (axis_num >= 4 && axis_num == in_tensor.size())) {
-      std::vector<int64_t> buffer_shape = {1, 100000};
+      //calculate the actual imm buffer size according to nodechip_reduce
+      int imm_buffer_size = 0;
+      if (axis_num >=4 && axis_num == in_tensor.size()){
+        for (int i = in_tensor.size() - 3; i >=0; i--){
+          imm_buffer_size *= in_tensor[i];
+        }
+        imm_buffer_size *= sizeof(float_t);
+      } else if (axis_num == 1 && (is_reduce[0] || is_reduce[1])) {
+        for (int i = in_tensor.size() - 1; i >=0; i--){
+          imm_buffer_size *= in_tensor[i];
+        }
+        imm_buffer_size *= sizeof(float_t) *2;
+      } else if (axis_num == 3 && (is_reduce[0] || is_reduce[1])) {
+        int tmp_val  = sizeof(float_t);
+        for (int i = in_tensor.size() - 3; i >=0; i--){
+          tmp_val *= in_tensor[i];
+        }
+        imm_buffer_size += tmp_val;
+        tmp_val = sizeof(float_t) * 2;
+        for (int i = in_tensor.size() - 1; i >= 0; i--){
+          tmp_val *= in_tensor[i];
+        }
+        imm_buffer_size += tmp_val;
+      }
+
+      std::vector<int64_t> buffer_shape = {1, imm_buffer_size};
       auto buffer_type = RankedTensorType::get(buffer_shape, type);
       auto buffer = tpu::BufferOp::create(reduceOp, buffer_type);
       reduceOp.buffer().replaceAllUsesWith(buffer);
