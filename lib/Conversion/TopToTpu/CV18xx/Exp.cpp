34c6035b61d77cda525d563190e106b0da408af5
shenglin.li <shenglin.li01@sophgo.com>
2022-11-29 18:39:24 +0800

    Support CV18XX SplitOp, ExpOp, ConstPow and PowConst.
    Change-Id: I82bad8ac55354eac8a5e3bc4bbc78ed7e05a4229

--- lib/Conversion/TopToTpu/CV18xx/Exp.cpp
+++ lib/Conversion/TopToTpu/CV18xx/Exp.cpp
@@ -11,13 +11,14 @@
 #define DEBUG_TYPE "lowering-log"
 namespace tpu_mlir {
 namespace cv18xx {
-static double active_log(double val) { return std::log(val); }
-
-void LogLowering::LoweringINT8(PatternRewriter &rewriter, top::LogOp op,
-                                   bool asymmetric) const {
+static double active_exp(double val) {
+    return std::exp(val);
+}
+void ExpLowering::LoweringINT8(PatternRewriter &rewriter, top::ExpOp op,
+                               bool asymmetric) const {
   auto stype = Module::getStorageType(op.output());
   Value table = create_lookup_table(op.input(), op.output(), asymmetric,
-                                    [](double val) { return std::log(val); });
+                                    [](double val) { return std::exp(val); });
   std::vector<NamedAttribute> attrs;
   for (auto &attr : op->getAttrs()) {
     attrs.push_back(attr);
@@ -29,40 +30,41 @@ void LogLowering::LoweringINT8(PatternRewriter &rewriter, top::LogOp op,
       attrs);
 }
 
-void LogLowering::LoweringBF16(PatternRewriter &rewriter,
-                                top::LogOp op) const {
+void ExpLowering::LoweringBF16(PatternRewriter &rewriter, top::ExpOp op) const {
   int table_h = 32;
   int table_w = 8;
   int table_hw = table_h * table_w;
   std::vector<float> table(table_hw);
-  std::vector<float> mantissa(table_hw);
-
-  bf16_gen_exponent_mantissa_table("log", table.data(), mantissa.data(), 0.0, 0);
+  std::vector<float> slope_table(table_hw);
+  float range_start = -15;
+  float range_end = 15;
+  bf16_gen_base_slope_table(table.data(), slope_table.data(), range_start, range_end, active_exp);
   auto shape = std::vector<int64_t>{1, 1, table_h, table_w};
-
   OpBuilder builder(op->getContext());
   auto table_type = RankedTensorType::get(shape, builder.getF32Type());
   auto table_op = top::WeightOp::create(op, "table", table, table_type);
-  auto mantissa_op =
-      top::WeightOp::create(op, "mantissa", mantissa, table_type);
-
+  auto slope_table_op = top::WeightOp::create(op, "slope_table", slope_table, table_type);
   std::vector<NamedAttribute> attrs;
   for (auto &attr : op->getAttrs()) {
     attrs.emplace_back(attr);
   }
   attrs.push_back(rewriter.getNamedAttr(
       "lut_mode",
-      tpu::LutModeAttr::get(op->getContext(), tpu::LutMode::Log)));
+      tpu::LutModeAttr::get(op->getContext(), tpu::LutMode::Slope)));
+  attrs.push_back(rewriter.getNamedAttr("min_range",
+                                        rewriter.getF64FloatAttr(range_start)));
+  attrs.push_back(
+      rewriter.getNamedAttr("max_range", rewriter.getF64FloatAttr(range_end)));
   auto tensor_type = op.output().getType().cast<RankedTensorType>();
   auto newType =
       RankedTensorType::get(tensor_type.getShape(), rewriter.getBF16Type());
   auto table_weight_op = dyn_cast<top::WeightOp>(table_op.getDefiningOp());
-  auto mantissa_weight_op =
-      dyn_cast<top::WeightOp>(mantissa_op.getDefiningOp());
+  auto slope_weight_op =
+      dyn_cast<top::WeightOp>(slope_table_op.getDefiningOp());
   rewriter.replaceOpWithNewOp<tpu::LutOp>(
       op, newType,
       ValueRange{op.input(), table_weight_op.clone_bf16(op),
-                 mantissa_weight_op.clone_bf16(op)},
+                 slope_weight_op.clone_bf16(op)},
       attrs);
   return;
 }
