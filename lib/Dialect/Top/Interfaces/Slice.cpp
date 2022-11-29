19e47bf99e07ff1aed6b1458760005578e1e466a
man.lu <man.lu@sophgo.com>
2022-11-29 15:28:48 +0000

    Slice inference supports tensor with arbitrary dimensions.
    - Slice inference supports tensor more then 4 dimensions.
    - "get_scale_and_shift_positive" is more safe due to there is a bug in left-shift.
    
    Change-Id: Iccd754f1c60fa1db1db5888873facc0e3f6d73ab

--- lib/Dialect/Top/Interfaces/Slice.cpp
+++ lib/Dialect/Top/Interfaces/Slice.cpp
@@ -11,14 +11,13 @@
 #include "tpu_mlir/Support/Dnnl/Dnnl.h"
 #include "tpu_mlir/Support/Helper/Module.h"
 #include "tpu_mlir/Support/MathUtils.h"
+#include <valarray>
 
 using namespace tpu_mlir;
 using namespace tpu_mlir::helper;
 using namespace mlir;
 
-int64_t top::SliceOp::getFLOPs() {
-  return 0;
-}
+int64_t top::SliceOp::getFLOPs() { return 0; }
 
 LogicalResult top::SliceOp::init(InferenceParameter &p) { return success(); }
 void top::SliceOp::deinit(InferenceParameter &p) {}
@@ -31,48 +30,30 @@ LogicalResult top::SliceOp::inference(InferenceParameter &p) {
   auto in_shape = Module::getShape(input());
   auto in_dims = in_shape.size();
   auto out_dims = out_shape.size();
-  //just support the dims of input & input is equal and slice at one axis now.
+  // just support the dims of input & input is equal.
   assert(in_dims == out_dims);
+  // slice[range] -> (offset + stride)
+  std::valarray<int64_t> in_stride_v(1, in_dims);
+  std::valarray<int64_t> out_stride_v(1, out_dims);
+  for (int i = in_stride_v.size() - 2; i >= 0; --i) {
+    in_stride_v[i] *= in_stride_v[i + 1] * in_shape[i + 1];
+    out_stride_v[i] *= out_stride_v[i + 1] * out_shape[i + 1];
+  }
+  auto in_offset_v = std::valarray<int64_t>(offset_v->data(), offset_v->size());
+  auto in_offset = (in_offset_v * in_stride_v).sum();
+  auto out_in_stride_v =
+      std::valarray<int64_t>(steps_v->data(), steps_v->size());
+  out_in_stride_v *= in_stride_v;
 
-  if (in_dims == 2) {
-    #pragma omp parallel for schedule(static, omp_schedule(out_num_elem))
-    for (int i = 0 ; i < out_shape[0]; i++) {
-      for (int j = 0; j < out_shape[1]; j++) {
-        memcpy(p.outputs[0] + i * out_shape[1] + j,
-                p.inputs[0] + (offset_v->at(0) + i * steps_v->at(0)) * in_shape[1] + (offset_v->at(1) + j * steps_v->at(1)),
-                  sizeof(float));
-      }
+#pragma omp parallel for schedule(static, omp_schedule(out_num_elem))
+  for (int64_t i = 0; i < out_num_elem; ++i) {
+    std::valarray<int64_t> out_it(1, out_dims);
+    int64_t tmp = i;
+    for (int j = 0; j < out_dims; j++) {
+      out_it[j] = tmp / out_stride_v[j];
+      tmp = tmp % out_stride_v[j];
     }
-  } else if (in_dims == 3) {
-    #pragma omp parallel for schedule(static, omp_schedule(out_num_elem))
-    for (int i = 0; i < out_shape[0]; i++) {
-      for (int j = 0; j < out_shape[1]; j++) {
-        for (int k = 0; k < out_shape[2]; k++) {
-          memcpy(p.outputs[0] + i * out_shape[1] * out_shape[2] + j * out_shape[2] + k,
-                p.inputs[0] + (offset_v->at(0) + i * steps_v->at(0)) * in_shape[1] * in_shape[2]
-                  + (offset_v->at(1) + j * steps_v->at(1)) * in_shape[2]
-                   + (offset_v->at(2) + k * steps_v->at(2)),
-                sizeof(float));
-        }
-      }
-    }
-  } else if (in_dims == 4) {
-    #pragma omp parallel for schedule(static, omp_schedule(out_num_elem))
-    for (int i = 0; i < out_shape[0]; i++) {
-      for (int j = 0; j < out_shape[1]; j++) {
-        for (int k = 0; k < out_shape[2]; k++) {
-          for (int z = 0; z < out_shape[3]; z++) {
-            memcpy(p.outputs[0] + i * out_shape[1] * out_shape[2] * out_shape[3]
-                     + j * out_shape[2] * out_shape[3] + k * out_shape[3] + z,
-                    p.inputs[0] + (offset_v->at(0) + i * steps_v->at(0)) * in_shape[1] * in_shape[2] * in_shape[3]
-                      + (offset_v->at(1) + j * steps_v->at(1)) * in_shape[2] * in_shape[3]
-                      + (offset_v->at(2) + k * steps_v->at(2)) * in_shape[3]
-                      + (offset_v->at(3) + z * steps_v->at(3)),
-                    sizeof(float));
-          }
-        }
-      }
-    }
+    p.outputs[0][i] = p.inputs[0][(out_it * out_in_stride_v).sum() + in_offset];
   }
 
   return success();
--- lib/Dialect/Tpu/Transforms/WeightReorder.cpp
+++ lib/Dialect/Tpu/Transforms/WeightReorder.cpp
@@ -7,7 +7,6 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "mlir/IR/BuiltinTypes.h"
 #include "tpu_mlir/Dialect/Tpu/IR/TpuOps.h"
 #include "tpu_mlir/Dialect/Tpu/Transforms/BM1684/WeightReorder.h"
 #include "tpu_mlir/Dialect/Tpu/Transforms/BM168x/WeightReorder.h"
