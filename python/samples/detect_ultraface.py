e4dc10da9ee788f1bd13539cefd0ba53b5275487
Pzzzzz <hello-cd.plus@hotmail.com>
2022-12-01 16:43:06 +0800

    add ultraface support
    Change-Id: Ie45f9212674b8b3c092e87a50760891655d4d172

---
+++ python/samples/detect_ultraface.py
@@ -0,0 +1,366 @@
+#!/usr/bin/env python3
+# Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
+#
+# TPU-MLIR is licensed under the 2-Clause BSD License except for the
+# third-party components.
+#
+# ==============================================================================
+
+import cv2
+import onnxruntime as ort
+import argparse
+import numpy as np
+from tools.model_runner import mlir_inference, model_inference, onnx_inference
+
+image_mean_test = image_mean = np.array([127, 127, 127])
+image_std = 128.0
+iou_threshold = 0.3
+center_variance = 0.1
+size_variance = 0.2
+
+min_boxes = [[10, 16, 24], [32, 48], [64, 96], [128, 192, 256]]
+shrinkage_list = []
+image_size = [320, 240]  # default input size 320*240
+feature_map_w_h_list = [[40, 20, 10, 5], [
+    30, 15, 8, 4]]  # default feature map size
+priors = []
+
+
+def vis(img, boxes, scores, color, conf=0.5):
+    for i in range(len(boxes)):
+        box = boxes[i]
+        score = scores[i]
+        if score < conf:
+            continue
+        x0 = int(box[0])
+        y0 = int(box[1])
+        x1 = int(box[2])
+        y1 = int(box[3])
+        text = '{:.1f}%'.format(score * 100)
+        txt_color = (255, 255, 255)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+
+        txt_size = cv2.getTextSize(text, font, 0.4, 1)[0]
+        cv2.rectangle(img, (x0, y0), (x1, y1), color, 2)
+
+        txt_bk_color = color
+        cv2.rectangle(img, (x0, y0 + 1), (x0 + txt_size[0] + 1, y0 + int(1.5 * txt_size[1])),
+                      txt_bk_color, -1)
+        cv2.putText(
+            img, text, (x0, y0 + txt_size[1]), font, 0.4, txt_color, thickness=1)
+    return img
+
+
+def generate_priors(feature_map_list, shrinkage_list, image_size, min_boxes, clamp=True):
+    priors = []
+    for index in range(0, len(feature_map_list[0])):
+        scale_w = image_size[0] / shrinkage_list[0][index]
+        scale_h = image_size[1] / shrinkage_list[1][index]
+        for j in range(0, feature_map_list[1][index]):
+            for i in range(0, feature_map_list[0][index]):
+                x_center = (i + 0.5) / scale_w
+                y_center = (j + 0.5) / scale_h
+
+                for min_box in min_boxes[index]:
+                    w = min_box / image_size[0]
+                    h = min_box / image_size[1]
+                    priors.append([x_center, y_center, w, h])
+    print("priors nums:{}".format(len(priors)))
+    priors = np.array(priors)
+    if clamp:
+        np.clip(priors, 0.0, 1.0, out=priors)
+    return priors
+
+
+def define_img_size(size):
+    global image_size, feature_map_w_h_list, priors
+    img_size_dict = {
+        128: [128, 96],
+        160: [160, 120],
+        320: [320, 240],
+        480: [480, 360],
+        640: [640, 480],
+        1280: [1280, 960]
+    }
+    image_size = img_size_dict[size]
+
+    feature_map_w_h_list_dict = {
+        128: [[16, 8, 4, 2], [12, 6, 3, 2]],
+        160: [[20, 10, 5, 3], [15, 8, 4, 2]],
+        320: [[40, 20, 10, 5], [30, 15, 8, 4]],
+        480: [[60, 30, 15, 8], [45, 23, 12, 6]],
+        640: [[80, 40, 20, 10], [60, 30, 15, 8]],
+        1280: [[160, 80, 40, 20], [120, 60, 30, 15]]
+    }
+    feature_map_w_h_list = feature_map_w_h_list_dict[size]
+
+    for i in range(0, len(image_size)):
+        item_list = []
+        for k in range(0, len(feature_map_w_h_list[i])):
+            item_list.append(image_size[i] / feature_map_w_h_list[i][k])
+        shrinkage_list.append(item_list)
+    priors = generate_priors(feature_map_w_h_list,
+                             shrinkage_list, image_size, min_boxes)
+
+
+def convert_locations_to_boxes(locations, priors, center_variance, size_variance):
+    """Convert regressional location results of SSD into boxes in the form of (center_x, center_y, h, w).
+
+    The conversion:
+        $$predicted\_center * center_variance = \frac {real\_center - prior\_center} {prior\_hw}$$
+        $$exp(predicted\_hw * size_variance) = \frac {real\_hw} {prior\_hw}$$
+    We do it in the inverse direction here.
+    Args:
+        locations (batch_size, num_priors, 4): the regression output of SSD. It will contain the outputs as well.
+        priors (num_priors, 4) or (batch_size/1, num_priors, 4): prior boxes.
+        center_variance: a float used to change the scale of center.
+        size_variance: a float used to change of scale of size.
+    Returns:
+        boxes:  priors: [[center_x, center_y, h, w]]. All the values
+            are relative to the image size.
+    """
+    # priors can have one dimension less.
+    if len(priors.shape) + 1 == len(locations.shape):
+        priors = np.expand_dims(priors, 0)
+    return np.concatenate(
+        [
+            locations[..., :2] * center_variance *
+            priors[..., 2:] + priors[..., :2],
+            np.exp(locations[..., 2:] * size_variance) * priors[..., 2:]
+        ],
+        axis=len(locations.shape) - 1,
+    )
+
+
+def center_form_to_corner_form(locations):
+    return np.concatenate(
+        [locations[..., :2] - locations[..., 2:] / 2,
+            locations[..., :2] + locations[..., 2:] / 2],
+        axis=len(locations.shape) - 1,
+    )
+
+
+def area_of(left_top, right_bottom):
+    """
+    Compute the areas of rectangles given two corners.
+    Args:
+        left_top (N, 2): left top corner.
+        right_bottom (N, 2): right bottom corner.
+    Returns:
+        area (N): return the area.
+    """
+    hw = np.clip(right_bottom - left_top, 0.0, None)
+    return hw[..., 0] * hw[..., 1]
+
+
+def iou_of(boxes0, boxes1, eps=1e-5):
+    """
+    Return intersection-over-union (Jaccard index) of boxes.
+    Args:
+        boxes0 (N, 4): ground truth boxes.
+        boxes1 (N or 1, 4): predicted boxes.
+        eps: a small number to avoid 0 as denominator.
+    Returns:
+        iou (N): IoU values.
+    """
+    overlap_left_top = np.maximum(boxes0[..., :2], boxes1[..., :2])
+    overlap_right_bottom = np.minimum(boxes0[..., 2:], boxes1[..., 2:])
+
+    overlap_area = area_of(overlap_left_top, overlap_right_bottom)
+    area0 = area_of(boxes0[..., :2], boxes0[..., 2:])
+    area1 = area_of(boxes1[..., :2], boxes1[..., 2:])
+    return overlap_area / (area0 + area1 - overlap_area + eps)
+
+
+def hard_nms(box_scores, iou_threshold, top_k=-1, candidate_size=200):
+    """
+    Perform hard non-maximum-supression to filter out boxes with iou greater
+    than threshold
+    Args:
+        box_scores (N, 5): boxes in corner-form and probabilities.
+        iou_threshold: intersection over union threshold.
+        top_k: keep top_k results. If k <= 0, keep all the results.
+        candidate_size: only consider the candidates with the highest scores.
+    Returns:
+        picked: a list of indexes of the kept boxes
+    """
+    scores = box_scores[:, -1]
+    boxes = box_scores[:, :-1]
+    picked = []
+    indexes = np.argsort(scores)
+    indexes = indexes[-candidate_size:]
+    while len(indexes) > 0:
+        current = indexes[-1]
+        picked.append(current)
+        if 0 < top_k == len(picked) or len(indexes) == 1:
+            break
+        current_box = boxes[current, :]
+        indexes = indexes[:-1]
+        rest_boxes = boxes[indexes, :]
+        iou = iou_of(
+            rest_boxes,
+            np.expand_dims(current_box, axis=0),
+        )
+        indexes = indexes[iou <= iou_threshold]
+
+    return box_scores[picked, :]
+
+
+def predict(width, height, confidences, boxes, prob_threshold, iou_threshold=0.5, top_k=-1):
+    """
+    Select boxes that contain human faces
+    Args:
+        width: original image width
+        height: original image height
+        confidences (N, 2): confidence array
+        boxes (N, 4): boxes array in corner-form
+        iou_threshold: intersection over union threshold.
+        top_k: keep top_k results. If k <= 0, keep all the results.
+    Returns:
+        boxes (k, 4): an array of boxes kept
+        labels (k): an array of labels for each boxes kept
+        probs (k): an array of probabilities for each boxes being in corresponding labels
+    """
+    boxes = boxes[0]
+    confidences = confidences[0]
+    # print(boxes)
+    # print(confidences)
+
+    picked_box_probs = []
+    picked_labels = []
+    for class_index in range(1, confidences.shape[1]):
+        # print(confidences.shape[1])
+        probs = confidences[:, class_index]
+        # print(probs)
+        mask = probs > prob_threshold
+        probs = probs[mask]
+
+        if probs.shape[0] == 0:
+            continue
+        subset_boxes = boxes[mask, :]
+        # print(subset_boxes)
+        box_probs = np.concatenate(
+            [subset_boxes, probs.reshape(-1, 1)], axis=1)
+        box_probs = hard_nms(
+            box_probs,
+            iou_threshold=iou_threshold,
+            top_k=top_k,
+        )
+        picked_box_probs.append(box_probs)
+        picked_labels.extend([class_index] * box_probs.shape[0])
+    if not picked_box_probs:
+        return np.array([]), np.array([]), np.array([])
+    picked_box_probs = np.concatenate(picked_box_probs)
+    picked_box_probs[:, 0] *= width
+    picked_box_probs[:, 1] *= height
+    picked_box_probs[:, 2] *= width
+    picked_box_probs[:, 3] *= height
+    return picked_box_probs[:, :4].astype(np.int32), np.array(picked_labels), picked_box_probs[:, 4]
+
+
+def preprocess(orig_image, input_shape):
+    image = cv2.cvtColor(orig_image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, input_shape)
+    image_mean = np.array([127, 127, 127])
+    image = (image - image_mean) / 128
+    image = np.transpose(image, [2, 0, 1])
+    image = np.expand_dims(image, axis=0)
+    image = image.astype(np.float32)
+    define_img_size(640)
+    return image
+
+
+# scale current rectangle to box
+def scale(box):
+    width = box[2] - box[0]
+    height = box[3] - box[1]
+    maximum = max(width, height)
+    dx = int((maximum - width) / 2)
+    dy = int((maximum - height) / 2)
+
+    bboxes = [box[0] - dx, box[1] - dy, box[2] + dx, box[3] + dy]
+    return bboxes
+
+
+# crop image
+def cropImage(image, box):
+    num = image[box[1]:box[3], box[0]:box[2]]
+    return num
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Inference inception_v3 network.')
+    parser.add_argument("--model", type=str, required=True,
+                        help="Model definition file")
+    parser.add_argument("--model_data", type=str,
+                        help="Caffemodel data, only for caffe model")
+    parser.add_argument("--net_input_dims", type=str,
+                        default="640,480", help="(h,w) of net input")
+    parser.add_argument("--input", type=str, required=True,
+                        help="Input image for testing")
+    parser.add_argument("--output",
+                        type=str,
+                        required=True,
+                        help="Output image after classification")
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    input_shape = tuple(map(int, args.net_input_dims.split(',')))
+
+    origin_image = cv2.imread(args.input)
+    color = (255, 128, 0)
+    image_data = preprocess(origin_image, input_shape)
+    data = {"input": image_data}  # input name from model
+
+    output = dict()
+    if args.model.endswith('.onnx'):
+        output = onnx_inference(data, args.model, False)
+        confidences = output['scores']
+        boxes = output['boxes']
+
+        boxes, label, probs = predict(origin_image.shape[1], origin_image.shape[0], confidences,
+                                      boxes, 0.7)
+
+    else:
+        if args.model.endswith('.mlir'):
+            output = mlir_inference(data, args.model, False)
+        elif args.model.endswith(".bmodel"):
+            output = model_inference(data, args.model)
+        else:
+            raise RuntimeError("not support modle file:{}".format(args.model))
+
+        confidences = output['scores_Softmax']
+        boxes = output['460_Concat']
+        boxes = convert_locations_to_boxes(
+            boxes, priors, center_variance, size_variance)
+        boxes = center_form_to_corner_form(boxes)
+
+        boxes, label, probs = predict(origin_image.shape[1], origin_image.shape[0], confidences,
+                                      boxes, 0.7)
+
+    # for i in range(boxes.shape[0]):
+    #     box = scale(boxes[i, :])
+    #     cv2.rectangle(
+    #         origin_image, (box[0], box[1]), (box[2], box[3]), color, 4)
+    # cv2.imwrite(args.output, origin_image)
+
+    if boxes is not None:
+        fix_img = vis(origin_image,
+                      boxes,
+                      probs,
+                      color)
+        cv2.imwrite(args.output, fix_img)
+    else:
+        raise RuntimeError(
+            "model:[{}] nothing detect out:{}".format(args.model, args.input))
+
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------
+# Main void
+
+if __name__ == '__main__':
+    main()
