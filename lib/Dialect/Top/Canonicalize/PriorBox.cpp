6d7a994bae6a7962e71850dbb099213c2bb98a9b
liang.chen <liang.chen@sophgo.com>
2022-12-14 12:04:54 +0800

    [18xx] add convert in caffe_convert
    1.add DetectionOutputOp、PriorBoxOp
    2.fix bug in concat only_merge
    
    Change-Id: Ifa7ca8a6e0afb9eeabbd11a63404b37c090222d3

---
+++ lib/Dialect/Top/Canonicalize/PriorBox.cpp
@@ -0,0 +1,164 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
+//
+// TPU-MLIR is licensed under the 2-Clause BSD License except for the
+// third-party components.
+//
+//===----------------------------------------------------------------------===//
+
+#include "tpu_mlir/Dialect/Top/IR/TopOps.h"
+
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/Pass/Pass.h"
+#include "tpu_mlir/Support/Helper/Module.h"
+
+using namespace mlir;
+using namespace tpu_mlir::top;
+using namespace tpu_mlir::helper;
+
+struct ConvertPriorBoxPattern : public OpRewritePattern<PriorBoxOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(PriorBoxOp op,
+                                PatternRewriter &rewriter) const override {
+    auto result = op.getResult();
+
+    auto shape = Module::getShape(result);
+    auto size =
+        std::accumulate(shape.begin(), shape.end(), 1, std::multiplies<>());
+    auto resultT = std::vector<float>(size);
+    auto min_size = Module::getF64Array(op.min_size());
+    auto max_size = Module::getF64Array(op.max_size());
+    auto aspect_ratios = Module::getF64Array(op.aspect_ratios());
+    auto variance = Module::getF64Array(op.variance());
+
+    bool clip = op.clip();
+    bool use_default_aspect_ratio = op.use_default_aspect_ratio();
+    double offset = op.offset().convertToDouble();
+    double step_w = op.step_w().convertToDouble();
+    double step_h = op.step_h().convertToDouble();
+    int64_t num_priors = op.num_priors();
+    int64_t img_height = op.img_h();
+    int64_t img_width = op.img_w();
+
+    if (max_size->size() > 0) {
+      assert(max_size->size() == min_size->size() &&
+             "num of max_size should the same with min_size");
+    }
+
+    // Must and only provide 4 variance.
+    assert(variance->size() == 4 && "variance size must be 4");
+
+    auto shape0 = Module::getShape(op.inputs()[0]);
+    auto shape1 = Module::getShape(op.inputs()[1]);
+
+    assert(shape1.size() == 4 && shape0.size() == 4);
+    const int64_t layer_width = shape0[3];
+    const int64_t layer_height = shape0[2];
+
+    if (img_height == 0 || img_width == 0) {
+      img_height = shape1[2];
+      img_width = shape1[3];
+    }
+
+    if (std::abs(step_w) < 1e-6 || std::abs(step_h) < 1e-6) {
+      step_w = static_cast<double>(img_width) / layer_width;
+      step_h = static_cast<double>(img_height) / layer_height;
+    }
+
+    std::vector<float> top_data(size);
+    int64_t dim = layer_height * layer_width * num_priors * 4;
+    int idx = 0;
+    for (int64_t h = 0; h < layer_height; ++h) {
+      for (int64_t w = 0; w < layer_width; ++w) {
+        double center_x = (w + offset) * step_w;
+        double center_y = (h + offset) * step_h;
+        double box_width, box_height;
+        for (size_t s = 0; s < min_size->size(); ++s) {
+          int min_size_ = (int)min_size->at(s);
+          if (use_default_aspect_ratio) {
+            // first prior: aspect_ratio = 1, size = min_size
+            box_width = box_height = min_size_;
+            // xmin
+            top_data[idx++] = (center_x - box_width / 2.) / img_width;
+            // ymin
+            top_data[idx++] = (center_y - box_height / 2.) / img_height;
+            // xmax
+            top_data[idx++] = (center_x + box_width / 2.) / img_width;
+            // ymax
+            top_data[idx++] = (center_y + box_height / 2.) / img_height;
+          }
+
+          if (max_size->size() > 0) {
+            int max_size_ = max_size->at(s);
+            // second prior: aspect_ratio = 1, size = sqrt(min_size * max_size)
+            box_width = box_height = sqrt(min_size_ * max_size_);
+            // xmin
+            top_data[idx++] = (center_x - box_width / 2.) / img_width;
+            // ymin
+            top_data[idx++] = (center_y - box_height / 2.) / img_height;
+            // xmax
+            top_data[idx++] = (center_x + box_width / 2.) / img_width;
+            // ymax
+            top_data[idx++] = (center_y + box_height / 2.) / img_height;
+          }
+
+          // rest of priors
+          for (size_t r = 0; r < aspect_ratios->size(); ++r) {
+            double ar = aspect_ratios->at(r);
+            if (fabs(ar - 1.) < 1e-6) {
+              continue;
+            }
+            box_width = min_size_ * sqrt(ar);
+            box_height = min_size_ / sqrt(ar);
+            // xmin
+            top_data[idx++] = (center_x - box_width / 2.) / img_width;
+            // ymin
+            top_data[idx++] = (center_y - box_height / 2.) / img_height;
+            // xmax
+            top_data[idx++] = (center_x + box_width / 2.) / img_width;
+            // ymax
+            top_data[idx++] = (center_y + box_height / 2.) / img_height;
+          }
+        }
+      }
+    }
+    // clip the prior's coordidate such that it is within [0, 1]
+    if (clip) {
+      for (int d = 0; d < dim; ++d) {
+        top_data[d] = std::min<float>(std::max<float>(top_data[d], 0.), 1.);
+      }
+    }
+
+    auto o_s = Module::getShape(op.output());
+
+    // set the variance.
+    // top_data += (o_s[2]);
+
+    int count = 0;
+    for (int64_t h = 0; h < layer_height; ++h) {
+      for (int64_t w = 0; w < layer_width; ++w) {
+        for (int64_t i = 0; i < num_priors; ++i) {
+          for (auto &v : *variance) {
+            top_data[(o_s[2]) + count] = v;
+            ++count;
+          }
+        }
+      }
+    }
+    auto op_name = Module::getName(op.output());
+    auto weight_name = op_name.str() + "loadweight";
+    auto weight_type =
+        RankedTensorType::get(shape, rewriter.getF32Type());
+    auto weight_operand = WeightOp::create(
+        op, weight_name, top_data, weight_type);
+    rewriter.replaceOp(op, weight_operand);
+    return success();
+  }
+};
+
+void PriorBoxOp::getCanonicalizationPatterns(RewritePatternSet &results,
+                                             MLIRContext *context) {
+  results.insert<ConvertPriorBoxPattern>(context);
+}
---
+++ lib/Dialect/Top/Interfaces/PriorBox.cpp
@@ -0,0 +1,125 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
+//
+// TPU-MLIR is licensed under the 2-Clause BSD License except for the
+// third-party components.
+//
+//===----------------------------------------------------------------------===//
+
+#include "tpu_mlir/Dialect/Top/IR/TopOps.h"
+#include "tpu_mlir/Support/Dnnl/Dnnl.h"
+#include "tpu_mlir/Support/Helper/Module.h"
+#include "tpu_mlir/Support/MathUtils.h"
+
+using namespace tpu_mlir;
+using namespace tpu_mlir::helper;
+using namespace mlir;
+
+int64_t top::PriorBoxOp::getFLOPs() { return Module::getNumElements(output()); }
+
+LogicalResult top::PriorBoxOp::init(InferenceParameter &p) { return success(); }
+void top::PriorBoxOp::deinit(InferenceParameter &p) {}
+
+LogicalResult top::PriorBoxOp::inference(InferenceParameter &p) {
+  #if 0
+  auto input_image_shape = Module::getShape(this->inputs()[1]);
+  assert(input_image_shape.size() == 4 && input_shape.size() == 4);
+
+  int layer_height = input_shape[2];
+  int layer_width = input_shape[3];
+
+  if (img_width == 0 || img_height == 0) {
+    img_height = input_image_shape[2];
+    img_width = input_image_shape[3];
+  }
+
+  if (step_w == 0 || step_h == 0) {
+    step_w = static_cast<float>(img_width) / layer_width;
+    step_h = static_cast<float>(img_height) / layer_height;
+  }
+
+  float *top_data = (float *)output_data->data();
+
+  int dim = layer_height * layer_width * num_priors * 4;
+  int idx = 0;
+  for (int h = 0; h < layer_height; ++h) {
+    for (int w = 0; w < layer_width; ++w) {
+      float center_x = (w + offset) * step_w;
+      float center_y = (h + offset) * step_h;
+      float box_width, box_height;
+      for (size_t s = 0; s < min_size.size(); ++s) {
+        int min_size_ = min_size[s];
+        if (use_default_aspect_ratio) {
+          // first prior: aspect_ratio = 1, size = min_size
+          box_width = box_height = min_size_;
+          // xmin
+          top_data[idx++] = (center_x - box_width / 2.) / img_width;
+          // ymin
+          top_data[idx++] = (center_y - box_height / 2.) / img_height;
+          // xmax
+          top_data[idx++] = (center_x + box_width / 2.) / img_width;
+          // ymax
+          top_data[idx++] = (center_y + box_height / 2.) / img_height;
+        }
+
+        if (max_size.size() > 0) {
+          int max_size_ = max_size[s];
+          // second prior: aspect_ratio = 1, size = sqrt(min_size * max_size)
+          box_width = box_height = sqrt(min_size_ * max_size_);
+          // xmin
+          top_data[idx++] = (center_x - box_width / 2.) / img_width;
+          // ymin
+          top_data[idx++] = (center_y - box_height / 2.) / img_height;
+          // xmax
+          top_data[idx++] = (center_x + box_width / 2.) / img_width;
+          // ymax
+          top_data[idx++] = (center_y + box_height / 2.) / img_height;
+        }
+
+        // rest of priors
+        for (size_t r = 0; r < aspect_ratios.size(); ++r) {
+          float ar = aspect_ratios[r];
+          if (fabs(ar - 1.) < 1e-6) {
+            continue;
+          }
+          box_width = min_size_ * sqrt(ar);
+          box_height = min_size_ / sqrt(ar);
+          // xmin
+          top_data[idx++] = (center_x - box_width / 2.) / img_width;
+          // ymin
+          top_data[idx++] = (center_y - box_height / 2.) / img_height;
+          // xmax
+          top_data[idx++] = (center_x + box_width / 2.) / img_width;
+          // ymax
+          top_data[idx++] = (center_y + box_height / 2.) / img_height;
+        }
+      }
+    }
+  }
+  // clip the prior's coordidate such that it is within [0, 1]
+  if (clip) {
+    for (int d = 0; d < dim; ++d) {
+      top_data[d] = std::min<float>(std::max<float>(top_data[d], 0.), 1.);
+    }
+  }
+
+  std::vector<int64_t> o_s = shape;
+
+  // set the variance.
+  top_data += (o_s[2]);
+
+  int count = 0;
+  for (int h = 0; h < layer_height; ++h) {
+    for (int w = 0; w < layer_width; ++w) {
+      for (int i = 0; i < num_priors; ++i) {
+        for (int j = 0; j < 4; ++j) {
+          top_data[count] = variance[j];
+          ++count;
+        }
+      }
+    }
+  }
+  #endif
+  return success();
+}
