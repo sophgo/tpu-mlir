598155ecbf559245436c794caa3e39db259954f9
pengchao.hu <pengchao.hu@sophgo.com>
2022-12-16 05:58:46 +0800

    refine common lowering
    Change-Id: I38fa6f78f18a29c1b2daa1fcd17c4fb1e2b31434

--- lib/Conversion/TopToTpu/BM1684X/Reduce.cpp
+++ lib/Conversion/TopToTpu/BM1684X/Reduce.cpp
@@ -12,26 +12,9 @@
 namespace tpu_mlir {
 namespace bm1684x {
 
-static void LoweringReduce(PatternRewriter &rewriter, top::ReduceOp op,
-                           Type type) {
-  rewriter.setInsertionPointAfter(op);
-  std::vector<Value> operands;
-  operands.push_back(op.input());
-  operands.push_back(Module::getNoneOp(op));
-  operands.push_back(Module::getNoneOp(op));
-  mlir::Type out_type = op.output().getType();
-  if (type.isF16()) {
-    out_type = getQuantF16Type(op.output());
-  } else if (type.isBF16()) {
-    out_type = getQuantBF16Type(op.output());
-  }
-  rewriter.replaceOpWithNewOp<tpu::ReduceOp>(op, out_type, operands,
-                                             op.getOperation()->getAttrs());
-}
-
 void ReduceLowering::LoweringF32(PatternRewriter &rewriter,
                                  top::ReduceOp op) const {
-  LoweringReduce(rewriter, op, rewriter.getF32Type());
+  lowering_common_f32<tpu::ReduceOp>(rewriter, op, 3);
 }
 
 void ReduceLowering::LoweringINT8(PatternRewriter &rewriter, top::ReduceOp op,
@@ -41,12 +24,12 @@ void ReduceLowering::LoweringINT8(PatternRewriter &rewriter, top::ReduceOp op,
 
 void ReduceLowering::LoweringBF16(PatternRewriter &rewriter,
                                   top::ReduceOp op) const {
-  LoweringReduce(rewriter, op, rewriter.getBF16Type());
+  lowering_common_bf16<tpu::ReduceOp>(rewriter, op, 3);
 }
 
 void ReduceLowering::LoweringF16(PatternRewriter &rewriter,
                                  top::ReduceOp op) const {
-  LoweringReduce(rewriter, op, rewriter.getF16Type());
+  lowering_common_f16<tpu::ReduceOp>(rewriter, op, 3);
 }
 
 void ReduceLowering::LoweringQuantized(PatternRewriter &rewriter,
--- lib/Conversion/TopToTpu/BM1684X/Softmax.cpp
+++ lib/Conversion/TopToTpu/BM1684X/Softmax.cpp
@@ -14,14 +14,7 @@ namespace bm1684x {
 
 void SoftmaxLowering::LoweringF32(PatternRewriter &rewriter,
                                   top::SoftmaxOp op) const {
-  std::vector<Value> operands;
-  operands.push_back(op.input());
-  auto none = Module::getNoneOp(op);
-  for (int i = 0; i < 4; i++) {
-    operands.push_back(none);
-  }
-  rewriter.replaceOpWithNewOp<tpu::SoftmaxOp>(op, op.output().getType(),
-                                              operands, op->getAttrs());
+  lowering_common_f32<tpu::SoftmaxOp>(rewriter, op, 5);
 }
 
 void SoftmaxLowering::LoweringINT8(PatternRewriter &rewriter, top::SoftmaxOp op,
@@ -75,7 +68,8 @@ void SoftmaxLowering::LoweringQuantized(PatternRewriter &rewriter,
   std::vector<NamedAttribute> attrs;
   for (auto &attr : op->getAttrs()) {
     if (attr.getName() == "axis" && op.axis() != 1) {
-      attrs.push_back(rewriter.getNamedAttr("axis", rewriter.getI64IntegerAttr(1)));
+      attrs.push_back(
+          rewriter.getNamedAttr("axis", rewriter.getI64IntegerAttr(1)));
     } else
       attrs.push_back(attr);
   }
@@ -83,33 +77,32 @@ void SoftmaxLowering::LoweringQuantized(PatternRewriter &rewriter,
     rewriter.replaceOpWithNewOp<tpu::SoftmaxOp>(
         op, op.output().getType(),
         ValueRange{op.input(), table_opd, Module::getNoneOp(op.getOperation()),
-                  Module::getNoneOp(op.getOperation()),
-                  Module::getNoneOp(op.getOperation())},
+                   Module::getNoneOp(op.getOperation()),
+                   Module::getNoneOp(op.getOperation())},
         attrs);
   } else {
     // transpose
-    std::string new_name =
-        Module::getName(op.input()).str() + "__transpose";
+    std::string new_name = Module::getName(op.input()).str() + "__transpose";
     auto name_loc = NameLoc::get(rewriter.getStringAttr(new_name));
     int64_t odr[] = {0, 3, 1, 2};
-    std::vector<int64_t> order(odr, odr+4);
+    std::vector<int64_t> order(odr, odr + 4);
     auto to_type = Module::getElementType(op.input());
     auto TransOp = do_transpose(name_loc, op.input(), order);
     // softmax
     rewriter.setInsertionPointAfter(op);
     new_name = (Module::getName(op.output()).str()) + "__softmax";
     name_loc = NameLoc::get(rewriter.getStringAttr(new_name));
-    auto newType = RankedTensorType::get(
-      Module::getShape(TransOp), Module::getElementType(op.output()));
-    auto newOp =
-        rewriter.create<tpu::SoftmaxOp>(name_loc, newType,
+    auto newType = RankedTensorType::get(Module::getShape(TransOp),
+                                         Module::getElementType(op.output()));
+    auto newOp = rewriter.create<tpu::SoftmaxOp>(
+        name_loc, newType,
         ValueRange{TransOp, table_opd, Module::getNoneOp(op.getOperation()),
-                 Module::getNoneOp(op.getOperation()),
-                 Module::getNoneOp(op.getOperation())},
+                   Module::getNoneOp(op.getOperation()),
+                   Module::getNoneOp(op.getOperation())},
         attrs);
     // transpose
     int64_t odr1[] = {0, 2, 3, 1};
-    std::vector<int64_t> order1(odr1, odr1+4);
+    std::vector<int64_t> order1(odr1, odr1 + 4);
     auto v = do_transpose(op->getLoc(), newOp, order1);
     rewriter.replaceOp(op, {v});
   }
