3bed2a4568bc76b80ff0c2486e919359d86e5cba
pengchao.hu <pengchao.hu@sophgo.com>
2023-01-18 20:37:42 +0800

    support BM1684 AvgPool2D f32/int8 global
    test by: test_onnnx.py --chip bm1684 --case AvgPool2D
    
    Change-Id: Ibc712fd9ba8c9de849e8705822aa0ddb17efae6c

--- lib/Dialect/Tpu/Interfaces/BM1684/Pooling2D.cpp
+++ lib/Dialect/Tpu/Interfaces/BM1684/Pooling2D.cpp
@@ -13,19 +13,31 @@
 #include "tpu_mlir/Support/Module.h"
 #include "tpu_mlir/Support/Dnnl/Pool.h"
 
-
-
 using namespace tpu_mlir::backend;
 
 void tpu::Pool2DOp::codegen_global_bm1684() {
   auto attr = parseParam();
   bool is_avg_pooling = getPoolMode() == tpu::PoolMode::Avg;
-  BM1684::instance().dl_nodechip_pooling_fix8b_forward_parallel_with_data_split(
-      module::getAddress(getInput()), module::getAddress(getOutput()), attr.n, attr.c,
-      attr.ih, attr.iw, attr.kh, attr.kw, attr.pad_h, attr.pad_h_after,
-      attr.pad_w, attr.pad_w_after, attr.sh, attr.sw, 0, 0, is_avg_pooling, 0,
-      0, 0, 0, 1, 0, 0, 1, attr.do_relu ? 1 : 0,
-      (CMD_ID_NODE *)BM1684::instance().cmdid_node);
+  auto in_addr = module::getAddress(getInput());
+  auto out_addr = module::getAddress(getOutput());
+  if (module::isUniformQuantized(getInput())) {
+    auto in_sign = module::isSign(getInput());
+    auto out_sign = module::isSign(getOutput());
+    BM1684::instance()
+        .dl_nodechip_pooling_fix8b_forward_parallel_with_data_split(
+            in_addr, out_addr, attr.n, attr.c, attr.ih, attr.iw, attr.kh,
+            attr.kw, attr.pad_h, attr.pad_h_after, attr.pad_w, attr.pad_w_after,
+            attr.sh, attr.sw, 0, 0, is_avg_pooling ? 1 : 0, 0, 0, 0, 0, in_sign,
+            0, 0, out_sign, attr.do_relu ? 1 : 0,
+            (CMD_ID_NODE *)BM1684::instance().cmdid_node);
+  } else {
+    // F32
+    BM1684::instance().dl_nodechip_pooling_forward_parallel_with_data_split(
+        in_addr, out_addr, attr.n, attr.c, attr.ih, attr.iw, attr.oh, attr.ow,
+        attr.kh, attr.kw, attr.pad_h, attr.pad_w, attr.sh, attr.sw,
+        is_avg_pooling ? 1 : 0, attr.count_include_pad ? 0 : 1,
+        attr.do_relu ? 1 : 0, (CMD_ID_NODE *)BM1684::instance().cmdid_node);
+  }
 }
 
 int64_t tpu::Pool2DOp::getBufferSize_bm1684(
