9d2ee606750b660bd0041a0cbb0a3466934989bc
HOU Yu <yu.hou@sophgo.com>
2023-01-19 16:20:59 +0800

    [bm1684x] add addConst for bm1684x
    but is op is not supported in onnx and in fact not tested
    
    Change-Id: I2f4a643971a81dbe3067569eb1da27075d4a910b

--- lib/Conversion/TopToTpu/BM1684X/AddConst.cpp
+++ lib/Conversion/TopToTpu/BM1684X/AddConst.cpp
@@ -19,7 +19,41 @@ void AddConstLowering::LoweringF32(PatternRewriter &rewriter,
 
 void AddConstLowering::LoweringINT8(PatternRewriter &rewriter,
                                     top::AddConstOp op, bool asymmetric) const {
-  LoweringF16(rewriter, op);
+  if (asymmetric)
+    return LoweringF16(rewriter, op);
+
+  auto in = op.getInput();
+  auto out = op.getOutput();
+  double in_scale, out_scale;
+
+  int64_t in_zp, out_zp;
+  module::getScaleAndZeroPoint(in, in_scale, in_zp, asymmetric);
+  module::getScaleAndZeroPoint(out, out_scale, out_zp, asymmetric);
+  int multiplier, rshift;
+  get_scale_and_shift_positive(in_scale / out_scale, multiplier, rshift, 8);
+
+  double const_b = op.getConstVal().convertToDouble();
+  const_b = static_cast<int>(round(const_b / out_scale)) << rshift;
+
+  std::vector<NamedAttribute> attrs;
+  for (auto &attr : op->getAttrs()) {
+    if (attr.getName() == "const_val") {
+      attrs.push_back(rewriter.getNamedAttr("const_val",
+                                            rewriter.getF64FloatAttr(const_b)));
+    } else {
+      attrs.push_back(attr);
+    }
+  }
+
+  attrs.push_back(
+    rewriter.getNamedAttr("multiplier", rewriter.getSI32IntegerAttr(multiplier)));
+  attrs.push_back(
+    rewriter.getNamedAttr("rshift", rewriter.getI64IntegerAttr(rshift)));
+
+
+  auto newType = getQuantInt8Type(op.getOutput(), asymmetric);
+  rewriter.replaceOpWithNewOp<tpu::AddConstOp>(op, newType, op.getInput(),
+                                               attrs);
 }
 
 void AddConstLowering::LoweringINT4(PatternRewriter &rewriter,
--- lib/Dialect/Tpu/Interfaces/Common/AddConst.cpp
+++ lib/Dialect/Tpu/Interfaces/Common/AddConst.cpp
@@ -10,9 +10,9 @@
 #include "tpu_mlir/Dialect/Tpu/IR/TpuOps.h"
 #include "tpu_mlir/Support/Dnnl/Dnnl.h"
 
-#include "tpu_mlir/Support/Module.h"
-#include "tpu_mlir/Support/MathUtils.h"
 #include "tpu_mlir/Support/Float16.h"
+#include "tpu_mlir/Support/MathUtils.h"
+#include "tpu_mlir/Support/Module.h"
 
 LogicalResult tpu::AddConstOp::init(InferenceParameter &p) { return success(); }
 
@@ -22,15 +22,15 @@ LogicalResult tpu::AddConstOp::inference(InferenceParameter &p) {
   auto num_elem = module::getNumElements(getOutput());
   auto out_type = module::getStorageType(getOutput());
   auto asym = module::isAsymmetric();
-#pragma omp parallel for schedule(static, omp_schedule(num_elem))
-  for (int64_t i = 0; i < num_elem; i++) {
-    p.outputs[0][i] = p.inputs[0][i] + getConstVal().convertToDouble();
-  }
-  if (getDoRelu()) {
-    auto limit = getReluLimit().convertToDouble();
-    function_relu(p.outputs[0], p.outputs[0], num_elem, limit);
-  }
   if (out_type.isa<FloatType>()) {
+#pragma omp parallel for schedule(static, omp_schedule(num_elem))
+    for (int64_t i = 0; i < num_elem; i++) {
+      p.outputs[0][i] = p.inputs[0][i] + getConstVal().convertToDouble();
+    }
+    if (getDoRelu()) {
+      auto limit = getReluLimit().convertToDouble();
+      function_relu(p.outputs[0], p.outputs[0], num_elem, limit);
+    }
     if (getDoRelu()) {
       auto limit = getReluLimit().convertToDouble();
       function_relu(p.outputs[0], p.outputs[0], num_elem, limit);
@@ -41,27 +41,28 @@ LogicalResult tpu::AddConstOp::inference(InferenceParameter &p) {
       F16(p.outputs[0], p.outputs[0], num_elem);
     }
   } else if (module::isUniformQuantized(getOutput())) {
-    if (asym == false) {
+    auto i_qtype = module::getUniformQuantizedType(getInput());
+    auto izp = i_qtype.getZeroPoint();
+    auto out_qtype = module::getUniformQuantizedType(getOutput());
+    double ozp = 0;
+    auto out_type = module::getStorageType(getOutput());
+    if (asym == true)
+      ozp = out_qtype.getZeroPoint();
 #pragma omp parallel for schedule(static, omp_schedule(num_elem))
-      for (int i = 0; i < num_elem; i++) {
-        // coeff has been merge in multiplier&&rshift
-        double sum = applyMultiplierAndRShift(p.outputs[0][i], getMultiplier(),
-                                              getRshift());
-        if (getDoRelu() && sum < 0) {
-          sum = 0;
-        }
-        p.outputs[0][i] = saturate(sum, out_type);
+    for (int i = 0; i < num_elem; i++) {
+      double sum = p.inputs[0][i] - izp;
+      sum = applyMultiplierAndRShift(sum, getMultiplier(), 0);
+      sum += getConstVal().convertToDouble();
+      if (getDoRelu() && sum < 0) {
+        sum = 0;
       }
-    } else {
-#pragma omp parallel for schedule(static, omp_schedule(num_elem))
-      for (int i = 0; i < num_elem; i++) {
-        // inputs has been requant
-        double sum = p.outputs[0][i];
-        if (getDoRelu() && sum < 0) {
-          sum = 0;
-        }
-        p.outputs[0][i] = saturate(sum, out_type);
-      }
+      p.outputs[0][i] = applyMultiplierAndRShift(sum, 1, getRshift());
+      if (out_type.isSignedInteger(8))
+        p.outputs[0][i] = to_int8(p.outputs[0][i]);
+      else if (out_type.isUnsignedInteger(8))
+        p.outputs[0][i] = to_uint8(p.outputs[0][i]);
+      else
+        llvm_unreachable("not support type");
     }
   }
   return success();
