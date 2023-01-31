fdd13575733844dd85b41899e4ce767004983032
jone.zhang <jone.zhang@sophgo.com>
2023-01-31 16:39:21 +0800

    add tpulang interface
    Change-Id: I41fd7ccba67efc2a9231ba17acc9978f5f5c3922

---
+++ python/transform/TpuLang.py
@@ -0,0 +1,129 @@
+# Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
+#
+# TPU-MLIR is licensed under the 2-Clause BSD License except for the
+# third-party components.
+#
+# ==============================================================================
+
+from typing import List
+from .MLIRImporter import Top
+from .TpuLangConverter import TpuLangConverter, Graph, Tensor, Operator
+
+from utils.mlir_shell import *
+
+import numpy as np
+
+class TpuLang:
+  graph = None
+  device = None
+  def __init__(self,
+               device: str,):
+    device_list = ['cpu', 'bm1684x', 'bm1686', 'cv183x']
+    if device.lower in device_list:
+      self.chip = device
+    else:
+      KeyError('TpuLang: unsupported device.')
+    # self.model_name = model_name
+    TpuLang.graph = Graph()
+
+  @staticmethod
+  def insert_op(op_name:str, inputs:List[Tensor], outputs:List[Tensor], params:dict={}):
+    op = Operator(op_name, params=params, inputs=inputs, outputs=outputs)
+    TpuLang.graph.operators.append(op)
+
+def ArrayAttr(data: list, data_type: str = 'int64'):
+    return [data, data_type, False]
+
+  # data_type must be in ["float32", "float16", "int64" "int32", "int16". "int8", "uint8", "bool"]
+def Attr(data, data_type: str = 'int64'):
+    assert data_type.find("int") >= 0 or data_type in ["float32", "float64", "bool"]
+    return [data, data_type, True]
+
+def conv_v2(input: Tensor, weight: Tensor, bias:Tensor=None, stride=None,
+            dilation=None, pad=None, group=1, input_zp=None, weight_zp=None,
+            out_dtype=None, out_name=None):
+    dilation = [1,1] if dilation is None else dilation
+    stride = [1,1] if stride is None else stride
+    pad = [0,0,0,0] if pad is None else pad
+    def _shape_inference():
+      kh_ext = dilation[0] * (weight.shape[2] - 1) + 1
+      kw_ext = dilation[1] * (weight.shape[3] - 1) + 1
+      oh = (input.shape[2] + pad[0] + pad[1] - kh_ext) // stride[0] + 1
+      ow = (input.shape[3] + pad[2] + pad[3] - kw_ext) // stride[1] + 1
+      return [input.shape[0], weight.shape[0], oh, ow]
+    attr = {
+        "kernel_shape": ArrayAttr(weight.shape[2:]),
+        "strides": ArrayAttr(stride),
+        "dilations": ArrayAttr(dilation),
+        "pads": ArrayAttr(pad),
+        "do_relu": Attr(False, "bool"),
+        "group": Attr(group)
+    }
+    input.quantization(zero_point=input_zp)
+    weight.quantization(zero_point=weight_zp)
+    output = Tensor(_shape_inference(), dtype=out_dtype, name=out_name)
+    inputs = [input, weight, bias]
+    TpuLang.insert_op(Top.ConvOp, inputs=inputs, outputs=[output], params=attr)
+    return output
+
+def requant_fp_to_int(tensor_i: Tensor, scale, offset, requant_mode, out_dtype, out_name=None,
+                      round_mode='half_away_from_zero'):
+    output = Tensor(tensor_i.shape, name=out_name, dtype=out_dtype)
+    output.quantization(scale=scale, zero_point=offset)
+    TpuLang.insert_op("top.Cast", inputs=[tensor_i], outputs=[output])
+    return output
+
+def compile(name: str,
+            inputs: List[Tensor],
+            outputs: List[Tensor],
+            cmp=True,
+            opt=2,
+            dyn=False,
+            profile=False,
+            refs = None):
+    TpuLang.graph.inputs = inputs
+    TpuLang.graph.outputs = outputs
+    # convert to mlir
+    converter = TpuLangConverter(name=name, graph=TpuLang.graph)
+    model_transform(name, converter)
+    model_inference(model_name=name, inputs=inputs)
+    # if cmp:
+      # model_validate(model_name=name, refs=refs)
+
+def model_transform(model_name, converter: TpuLangConverter):
+    mlir_file = model_name + '.mlir'
+    mlir_origin = model_name + '_origin.mlir'
+    converter.generate_mlir(mlir_origin)
+    mlir_opt_for_top(mlir_origin, mlir_file)
+    print("Mlir file generated:{}".format(mlir_file))
+
+def model_inference(model_name, inputs):
+    in_f32_npz = model_name + '_in_f32.npz'
+    mlir_file = model_name + '.mlir'
+    ref_inputs = dict()
+    for tensor in TpuLang.graph.inputs:
+      ref_inputs[tensor.name] = tensor.buffer
+    np.savez(in_f32_npz, **ref_inputs)
+    res_npz = model_name + '_top_outputs.npz'
+    # inference of mlir model
+    from tools.model_runner import mlir_inference, show_fake_cmd
+    show_fake_cmd(in_f32_npz, mlir_file, res_npz)
+    f32_outputs = mlir_inference(ref_inputs, mlir_file)
+    np.savez(res_npz, **f32_outputs)
+
+def model_validate(model_name, refs: dict):
+    ref_outputs = dict()
+    for tensor in TpuLang.graph.outputs:
+      ref_outputs[tensor.name] = refs[tensor.name]
+    ref_npz = model_name + '_ref_outputs.npz'
+    np.savez(ref_npz, **ref_outputs)
+
+    # compare all blobs layer by layers
+    f32_blobs_compare(res_npz, ref_npz, '0.99,0.99')
+
+
+def init(device: str, assist=False, outdir=''):
+  TpuLang(device=device)
+
+def deinit():
+  pass
---
+++ python/transform/TpuLangConverter.py
@@ -0,0 +1,290 @@
+# Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
+#
+# TPU-MLIR is licensed under the 2-Clause BSD License except for the
+# third-party components.
+#
+# ==============================================================================
+
+from typing import Union, Iterable, List
+from .MLIRImporter import MLIRImporter, Top, State
+from .BaseConverter import BaseConverter
+from mlir.ir import *
+from utils.mlir_shell import *
+import numpy as np
+
+def _indent(sOrIt_: Union[str, Iterable], numSpaces: int) -> str:
+    """Indent string"""
+    if sOrIt_ is None:
+        return "None"
+    if isinstance(sOrIt_, str):
+        s = sOrIt_.split("\n")
+        if len(s) == 1:
+            return sOrIt_
+        s = [(numSpaces * " ") + line for line in s]
+        return "\n".join(s)
+    s = "\n".join([line.__repr__() for line in sOrIt_])
+    return _indent(s, numSpaces)
+
+class Tensor:
+  ID = 0
+  def __init__(self, shape, name=None, ttype="neuron", data=None, dtype="float32", is_const=False):
+    self.id = int(Tensor.ID)
+    self.shape = shape if isinstance(shape, list) else [shape]
+    self.name = "BMTensor" + str(self.id) if name is None else name
+    assert ttype.lower() in ["neuron", "coeff"]
+    self.ttype = ttype.lower()
+    assert dtype.lower() in ["float32", "float16", "int32", "uint32", "int16", "uint16", "int8", "uint8"]
+    self.dtype = dtype.lower()
+    self.buffer = data
+    self.is_const = is_const
+    self.is_quantized = False
+    self.quantization()
+    Tensor.ID += 1
+
+  def quantization(self, scale=None, zero_point=None):
+    if self.is_quantized is False:
+      self.is_quantized = scale is not None or zero_point is not None
+      self.scale = scale
+      self.zero_point = zero_point
+    else:
+      if self.scale is None:
+        self.scale = scale
+      elif scale is not None:
+        assert self.scale == scale
+      if self.zero_point is None:
+        self.zero_point = zero_point
+      elif zero_point is not None:
+        assert self.zero_point == zero_point
+
+  def __repr__(self):
+    s = "tensor (\n{modstr}\n)"
+    modstr = [self.id, self.name, self.shape, self.ttype, self.dtype, self.buffer]
+    if self.is_quantized:
+      modstr += [self.scale, self.zero_point]
+    return s.format(modstr=_indent(modstr, 2))
+
+class Operator:
+  def __init__(self, op_name:str, inputs:List[Tensor], outputs:List[Tensor], params:dict={}):
+    self.op_name = op_name
+    self.params = params
+    self.inputs = inputs
+    self.outputs = outputs
+
+  def __repr__(self):
+    s = "{type} (\n{modstr}\n)"
+    modstr = "\n".join(
+        ["inputs (\n{}\n)".format(_indent(self.inputs, 2)),
+         "outputs (\n{}\n)".format(_indent(self.outputs, 2)),]
+    )
+    return s.format(type=self.op_name, modstr=_indent(modstr, 2))
+
+class Graph:
+  def __init__(self, name="main"):
+    self.name = name
+    self.operators: List[Operator] = []
+    self._inputs = None
+    self._outputs = None
+
+  @property
+  def inputs(self):
+    return self._inputs
+
+  @inputs.setter
+  def inputs(self, inputs: List[Tensor]):
+    self._inputs = inputs
+
+  @property
+  def outputs(self):
+    return self._outputs
+
+  @outputs.setter
+  def outputs(self, outputs: List[Tensor]):
+    self._outputs = outputs
+
+  def __repr__(self) -> str:
+    s = "{name} (\n{modstr}\n)"
+    modstr = "\n".join(
+        ["inputs (\n{}\n)".format(_indent(self.inputs, 2))]
+      + ["outputs (\n{}\n)".format(_indent(self.outputs, 2))]
+      + ["body (\n{}\n)".format(_indent(self.operators, 2))]
+    )
+    return s.format(name=self.name, modstr=_indent(modstr, 2))
+
+class TpuLangConverter(BaseConverter):
+  MLIRImporterTypeStr = {
+    "float64": "f64",
+    "float32": "F32",
+    "float16": "F16",
+    "int8": "INT8",
+    "int16": "INT16",
+    "int32": "INT32",
+    "int64": "INT64",
+    "uint8": "UINT8",
+    "uint16": "UINT16",
+    "uint32": "UINT32",
+    "uint64": "UINT64",
+    "bool": "BOOL",
+  }
+
+  def __init__(self,
+               name: str,
+               graph: Graph):
+    super().__init__()
+    self.model_name = name
+    self.model = graph
+    self.load_model()
+    self.init_MLIRImporter()
+    self.weight_file = self.mlir.weight_file
+    self.constant = {}
+    self.type_to_mlir = self.__type2mlir(self.mlir.ctx)
+
+  def __del__(self):
+    if self.mlir != None:
+      del self.mlir
+
+  def load_model(self):
+    self.num_input = len(self.model.inputs)
+    self.input_types = []
+    self.output_types = []
+    for tensor in self.model.inputs:
+      self.input_names.append(tensor.name)
+      self.addShape(tensor.name, tensor.shape)
+      if tensor.is_quantized:
+        self.input_types.append(self.get_quantized_type(tensor))
+      else:
+        self.input_types.append(self.MLIRImporterTypeStr[tensor.dtype])
+    for tensor in self.model.outputs:
+      self.output_names.append(tensor.name)
+      self.addShape(tensor.name, tensor.shape)
+      if tensor.is_quantized:
+        self.output_types.append(self.get_quantized_type(tensor))
+      else:
+        self.output_types.append(self.MLIRImporterTypeStr[tensor.dtype])
+
+  def init_MLIRImporter(self):
+    input_shapes = list()
+    for _name in self.input_names:
+      input_shapes.append(self.getShape(_name))
+    output_shapes = list()
+    for _name in self.output_names:
+      output_shapes.append(self.getShape(_name))
+    # init importer
+    self.mlir = MLIRImporter(input_shapes, output_shapes, self.model_name,
+                             state=State.TOP_F32, do_declare=False)
+    self.mlir.declare_func(self.input_types, self.output_types)
+
+
+  def __create_weight_op(self, tensor:Tensor):
+    # constant variable/op
+    tensor_type = self.__get_tensor_type(tensor)
+    name_loc = Location.name(tensor.name)
+    op = Operation.create(Top.WeightOp, results=[tensor_type], loc=name_loc)
+    self.mlir.insert_point.insert(op)
+    self.constant[tensor.name] = tensor.buffer
+    return op.results[0]
+
+  def attr_to_mlir(self, params: dict):
+    attrs = {}
+    for key, value in params.items():
+      if value[2] is False:
+        attrs[key] = self.mlir.ArrayAttr(value[0], self.MLIRImporterTypeStr[value[1]])
+      elif value[1].find("int") >= 0:
+        attrs[key] = IntegerAttr.get(self.type_to_mlir[value[1]], value[0])
+      elif value[1] is "bool":
+        attrs[key] = BoolAttr.get(value[0])
+      else:
+        attrs[key] = FloatAttr.get(self.type_to_mlir[value[1]], value[0])
+    return attrs
+
+  def __type2mlir(self, mlir_ctx):
+    return {
+        "float64": F64Type.get(mlir_ctx),
+        "float32": F32Type.get(mlir_ctx),
+        "float16": F16Type.get(mlir_ctx),
+        "int64": IntegerType.get_signless(64, mlir_ctx),
+        "int32": IntegerType.get_signless(32, mlir_ctx),
+        "int16": IntegerType.get_signless(16, mlir_ctx),
+        "int8": IntegerType.get_signless(8, mlir_ctx),
+        "uint32": IntegerType.get_unsigned(32, mlir_ctx),
+        "uint16": IntegerType.get_unsigned(16, mlir_ctx),
+        "uint8": IntegerType.get_unsigned(8, mlir_ctx),
+        "bool": IntegerType.get_signless(1, mlir_ctx),
+    }
+
+  def get_quantized_type(self, tensor):
+    # TODO
+    return
+
+  def __get_tensor_type(self, tensor: Tensor):
+    # TODO
+    elem_type = self.type_to_mlir[tensor.dtype]
+    if tensor.shape is not None:
+      return RankedTensorType.get(tensor.shape, elem_type)
+    return UnrankedTensorType.get(elem_type)
+
+  def convert_subgraph(self, subgraph:Graph):
+    class symbolTable:
+      symbol_table = {}
+
+      def __init__(self, gen_value_func):
+        self.gen_value_func = gen_value_func
+
+      def __getitem__(self, tensor:Tensor):
+        if tensor.id not in self.symbol_table:
+          if tensor.buffer is None:
+            raise Exception("Tensor '{}' is not constant!".format(tensor.name))
+          if tuple(tensor.shape) != tuple(tensor.buffer.shape):
+            raise Exception(
+                "Tensor shape is ambiguous! '{t_s}' vs '{b_s}'".format(
+                t_s=tensor.shape, b_s=tensor.buffer.shape
+            )
+          )
+          op = self.gen_value_func(tensor)
+          self.symbol_table[tensor.id] = op
+          return op
+        return self.symbol_table[tensor.id]
+
+      def update(self, other):
+        self.symbol_table.update(other)
+
+    symbol_table = symbolTable(self.__create_weight_op)
+    for idx, input in enumerate(subgraph.inputs):
+      input_op = self.mlir.create_input_op(input.name, idx, **{})
+      symbol_table.update({input.id : input_op})
+
+    def add_operation(operation: Operator):
+      # get operation attributes
+      attributes = self.attr_to_mlir(operation.params)
+      # get input operands
+      operands = []
+      for tensor in operation.inputs:
+        operands.append(symbol_table[tensor] if tensor is not None else self.mlir.none_op)
+      rst_type = [self.__get_tensor_type(x) for x in operation.outputs]
+      name_loc = Location.fused([Location.name(x.name) for x in operation.outputs])
+      op = Operation.create(
+          operation.op_name,
+          results=rst_type,
+          operands=operands,
+          attributes=attributes,
+          loc=name_loc,
+      )
+      self.mlir.insert_point.insert(op)
+      symbol_table.update(dict(zip((x.id for x in operation.outputs), op.results)))
+
+    return_op = []
+    for op in subgraph.operators:
+      add_operation(op)
+      for out in op.outputs:
+        if out.name in self.output_names:
+          return_op.append(symbol_table[out])
+
+    return return_op
+
+  def generate_mlir(self, mlir_file: str):
+    return_op = self.convert_subgraph(self.model)
+    self.mlir.create_return_op(return_op)
+    mlir_txt = self.mlir.print_module()
+    with open(mlir_file, "w") as f:
+      f.write(mlir_txt)
+    np.savez(self.weight_file, **self.constant)
+    print("Save mlir file: {}".format(mlir_file))
