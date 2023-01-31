ef1ad06d30b5c32be73b7b7f9d29426ad7c2c4c2
hemin.lin <hemin.lin@sophgo.com>
2023-01-31 11:32:43 +0800

    [Canonicalize] add some filter
          1.pad_conv/pool
          2.binary(mul, add, compare) to binary_const
          3.mul to scale
          4.div const to mul const
    
    Change-Id: I28fc8e035ae256d9709c949f8d6dca3e5c0c7bbc

---
+++ lib/Dialect/Top/Canonicalize/Compare.cpp
@@ -0,0 +1,52 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
+//
+// TPU-MLIR is licensed under the 2-Clause BSD License except for the
+// third-party components.
+//
+//===----------------------------------------------------------------------===//
+
+#include "mlir/IR/PatternMatch.h"
+#include "mlir/Pass/Pass.h"
+#include "tpu_mlir/Dialect/Top/IR/TopOps.h"
+#include "tpu_mlir/Support/Module.h"
+
+using namespace mlir;
+using namespace tpu_mlir::top;
+
+struct CompareToCompareConst : public OpRewritePattern<CompareOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(CompareOp op,
+                                PatternRewriter &rewriter) const override {
+
+    auto right_shape = op.getRhs().getType().dyn_cast<TensorType>().getShape();
+    int right_elt_num = 1;
+    for (int i = 0; i < right_shape.size(); ++i)
+      right_elt_num *= right_shape[i];
+    if (right_elt_num > 1)
+      return failure();
+
+    std::shared_ptr<std::vector<float>> const_val;
+    if (auto right_op = dyn_cast<WeightOp>(op.getRhs().getDefiningOp())) {
+      const_val = right_op.read<float>();
+    } else {
+      return failure();
+    }
+
+    Type output = op.getOutput().getType();
+    std::vector<NamedAttribute> attrs;
+    attrs.push_back(rewriter.getNamedAttr("mode", op.getModeAttr()));
+    attrs.push_back(rewriter.getNamedAttr(
+        "const_val", rewriter.getF64FloatAttr(const_val->at(0))));
+    attrs.push_back(rewriter.getNamedAttr("inversed", rewriter.getBoolAttr(0)));
+    rewriter.replaceOpWithNewOp<CompareConstOp>(op, output, op.getLhs(), attrs);
+    return success();
+  }
+};
+
+void CompareOp::getCanonicalizationPatterns(RewritePatternSet &results,
+                                            MLIRContext *context) {
+  results.insert<CompareToCompareConst>(context);
+}
