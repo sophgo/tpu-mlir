d87310f78ccbd2b483708c135a5611329945074e
pengchao.hu <pengchao.hu@sophgo.com>
2023-02-01 20:51:07 +0800

    add reshape fuse pass
    test by: test_onnx.py --case ReshapeFuse
    
    Change-Id: Id8442114be66070128b241d1020fe7a5264929e0

--- lib/Dialect/Top/Canonicalize/Reshape.cpp
+++ lib/Dialect/Top/Canonicalize/Reshape.cpp
@@ -8,13 +8,14 @@
 //===----------------------------------------------------------------------===//
 
 #include "tpu_mlir/Dialect/Top/IR/TopOps.h"
-
+#include "tpu_mlir/Support/Module.h"
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Pass/Pass.h"
 
 using namespace mlir;
 using namespace tpu_mlir::top;
 
+// reshape + reshape
 struct TopFuseReshape : public OpRewritePattern<ReshapeOp> {
   using OpRewritePattern::OpRewritePattern;
 
@@ -30,7 +31,82 @@ struct TopFuseReshape : public OpRewritePattern<ReshapeOp> {
   }
 };
 
+// reshape (in == out)
+struct TopFuseReshape2 : public OpRewritePattern<ReshapeOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(ReshapeOp op,
+                                PatternRewriter &rewriter) const override {
+    auto in_op = op.getInput().getDefiningOp();
+    if (!in_op->hasOneUse()) {
+      return failure();
+    }
+    auto shape0 = module::getShape(op.getOutput());
+    auto shape1 = module::getShape(op.getInput());
+    if (shape0 != shape1) {
+      return failure();
+    }
+    op.getOutput().replaceAllUsesWith(op.getInput());
+    op->erase();
+    return success();
+  }
+};
+
+// add + reshape + add + reshape
+struct TopFuseReshape3 : public OpRewritePattern<ReshapeOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(ReshapeOp op,
+                                PatternRewriter &rewriter) const override {
+    auto in = op.getInput();
+    auto add_op = dyn_cast<AddOp>(in.getDefiningOp());
+    if (!(add_op && add_op->hasOneUse() && in.hasOneUse())) {
+      return failure();
+    }
+    if (add_op.getNumOperands() != 2) {
+      return failure();
+    }
+    auto a_in = add_op.getInputs()[0];
+    auto b_in = add_op.getInputs()[1];
+    if (!module::isWeight(b_in)) {
+      return failure();
+    }
+    if (!a_in.hasOneUse()) {
+      return failure();
+    }
+    if (!isa<ReshapeOp>(a_in.getDefiningOp())) {
+      return failure();
+    }
+    std::vector<int64_t> shape0 = module::getShape(op.getInput());
+    std::vector<int64_t> shape1 = module::getShape(op.getOutput());
+    if (shape0.size() != 1 + shape1.size()) {
+      return failure();
+    }
+    if (!std::equal(shape0.begin() + 1, shape0.end(), shape1.begin())) {
+      return failure();
+    }
+    if (shape0[0] != 1) {
+      return failure();
+    }
+    std::vector<int64_t> a_shape = module::getShape(a_in);
+    std::vector<int64_t> b_shape = module::getShape(b_in);
+    if (a_shape[0] != 1 || b_shape[0] != 1) {
+      return failure();
+    }
+    a_shape.erase(a_shape.begin());
+    b_shape.erase(b_shape.begin());
+    shape0.erase(shape0.begin());
+    auto b_type = RankedTensorType::get(b_shape, module::getElementType(b_in));
+    b_in.setType(b_type);
+    auto a_type = RankedTensorType::get(a_shape, module::getElementType(a_in));
+    a_in.setType(a_type);
+    auto in_type = RankedTensorType::get(shape0, module::getElementType(in));
+    in.setType(in_type);
+    return success();
+  }
+};
+
 void ReshapeOp::getCanonicalizationPatterns(RewritePatternSet &results,
                                             MLIRContext *context) {
-  results.insert<TopFuseReshape>(context);
+  results.insert<TopFuseReshape, TopFuseReshape2, TopFuseReshape3>(context);
 }
