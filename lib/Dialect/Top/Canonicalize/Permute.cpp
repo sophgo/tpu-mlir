ae7e3e1ae6bcf5dc1ada9ebfeb14fa98f40791c5
pengchao.hu <pengchao.hu@sophgo.com>
2023-02-02 16:19:57 +0800

    fuse permute + permute
    test by: test_onnx.py --case PermuteFuse
    
    Change-Id: I1382197424dcdc04bdb8a45c6ef5292ea19ee337

--- lib/Dialect/Top/Canonicalize/Permute.cpp
+++ lib/Dialect/Top/Canonicalize/Permute.cpp
@@ -8,13 +8,12 @@
 //===----------------------------------------------------------------------===//
 
 #include "tpu_mlir/Dialect/Top/IR/TopOps.h"
-
+#include "tpu_mlir/Support/MathUtils.h"
 #include "tpu_mlir/Support/Module.h"
 #include "mlir/IR/PatternMatch.h"
 #include "mlir/Pass/Pass.h"
 
 using namespace tpu_mlir::top;
-
 
 struct TopPermuteToPixelShuffle : public OpRewritePattern<PermuteOp> {
   using OpRewritePattern::OpRewritePattern;
@@ -31,7 +30,8 @@ struct TopPermuteToPixelShuffle : public OpRewritePattern<PermuteOp> {
     if (*order != ps) {
       return failure();
     }
-    auto reshape_before = dyn_cast_or_null<ReshapeOp>(op.getInput().getDefiningOp());
+    auto reshape_before =
+        dyn_cast_or_null<ReshapeOp>(op.getInput().getDefiningOp());
     if (!reshape_before) {
       return failure();
     }
@@ -59,14 +59,102 @@ struct TopPermuteToPixelShuffle : public OpRewritePattern<PermuteOp> {
         "block_h", rewriter.getI64IntegerAttr(upscale_factor)));
     attrs.push_back(rewriter.getNamedAttr(
         "block_w", rewriter.getI64IntegerAttr(upscale_factor)));
-    rewriter.replaceOpWithNewOp<Depth2SpaceOp>(reshape_after, reshape_after.getResult().getType(), ValueRange{reshape_before.getInput()}, attrs);
+    rewriter.replaceOpWithNewOp<Depth2SpaceOp>(
+        reshape_after, reshape_after.getResult().getType(),
+        ValueRange{reshape_before.getInput()}, attrs);
     rewriter.eraseOp(op);
     rewriter.eraseOp(reshape_before);
     return success();
   }
 };
 
+// permute + permute or permute + reshape + permute
+struct PermuteFuse : public OpRewritePattern<PermuteOp> {
+  using OpRewritePattern::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(PermuteOp op,
+                                PatternRewriter &rewriter) const override {
+    auto in = op.getInput();
+    if (in.hasOneUse() == false) {
+      return failure();
+    }
+    if (auto rop = dyn_cast<ReshapeOp>(in.getDefiningOp())) {
+      in = rop.getInput();
+      if (in.hasOneUse() == false) {
+        return failure();
+      }
+    }
+    auto permute_op = dyn_cast<PermuteOp>(in.getDefiningOp());
+    if (!permute_op) {
+      return failure();
+    }
+    // op order
+    std::vector<int64_t> in0_shape = module::getShape(permute_op.getInput());
+    auto in0_order = module::getI64Array(permute_op.getOrder());
+    std::vector<int64_t> in1_shape = module::getShape(op.getInput());
+    auto in1_order = module::getI64Array(op.getOrder());
+    std::vector<int64_t> out1_shape = module::getShape(op.getOutput());
+    std::vector<int64_t> in0_shape_fix;
+    std::vector<int64_t> in0_order_fix;
+    std::vector<int64_t> out0_shape_fix;
+    std::vector<int64_t> in1_shape_fix;
+    std::vector<int64_t> in1_order_fix;
+    int to_dim;
+    for (to_dim = 2; to_dim <= 5; to_dim++) {
+      auto ret = permute_reset(in0_shape, *in0_order, in0_shape_fix,
+                               in0_order_fix, to_dim);
+      if (ret == false) {
+        continue;
+      }
+      ret = permute_reset(in1_shape, *in1_order, in1_shape_fix, in1_order_fix,
+                          to_dim);
+      if (ret == false) {
+        continue;
+      }
+      break;
+    }
+    if (to_dim > 5) {
+      return failure();
+    }
+    for (auto o : in0_order_fix) {
+      out0_shape_fix.push_back(in0_shape_fix[o]);
+    }
+    if (in1_shape_fix != out0_shape_fix) {
+      return failure();
+    }
+    // test
+    std::vector<int64_t> origin_data;
+    for (int64_t i = 0; i < to_dim; i++) {
+      origin_data.push_back(i);
+    }
+    std::vector<int64_t> result0_data;
+    for (auto o : in0_order_fix) {
+      result0_data.push_back(origin_data[o]);
+    }
+    std::vector<int64_t> result1_data;
+    for (auto o : in1_order_fix) {
+      result1_data.push_back(result0_data[o]);
+    }
+    if (result1_data != origin_data) {
+      return failure();
+    }
+    // bingoo !
+    if (out1_shape == in0_shape) {
+      op.getOutput().replaceAllUsesWith(permute_op.getInput());
+    } else {
+      std::string in_name =
+          module::getName(permute_op.getInput()).str() + "_Reshape";
+      auto loc = NameLoc::get(rewriter.getStringAttr(in_name));
+      rewriter.setInsertionPoint(op);
+      auto rs_op = rewriter.create<ReshapeOp>(
+          loc, op.getOutput().getType(), ValueRange{permute_op.getInput()});
+      op.getOutput().replaceAllUsesWith(rs_op.getOutput());
+    }
+    return success();
+  }
+};
+
 void PermuteOp::getCanonicalizationPatterns(RewritePatternSet &results,
                                             MLIRContext *context) {
-  results.insert<TopPermuteToPixelShuffle>(context);
+  results.insert<TopPermuteToPixelShuffle, PermuteFuse>(context);
 }
