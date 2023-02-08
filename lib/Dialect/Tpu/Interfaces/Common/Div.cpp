0101502f03a02724d88938a8c4946df79e5a3523
pengchao.hu <pengchao.hu@sophgo.com>
2023-02-08 09:53:46 +0800

    add channel norm case
    Change-Id: I3cdd8c5ac23d9ea47e87b17615579bfced1d9944

--- lib/Dialect/Tpu/Interfaces/Common/Div.cpp
+++ lib/Dialect/Tpu/Interfaces/Common/Div.cpp
@@ -9,38 +9,47 @@
 
 #include "tpu_mlir/Dialect/Tpu/IR/TpuOps.h"
 #include "tpu_mlir/Support/Dnnl/Dnnl.h"
-
 #include "tpu_mlir/Support/Module.h"
 #include "tpu_mlir/Support/Float16.h"
 #include "tpu_mlir/Support/MathUtils.h"
 
-
-
 LogicalResult tpu::DivOp::init(InferenceParameter &p) {
+  auto binary = new Binary();
+  (*binary)
+      .lhs(p.inputs[0], module::getShape(getInputs()[0]))
+      .rhs(p.inputs[1], module::getShape(getInputs()[1]))
+      .dst(p.outputs[0], module::getShape(getOutput()))
+      .do_relu(getDoRelu())
+      .relu_limit(getReluLimit().convertToDouble())
+      .algorithem(algorithm::binary_div)
+      .setup();
+
+  p.handle = (void *)binary;
+
   return success();
 }
 
-void tpu::DivOp::deinit(InferenceParameter &p) {}
+void tpu::DivOp::deinit(InferenceParameter &p) {
+  if (p.handle != nullptr) {
+    auto binary = (Binary *)p.handle;
+    delete binary;
+    p.handle = nullptr;
+  }
+}
 
 LogicalResult tpu::DivOp::inference(InferenceParameter &p) {
-  auto num_elem = module::getNumElements(getOutput());
-  int lhs_num_elem = module::getNumElements(getInputs()[0]);
-  int rhs_num_elem = module::getNumElements(getInputs()[1]);
-  int c_loop = lhs_num_elem / rhs_num_elem;
-#pragma omp parallel for schedule(static, omp_schedule(num_elem))
-  for (int64_t i = 0; i < num_elem; i++) {
-    try {
-      if (p.inputs[1][(c_loop == 1) ? i : i % rhs_num_elem] == 0) {
-        throw "division by zero";
-      }
-      p.outputs[0][i] = p.inputs[0][i] / p.inputs[1][(c_loop == 1) ? i : i % rhs_num_elem];
-    }catch (const char* msg) {
-     std::cerr << msg << std::endl;
-    }
+  if (p.handle == nullptr) {
+    return failure();
   }
-  if (getDoRelu()) {
-    auto limit = getReluLimit().convertToDouble();
-    function_relu(p.outputs[0], p.outputs[0], num_elem, limit);
+  auto binary = (Binary *)p.handle;
+  binary->run();
+  auto out_type = module::getStorageType(getOutput());
+  auto dst = p.outputs[0];
+  auto num_elements = module::getNumElements(getOutput());
+  if (out_type.isF16()) {
+    F16(dst, dst, num_elements);
+  } else if (out_type.isBF16()) {
+    BF16(dst, dst, num_elements);
   }
   return success();
 }
