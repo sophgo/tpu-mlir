784d4827c7c256536b3e21ffddfa3ff3fa4d92a4
pengchao.hu <pengchao.hu@sophgo.com>
2023-02-08 21:26:04 +0800

    layernorm f16 support
    lowering to f16 if in f16 range, or to f32
    
    Change-Id: I14f983f3e2e9c86ac0b7f9da2d289104eff59131

--- lib/Conversion/TopToTpu/BM1684X/LayerNorm.cpp
+++ lib/Conversion/TopToTpu/BM1684X/LayerNorm.cpp
@@ -12,7 +12,8 @@
 namespace tpu_mlir {
 namespace bm1684x {
 
-static void LoweringLayerNorm(PatternRewriter &rewriter, top::LayerNormOp op, Type type) {
+static void LoweringLayerNorm(PatternRewriter &rewriter, top::LayerNormOp op,
+                              Type type) {
   rewriter.setInsertionPointAfter(op);
   std::vector<Value> opds;
   opds.reserve(3);
@@ -41,7 +42,8 @@ static void LoweringLayerNorm(PatternRewriter &rewriter, top::LayerNormOp op, Ty
     attrs.push_back(attr);
   }
   if (type.isF32()) {
-    rewriter.replaceOpWithNewOp<tpu::LayerNormOp>(op, op.getResultTypes(), opds, attrs);
+    rewriter.replaceOpWithNewOp<tpu::LayerNormOp>(op, op.getResultTypes(), opds,
+                                                  attrs);
     return;
   }
   std::vector<Type> new_types;
@@ -64,24 +66,43 @@ void LayerNormLowering::LoweringF32(PatternRewriter &rewriter,
   LoweringLayerNorm(rewriter, op, rewriter.getF32Type());
 }
 
-void LayerNormLowering::LoweringINT8(PatternRewriter &rewriter, top::LayerNormOp op,
+void LayerNormLowering::LoweringINT8(PatternRewriter &rewriter,
+                                     top::LayerNormOp op,
                                      bool asymmetric) const {
-  LoweringLayerNorm(rewriter, op, rewriter.getF32Type());
+  LoweringF16(rewriter, op);
 }
 
-void LayerNormLowering::LoweringINT4(PatternRewriter &rewriter, top::LayerNormOp op,
+void LayerNormLowering::LoweringINT4(PatternRewriter &rewriter,
+                                     top::LayerNormOp op,
                                      bool asymmetric) const {
   LoweringINT8(rewriter, op, asymmetric);
 }
 
 void LayerNormLowering::LoweringBF16(PatternRewriter &rewriter,
                                      top::LayerNormOp op) const {
-  LoweringLayerNorm(rewriter, op, rewriter.getF32Type());
+  LoweringLayerNorm(rewriter, op, rewriter.getBF16Type());
 }
 
 void LayerNormLowering::LoweringF16(PatternRewriter &rewriter,
                                     top::LayerNormOp op) const {
-  LoweringLayerNorm(rewriter, op, rewriter.getF32Type());
+  if (module::isCalibratedType(op.getInput()) == false) {
+    LoweringLayerNorm(rewriter, op, rewriter.getF32Type());
+    return;
+  }
+  auto cali_type = module::getCalibratedType(op.getInput());
+  auto max = cali_type.getMax();
+  auto min = cali_type.getMin();
+  auto shape = module::getShape(op.getInput());
+  auto axis = op.getAxis();
+  auto inner_size = std::accumulate(shape.begin() + axis, shape.end(), 1,
+                                    std::multiplies<int64_t>());
+  // assume half distance is (max - min)
+  auto limit = std::pow((max - min) / 2, 2) * inner_size / 2;
+  if (limit > 65504.0) { // F16 Max
+    LoweringLayerNorm(rewriter, op, rewriter.getF32Type());
+    return;
+  }
+  LoweringLayerNorm(rewriter, op, rewriter.getF16Type());
 }
 
 void LayerNormLowering::LoweringQuantized(PatternRewriter &rewriter,
