09364c0abc6f766036609a6ecaf9fa65de637985
pengchao.hu <pengchao.hu@sophgo.com>
2023-02-12 12:20:58 +0800

    WhereOp support INT8
    Change-Id: I9ce51a87e45f883d18d5813aa17f8410b9c1f089

--- lib/Dialect/Tpu/Interfaces/BM1684X/Where.cpp
+++ lib/Dialect/Tpu/Interfaces/BM1684X/Where.cpp
@@ -13,14 +13,6 @@
 
 using namespace tpu_mlir::backend;
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-#ifdef __cplusplus
-}
-#endif
-
 // =========================================
 // GlobalGenInterface
 // =========================================
