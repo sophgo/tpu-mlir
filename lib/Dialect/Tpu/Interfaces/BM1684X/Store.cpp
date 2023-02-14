817c7d6755aaea70c8ad61719f1bb38160d82e1d
mingyi.dong <mingyi.dong@sophgo.com>
2023-02-14 17:21:00 +0800

    support INT4 for A2 backend (2/2)
    Change-Id: I8a8cef31cc93a59b43e019cad5f8d794818dda5d

--- lib/Dialect/Tpu/Interfaces/BM1684X/Store.cpp
+++ lib/Dialect/Tpu/Interfaces/BM1684X/Store.cpp
@@ -9,6 +9,7 @@
 
 #include "tpu_mlir/Backend/BM168x/BM1684X.h"
 #include "tpu_mlir/Dialect/Tpu/IR/TpuOps.h"
+#include "tpu_mlir/Support/MathUtils.h"
 #include "tpu_mlir/Support/Module.h"
 
 using namespace tpu_mlir::backend;
@@ -27,38 +28,50 @@ void tpu::StoreOp::codegen_local_bm1684x(int64_t n_step, int64_t h_step,
                                          local_sec_info_t &sec_info) {
   CMD_ID_NODE *pid_node = (CMD_ID_NODE *)BM168x::instance()->gdma_node;
   auto gi = getGroupInfo(n_step, h_step);
-  int64_t N, C, H, W;
+  int64_t N, C, H, W, real_hslice;
   int gdma_format;
   module::getNCHW(getOutput(), N, C, H, W);
   auto data_type = BM168x::getDataType(getOutput());
-  if(data_type == DTYPE_UINT4 || data_type == DTYPE_INT4) {
+
+  real_hslice = gi.h_slice;
+  if (data_type == DTYPE_UINT4 || data_type == DTYPE_INT4) {
     gdma_format = BM168x::GDMA_VALUE_FORMAT_INT8;
-    data_type  = DTYPE_INT8;
-    W >>= 1;
+    data_type = DTYPE_INT8;
+    if (gi.h_slice == H) {
+      if (W * H & 0x1 == 1) {
+        W = align_up(W * H, (int64_t)2) / 2;
+        H = 1;
+        real_hslice = 1;
+      } else {
+        if (W & 0x1 == 1)
+          real_hslice >>= 1;
+        else
+          W >>= 1;
+      }
+    } else {
+      real_hslice = gi.h_slice;   // to do for int4
+    }
   }
   gdma_format = BM168x::getGdmaFormat(data_type);
   auto fmt_bytes = BM168x::getFmtBytes(data_type);
 
   auto g_stride = BM168x::getGlobalStride(N, C, H, W);
-  auto s_stride = BM168x::getLocalStride(gi.n_slice, C, gi.h_slice, W,
-                                         fmt_bytes, gi.eu_align);
+  auto s_stride = BM168x::getLocalStride(
+      gi.n_slice, C, real_hslice, W, fmt_bytes, gi.eu_align);
   auto g_addr = module::getAddress(getOutput());
   int64_t g_offset =
       (gi.n_idx * g_stride.N + gi.h_idx * g_stride.H) * fmt_bytes;
   BM168x::instance()->dl_tensor_stride_move_gen_cmd(
-      gi.out_addr, 0, g_addr + g_offset, gi.n_slice, C, gi.h_slice, W,
-      s_stride.N, s_stride.C, s_stride.H, s_stride.W, g_stride.N, g_stride.C,
-      g_stride.H, g_stride.W, gdma_format, GDMA_VALUE_DIR_L2S, 0, pid_node);
+      gi.out_addr, 0, g_addr + g_offset, gi.n_slice, C,
+      real_hslice, W, s_stride.N, s_stride.C, s_stride.H,
+      s_stride.W, g_stride.N, g_stride.C, g_stride.H, g_stride.W, gdma_format,
+      GDMA_VALUE_DIR_L2S, 0, pid_node);
 }
 
-//dynamic codegen
-int64_t tpu::StoreOp::dyn_codegen_local_bm1684x(void *buffer) {
-return 0;
-}
+// dynamic codegen
+int64_t tpu::StoreOp::dyn_codegen_local_bm1684x(void *buffer) { return 0; }
 
 // ======================================
 // Dynamic GlobalGenInterface
 // ======================================
-int64_t tpu::StoreOp::dyn_codegen_global_bm1684x(void *buffer) {
-  return 0;
-}
+int64_t tpu::StoreOp::dyn_codegen_global_bm1684x(void *buffer) { return 0; }
