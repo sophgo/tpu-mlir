ed7f96fb3b612c81f039d032d69259c707842a0e
zhibin.xin <zhibin.xin@sophgo.com>
2023-02-22 19:03:05 +0800

    bm1684x support fuse_preprocess
    Change-Id: Id64e6fa39c61070fbe39e27e2d187d6b17e98c8a

---
+++ lib/Conversion/TopToTpu/BM1684X/Preprocess.cpp
@@ -0,0 +1,333 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
+//
+// TPU-MLIR is licensed under the 2-Clause BSD License except for the
+// third-party components.
+//
+//===----------------------------------------------------------------------===//
+#include "tpu_mlir/Conversion/TopToTpu/LoweringBM1684X.h"
+#include "llvm/Support/Debug.h"
+
+#define DEBUG_TYPE "lowering-Preprocess"
+namespace tpu_mlir {
+namespace bm1684x {
+
+template <typename T>
+void swapInputChannelOfFilter(std::vector<T> &filter_data,
+                              std::vector<T> &new_filter,
+                              RankedTensorType &filter_type) {
+
+  std::vector<int64_t> shape(filter_type.getShape());
+  int64_t size = std::accumulate(std::begin(shape), std::end(shape), 1,
+                                 std::multiplies<>());
+  assert(filter_data.size() == size && "filter size should be equal");
+
+  int64_t oc, ic, frame_size;
+  int64_t index = shape.size();
+  assert((index == 4 || index == 5) && "filter shape size should be 4 or 5");
+
+  frame_size = shape[index - 1] * shape[index - 2];
+  ic = shape[index - 3];
+  oc = shape[index - 4];
+  if (index == 5) {
+    oc *= shape[index - 5];
+  }
+  std::vector<int> order{2, 1, 0};
+  T *filter = (T *)filter_data.data();
+  for (int i = 0; i < oc; ++i) {
+    for (int j = 0; j < ic; ++j) {
+      assert(order[j] < ic);
+      T *in = filter + i * ic * frame_size + order[j] * frame_size;
+      T *out = (T *)new_filter.data() + i * ic * frame_size + j * frame_size;
+      memcpy(out, in, frame_size * sizeof(T));
+    }
+  }
+}
+
+template <typename T>
+LogicalResult FoldSwapAxisOp(Operation *op, PatternRewriter &rewriter) {
+  auto swapOp = dyn_cast_or_null<top::SwapChannelOp>(op);
+  if (!swapOp) {
+    return failure();
+  }
+  auto nextOp = module::getNextOp(swapOp);
+  if (!nextOp) {
+    return failure();
+  }
+  auto convOp = dyn_cast_or_null<tpu::Conv2DOp>(nextOp);
+  if (!convOp) {
+    return failure();
+  }
+  if (convOp.getGroup() != 1) {
+    return failure();
+  }
+  assert(convOp.getNumOperands() == 3 && "Conv2D op should have 3 operands");
+  // filter
+  auto filterOp = cast<top::WeightOp>(convOp.getFilter().getDefiningOp());
+  auto filter_data = *(filterOp.read<T>());
+  auto filter_name = module::getName(filterOp.getOutput()).str();
+  auto filter_type =
+      convOp.getFilter().getType().template cast<RankedTensorType>();
+  std::vector<T> new_filter_data(filter_data.size());
+  swapInputChannelOfFilter<T>(filter_data, new_filter_data, filter_type);
+  auto newFilterOp = top::WeightOp::create(
+      convOp, filter_name + "_swap_channel", new_filter_data, filter_type);
+  convOp.setOperand(1, newFilterOp);
+  rewriter.replaceOp(op, {swapOp.getOperand()});
+  return success();
+}
+
+class LowerPreprocess {
+public:
+  LowerPreprocess(bool asymmetric) { _asymmetric = asymmetric; };
+
+  LowerPreprocess(){};
+
+  void lowerPreprocess(PatternRewriter &rewriter, top::PreprocessOp op) {
+    auto name = module::getName(op.getOutput()).str();
+    std::string quant_mode = op.getQuantMode().str();
+    auto resized_dims = module::getI64Array(op.getResizeDims());
+    std::string channel_order = op.getChannelOrder().str();
+    this->mean = *(module::getF64Array(op.getMean()));
+    this->scale = *(module::getF64Array(op.getScale()));
+    std::string pixel_format = op.getCustomizationFormat().str();
+    module::getNCHW(op.getResult(), n, c, h, w, false);
+    if (resized_dims->size() == 2) {
+      this->resize_h = resized_dims->at(0);
+      this->resize_w = resized_dims->at(1);
+    } else {
+      this->resize_h = h;
+      this->resize_w = w;
+    }
+    auto caliType = module::getCalibratedType(op.getOutput());
+    double qmax = caliType.getMax();
+    double qmin = _asymmetric ? caliType.getMin() : -qmax;
+    std::map<std::string, std::pair<std::string, std::string>> attributes_map =
+        {{"RGB_PLANAR", {"rgb", "nchw"}},  {"RGB_PACKED", {"rgb", "nhwc"}},
+         {"BGR_PLANAR", {"bgr", "nchw"}},  {"BGR_PACKED", {"bgr", "nhwc"}},
+         {"GRAYSCALE", {"bgr", "nchw"}},   {"YUV420_PLANAR", {"bgr", "nchw"}},
+         {"YUV_NV21", {"bgr", "nchw"}},    {"YUV_NV12", {"bgr", "nchw"}},
+         {"RGBA_PLANAR", {"rgba", "nchw"}}};
+    auto color = std::get<0>(attributes_map[pixel_format]);
+    auto layout = std::get<1>(attributes_map[pixel_format]);
+    bool swap_channel = (color != channel_order) ? true : false;
+    llvm::errs() << "pixel_format:" << pixel_format << ", color:" << color
+                 << ", layout:" << layout << ", swap_channel:" << swap_channel
+                 << "\n";
+    mlir::Value currentOut = op.getInput();
+    rewriter.setInsertionPointAfterValue(currentOut);
+    // create transpose Op if need
+    if (layout == "nhwc") {
+      currentOut = this->insertTransposeOp(rewriter, name, currentOut);
+      rewriter.setInsertionPointAfterValue(currentOut);
+    }
+
+    // create SliceOp
+    if (resize_h != h || resize_w != w) {
+      currentOut = this->insertSliceOp(rewriter, name, currentOut);
+      rewriter.setInsertionPointAfterValue(currentOut);
+    }
+
+    if (quant_mode == "INT8") {
+      currentOut = this->insertScaleLutOp(rewriter, name, currentOut, qmax,
+                                          swap_channel);
+      rewriter.setInsertionPointAfterValue(currentOut);
+    } else if (quant_mode == "BF16" || quant_mode == "F16" ||
+               quant_mode == "F32") {
+      currentOut =
+          this->insertScaleOp(rewriter, name, currentOut, qmax, swap_channel);
+      rewriter.setInsertionPointAfterValue(currentOut);
+    } else {
+      llvm_unreachable("Not Implemented");
+    }
+
+    if (swap_channel) {
+      currentOut =
+          this->insertSwapAxisOp(rewriter, name, currentOut, qmax, qmin);
+    }
+
+    rewriter.replaceOp(op, {currentOut});
+
+    Operation *curOp = currentOut.getDefiningOp();
+    if (quant_mode == "INT8") {
+      FoldSwapAxisOp<int8_t>(curOp, rewriter);
+    } else {
+      FoldSwapAxisOp<uint16_t>(curOp, rewriter);
+    }
+  }
+
+private:
+  int64_t n, c, h, w;
+  int64_t resize_h, resize_w;
+  std::vector<double> mean, scale;
+  bool _asymmetric = false;
+
+  Value insertTransposeOp(PatternRewriter &rewriter, std::string &name,
+                          Value opd) {
+    auto loc = NameLoc::get(rewriter.getStringAttr(name + "_tranpose"));
+    std::vector<int64_t> order{0, 3, 1, 2};
+    std::vector<NamedAttribute> attrs;
+    attrs.emplace_back(
+        rewriter.getNamedAttr("order", rewriter.getI64ArrayAttr(order)));
+    auto cali_type =
+        quant::CalibratedQuantizedType::get(rewriter.getF32Type(), 0, 255);
+    auto type = RankedTensorType::get({n, c, resize_h, resize_w}, cali_type);
+    auto newOp =
+        rewriter.create<top::PermuteOp>(loc, type, ArrayRef<Value>{opd}, attrs);
+    return newOp.getOutput();
+  }
+
+  Value insertSliceOp(PatternRewriter &rewriter, std::string &name, Value opd) {
+    auto loc = NameLoc::get(rewriter.getStringAttr(name + "_slice"));
+    int64_t start_h = resize_h / 2 - h / 2;
+    int64_t start_w = resize_w / 2 - w / 2;
+    std::vector<int64_t> slice_offset{0, 0, start_h, start_w};
+    std::vector<int64_t> slice_step{1, 1, 1, 1};
+    std::vector<NamedAttribute> attrs;
+    attrs.emplace_back(rewriter.getNamedAttr(
+        "offset", rewriter.getI64ArrayAttr(slice_offset)));
+    attrs.emplace_back(
+        rewriter.getNamedAttr("steps", rewriter.getI64ArrayAttr(slice_step)));
+    auto cali_type =
+        quant::CalibratedQuantizedType::get(rewriter.getF32Type(), 0, 255);
+    auto type = RankedTensorType::get({n, c, h, w}, cali_type);
+    auto newOp =
+        rewriter.create<top::SliceOp>(loc, type, ArrayRef<Value>{opd}, attrs);
+    return newOp.getOutput();
+  }
+
+  Value insertScaleLutOp(PatternRewriter &rewriter, std::string &name,
+                         Value opd, double threshold, bool swap_channel) {
+    auto loc = NameLoc::get(rewriter.getStringAttr(name + "_scale_lut"));
+    double qscale = module::getScale(threshold, true, 8);
+    std::vector<double> scales;
+    std::vector<double> bias;
+    for (int i = 0; i < c; i++) {
+      scales.push_back(this->scale[i]);
+      bias.push_back(-1 * this->scale[i] * this->mean[i]);
+    }
+    if (swap_channel) {
+      // keep order bgr
+      std::swap(scales[0], scales[2]);
+      std::swap(bias[0], bias[2]);
+    }
+    // quant
+    for (int i = 0; i < c; i++) {
+      scales[i] /= qscale;
+      bias[i] /= qscale;
+    }
+    std::vector<NamedAttribute> attrs;
+    attrs.emplace_back(
+        rewriter.getNamedAttr("scale", rewriter.getF64ArrayAttr(scales)));
+    attrs.emplace_back(
+        rewriter.getNamedAttr("bias", rewriter.getF64ArrayAttr(bias)));
+    auto cali_type = quant::CalibratedQuantizedType::get(rewriter.getF32Type(),
+                                                         -threshold, threshold);
+    auto type = RankedTensorType::get({n, c, h, w}, cali_type);
+    auto newOp = rewriter.create<top::ScaleLutOp>(loc, type,
+                                                  ArrayRef<Value>{opd}, attrs);
+    return newOp.getOutput();
+  }
+
+  Value insertScaleOp(PatternRewriter &rewriter, std::string &name, Value opd,
+                      double threshold, bool swap_channel) {
+    auto loc = NameLoc::get(rewriter.getStringAttr(name + "_scale"));
+    auto none = module::getNoneOp(opd.getDefiningOp());
+    std::vector<float> scales;
+    std::vector<float> bias;
+    for (int i = 0; i < c; i++) {
+      scales.push_back(this->scale[i]);
+      bias.push_back(-1 * this->scale[i] * this->mean[i]);
+    }
+
+    llvm::errs() << "scale:";
+    for (auto s : scales)
+      llvm::errs() << " " << s;
+    llvm::errs() << "\n";
+    llvm::errs() << "bias:";
+    for (auto b : bias)
+      llvm::errs() << " " << b;
+    llvm::errs() << "\n";
+
+    if (c == 3 && swap_channel) {
+      std::swap(scales[0], scales[2]);
+      std::swap(bias[0], bias[2]);
+    }
+
+    auto scale_type =
+        RankedTensorType::get({1, c, 1, 1}, rewriter.getF32Type());
+    auto bias_type = RankedTensorType::get({1, c, 1, 1}, rewriter.getF32Type());
+    std::vector<Value> operands;
+    operands.emplace_back(opd);
+    operands.emplace_back(none);
+    operands.emplace_back(none);
+    std::vector<NamedAttribute> attrs;
+    attrs.emplace_back(
+        rewriter.getNamedAttr("do_relu", rewriter.getBoolAttr(false)));
+    auto cali_type = quant::CalibratedQuantizedType::get(rewriter.getF32Type(),
+                                                         -threshold, threshold);
+    auto type = RankedTensorType::get({n, c, h, w}, cali_type);
+    auto newOp = rewriter.create<top::ScaleOp>(loc, type, operands, attrs);
+    auto scale_weight =
+        top::WeightOp::create(newOp, name + "_scale_0", scales, scale_type);
+    auto bias_Weight =
+        top::WeightOp::create(newOp, name + "_scale_1", bias, bias_type);
+    newOp.setOperand(1, scale_weight);
+    newOp.setOperand(2, bias_Weight);
+    return newOp.getOutput();
+  }
+
+  Value insertSwapAxisOp(PatternRewriter &rewriter, std::string &name,
+                         Value opd, float qmax, float qmin) {
+    llvm::errs() << "Entering insertSwapAxisOp.\n";
+    auto loc = NameLoc::get(rewriter.getStringAttr(name + "_swapaxis"));
+    std::vector<NamedAttribute> attrs;
+    std::vector<int64_t> orders{2, 1, 0};
+    attrs.emplace_back(rewriter.getNamedAttr("channel_order",
+                                             rewriter.getI64ArrayAttr(orders)));
+    auto cali_type =
+        quant::CalibratedQuantizedType::get(rewriter.getF32Type(), qmin, qmax);
+    auto type = RankedTensorType::get({n, c, h, w}, cali_type);
+    auto newOp = rewriter.create<top::SwapChannelOp>(
+        loc, type, ArrayRef<Value>{opd}, attrs);
+    return newOp.getOutput();
+  }
+};
+
+void PreprocessLowering::LoweringINT8(PatternRewriter &rewriter,
+                                      top::PreprocessOp op,
+                                      bool asymmetric) const {
+  LowerPreprocess prelower = LowerPreprocess(asymmetric);
+  prelower.lowerPreprocess(rewriter, op);
+}
+
+void PreprocessLowering::LoweringF32(PatternRewriter &rewriter,
+                                     top::PreprocessOp op) const {
+  LowerPreprocess prelower = LowerPreprocess();
+  prelower.lowerPreprocess(rewriter, op);
+}
+
+void PreprocessLowering::LoweringBF16(PatternRewriter &rewriter,
+                                      top::PreprocessOp op) const {
+  LoweringF32(rewriter, op);
+}
+
+void PreprocessLowering::LoweringINT4(PatternRewriter &rewriter,
+                                      top::PreprocessOp op,
+                                      bool asymmetric) const {
+  llvm_unreachable("Not Implemented");
+}
+
+void PreprocessLowering::LoweringF16(PatternRewriter &rewriter,
+                                     top::PreprocessOp op) const {
+  LoweringF32(rewriter, op);
+}
+
+void PreprocessLowering::LoweringQuantized(PatternRewriter &rewriter,
+                                           top::PreprocessOp op) const {
+  llvm_unreachable("Not Implemented");
+}
+
+} // namespace bm1684x
+} // namespace tpu_mlir
---
+++ lib/Conversion/TopToTpu/BM1684X/ScaleLut.cpp
@@ -0,0 +1,74 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
+//
+// TPU-MLIR is licensed under the 2-Clause BSD License except for the
+// third-party components.
+//
+//===----------------------------------------------------------------------===//
+#include "tpu_mlir/Conversion/TopToTpu/LoweringBM1684X.h"
+#include "llvm/Support/Debug.h"
+
+#define DEBUG_TYPE "lowering-ScaleLut"
+namespace tpu_mlir {
+namespace bm1684x {
+void ScaleLutLowering::LoweringINT8(PatternRewriter &rewriter, top::ScaleLutOp op,
+                                bool asymmetric) const {
+  Value input_val = op.getInput();
+  int64_t n, c, h, w;
+  module::getNCHW(input_val, n, c, h, w);
+  std::string name = module::getName(op.getOutput()).str();
+  auto scale = module::getF64Array(op.getScale());
+  auto bias = module::getF64Array(op.getBias());
+  int table_h = 16;
+  int table_w = 16;
+  int table_hw = table_h * table_w;
+  int table_size = c * table_hw;
+  std::vector<int8_t> table(table_size, 0);
+  for (int i = 0; i < c; i++) {
+    for (int idx = 0; idx < table_hw; ++idx) {
+      table[i * table_hw + idx] = to_int8(idx * scale->at(i) + bias->at(i), ROUNDING_HALF_UP);
+    }
+  }
+  std::vector<NamedAttribute> attrs;
+  attrs.emplace_back(rewriter.getNamedAttr("scale", op.getScaleAttr()));
+  attrs.emplace_back(rewriter.getNamedAttr("bias", op.getBiasAttr()));
+  auto table_shape = std::vector<int64_t>{1, c, table_h, table_w};
+  auto table_type = RankedTensorType::get(table_shape, rewriter.getI8Type());
+  auto table_op = top::WeightOp::create(op, name + "_table", table, table_type);
+  std::vector<Value> operands;
+  operands.emplace_back(input_val);
+  operands.emplace_back(table_op);
+  auto newType = getQuantInt8Type(op.getOutput(), asymmetric);
+  rewriter.replaceOpWithNewOp<tpu::ScaleLutOp>(op, newType, operands, attrs);
+}
+
+void ScaleLutLowering::LoweringF32(PatternRewriter &rewriter,
+                                    top::ScaleLutOp op) const {
+  lowering_common_f32<tpu::ScaleLutOp>(rewriter, op);
+}
+
+void ScaleLutLowering::LoweringINT4(PatternRewriter &rewriter,
+                                     top::ScaleLutOp op,
+                                     bool asymmetric) const {
+  LoweringINT8(rewriter, op, asymmetric);
+}
+
+void ScaleLutLowering::LoweringF16(PatternRewriter &rewriter,
+                                    top::ScaleLutOp op) const {
+  LoweringF32(rewriter, op);
+}
+
+void ScaleLutLowering::LoweringBF16(PatternRewriter &rewriter,
+                                top::ScaleLutOp op) const {
+  LoweringF32(rewriter, op);
+}
+
+void ScaleLutLowering::LoweringQuantized(PatternRewriter &rewriter,
+                                          top::ScaleLutOp op) const {
+  lowering_common<tpu::ScaleLutOp>(rewriter, op, op.getOutput().getType());
+}
+
+
+}
+}
