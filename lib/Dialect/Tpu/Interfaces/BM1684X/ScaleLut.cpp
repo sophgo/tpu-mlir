c9c41fa1273177213c03936a5db81953c1d7ba4a
zhibin.xin <zhibin.xin@sophgo.com>
2023-02-24 10:41:49 +0800

    update bm1684x scalelut
    Change-Id: I2fa413673de5f7d63a5b8f3e660c2700398acf08

--- lib/Dialect/Tpu/Interfaces/BM1684X/ScaleLut.cpp
+++ lib/Dialect/Tpu/Interfaces/BM1684X/ScaleLut.cpp
@@ -7,28 +7,18 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "tpu_mlir/Dialect/Tpu/IR/TpuOps.h"
 #include "tpu_mlir/Backend/BM168x/BM1684X.h"
+#include "tpu_mlir/Dialect/Tpu/IR/TpuOps.h"
 
 #include "tpu_mlir/Support/Module.h"
-
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 typedef struct {
-  uint64_t input_addr;
-  uint64_t table_addr;
-  uint64_t output_addr;
-  unsigned int buffer_addr; // used only for local layer
-  int shape[MAX_SHAPE_DIMS];
   int shape_dim;
   int table_length;
-  int input_dtype;
-  int table_dtype;
-  int output_dtype;
-  int is_local_layer;
 } scalelut_param_t;
 
 #ifdef __cplusplus
@@ -42,23 +32,14 @@ typedef struct {
 using namespace tpu_mlir::backend;
 
 void tpu::ScaleLutOp::codegen_global_bm1684x() {
-  scalelut_param_t p = {0};
-  p.input_addr = module::getAddress(getInput());
-  p.table_addr = module::getAddress(getTable());
-  p.output_addr = module::getAddress(getOutput());
-  p.input_dtype = BM168x::getDataType(getInput());
-  p.table_dtype = BM168x::getDataType(getTable());
-  p.output_dtype = BM168x::getDataType(getOutput());
-  p.is_local_layer = 0;
-  p.shape_dim = 4;
-  int64_t n, c, h, w;
-  module::getNCHW(getInput(), n, c, h, w);
-  p.shape[0] = n;
-  p.shape[1] = c;
-  p.shape[2] = h;
-  p.shape[3] = w;
-  p.table_length = 256;
-  BM168x::call_global_func("backend_api_scalelut", &p, sizeof(p));
+  auto op = getOperation();
+  scalelut_param_t param = {0};
+  param.shape_dim = 4;
+  param.table_length = 256;
+  auto input_spec = BM168x::get_input_spec(op);
+  auto output_spec = BM168x::get_output_spec(op);
+  BM168x::call_global_func("backend_api_scalelut_global", &param, sizeof(param),
+                           input_spec->data(), output_spec->data());
 }
 
 // =========================================
@@ -71,8 +52,8 @@ int64_t tpu::ScaleLutOp::getBufferSize_bm1684x(
   return 0;
 }
 
-
-void tpu::ScaleLutOp::codegen_local_bm1684x(int64_t n_step, int64_t h_step, local_sec_info_t &sec_info) {
+void tpu::ScaleLutOp::codegen_local_bm1684x(int64_t n_step, int64_t h_step,
+                                            local_sec_info_t &sec_info) {
   llvm_unreachable("Not Implemented");
 }
 
@@ -80,6 +61,4 @@ void tpu::ScaleLutOp::codegen_local_bm1684x(int64_t n_step, int64_t h_step, loca
 // Dynamic GlobalGenInterface
 // ======================================
 
-int64_t tpu::ScaleLutOp::dyn_codegen_global_bm1684x(void *buffer) {
-  return 0;
-}
+int64_t tpu::ScaleLutOp::dyn_codegen_global_bm1684x(void *buffer) { return 0; }
