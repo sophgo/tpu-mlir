b1912dee220303f65f2fe5b52fc9f90bd216b72d
pengchao.hu <pengchao.hu@sophgo.com>
2023-03-10 15:36:41 +0800

    support torch prim::TupleConstruct
    test by test_torch.py --case LayerNorm
    
    Change-Id: I9e6be5fdf6ce6e761a1f63c4d45dc713eb1686d3

--- lib/Dialect/Top/Transforms/ShapeInfer.cpp
+++ lib/Dialect/Top/Transforms/ShapeInfer.cpp
@@ -8,7 +8,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "tpu_mlir/Dialect/Top/Transforms/Passes.h"
-
+#include "mlir/Transforms/GreedyPatternRewriteDriver.h"
 #include "mlir/IR/PatternMatch.h"
 
 using namespace llvm;
@@ -17,11 +17,40 @@ using namespace mlir;
 namespace tpu_mlir {
 namespace top {
 
+class TupleFusePattern : public OpRewritePattern<TupleOp> {
+public:
+  using OpRewritePattern<TupleOp>::OpRewritePattern;
+
+  LogicalResult matchAndRewrite(TupleOp op,
+                                PatternRewriter &rewriter) const override {
+    auto out = op.getOutput();
+    for (auto user : op->getUsers()) {
+      std::vector<Value> operands;
+      for (auto opd : user->getOperands()) {
+        if (opd == out) {
+          for (auto v : op.getOperands()) {
+            operands.push_back(v);
+          }
+        } else {
+          operands.push_back(opd);
+        }
+      }
+      user->setOperands(operands);
+    }
+    op.erase();
+    return success();
+  }
+};
+
 class ShapeInferPass : public ShapeInferBase<ShapeInferPass> {
 public:
   ShapeInferPass() {}
   void runOnOperation() override {
     auto mOp = getOperation();
+    auto ctx = &getContext();
+    RewritePatternSet patterns(ctx);
+    patterns.add<TupleFusePattern>(ctx);
+    applyPatternsAndFoldGreedily(mOp, std::move(patterns));
     for (auto func : mOp.getOps<FuncOp>()) {
       func.walk([&](ShapeInterface op) { op.shape_inference(); });
     }
