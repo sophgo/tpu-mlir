dcfef2ce2694c5d923860fddf19b027c95c5041d
xu.yang <xu.yang@sophgo.com>
2023-03-16 12:22:05 +0800

    [BM1684X][MLIR-23] conv3d getBufferSize_bm1684x type judge wrong
    Change-Id: Icf47ac585d2fb20e631ed5511a7d23c955688e61

--- lib/Dialect/Tpu/Interfaces/BM1684X/Conv3D.cpp
+++ lib/Dialect/Tpu/Interfaces/BM1684X/Conv3D.cpp
@@ -250,8 +250,8 @@ int64_t tpu::Conv3DOp::getBufferSize_bm1684x(
                  ceiling_func(i * attr.groups % npu_num + attr.oc / attr.groups,
                               npu_num));
   }
-  auto in_type = getInput().getType();
-  auto out_type = getOutput().getType();
+  auto in_type = getInput().getType().getElementType();
+  auto out_type = getOutput().getType().getElementType();
   // output start npu id must be same with weight start npu id
   if ((in_type.isF16() || in_type.isBF16()) && !out_type.isF32() &&
       attr.kd > 1) {
