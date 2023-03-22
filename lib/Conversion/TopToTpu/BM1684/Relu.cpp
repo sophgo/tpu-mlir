143051e82e3d384a4f5a854f13208883f7603ca6
Julian Cheng <zuliang.cheng@sophgo.com>
2023-03-22 11:53:49 +0800

    add Relu to BM1684 for fp32 and int8
    Change-Id: I8ad9f031ac4449f920414b7480e3454f6553cd02

--- lib/Conversion/TopToTpu/BM1684/Relu.cpp
+++ lib/Conversion/TopToTpu/BM1684/Relu.cpp
@@ -19,7 +19,28 @@ void ReluLowering::LoweringF32(PatternRewriter &rewriter,
 
 void ReluLowering::LoweringINT8(PatternRewriter &rewriter, top::ReluOp op,
                                 bool asymmetric) const {
-  lowering_common_int8<tpu::ReluOp>(rewriter, op, false);
+  std::vector<Value> operands;
+  float coeff_v = 1.0;
+  int64_t re_zp, o_zp;
+  double re_scale, o_scale;
+
+  auto re = op.getOperand();
+  operands.push_back(re);
+
+  module::getScaleAndZeroPoint(op.getOutput(), o_scale, o_zp, asymmetric);
+  module::getScaleAndZeroPoint(re, re_scale, re_zp, asymmetric);
+  auto re_rshift =
+      calRightShiftNumUseCblas(coeff_v, re_scale, o_scale, BITS_INT8);
+  float scale = 1.0 * (1 << re_rshift) * re_scale / o_scale;
+  int8_t multiplier_int8;
+  quantizeToInt8(&coeff_v, &multiplier_int8, 1, scale);
+  std::vector<NamedAttribute> attrs;
+  attrs.push_back(rewriter.getNamedAttr("relu_limit", op.getReluLimitAttr()));
+  attrs.push_back(rewriter.getNamedAttr(
+      "multiplier", rewriter.getSI32IntegerAttr(multiplier_int8)));
+  attrs.push_back(rewriter.getNamedAttr("rshift", rewriter.getI64IntegerAttr(re_rshift)));
+  auto newType = getQuantInt8Type(op.getOutput());
+  rewriter.replaceOpWithNewOp<tpu::ReluOp>(op, newType, operands, attrs);
 }
 
 } // namespace bm1684
