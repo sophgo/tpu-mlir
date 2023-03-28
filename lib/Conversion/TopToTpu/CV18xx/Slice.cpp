4e43bb0fe72ce3af2d108fe1bfd6fc7ac6861e6a
shenglin.li <shenglin.li01@sophgo.com>
2023-03-28 10:03:53 +0800

    Fix CV18xx Slice lowering for AlignInput.
    Change-Id: I2504bd692798c32676514622badadfd858bd20e2

--- lib/Conversion/TopToTpu/CV18xx/Slice.cpp
+++ lib/Conversion/TopToTpu/CV18xx/Slice.cpp
@@ -20,6 +20,17 @@ void SliceLowering::LoweringINT8(PatternRewriter &rewriter, top::SliceOp op,
 
 void SliceLowering::LoweringBF16(PatternRewriter &rewriter,
                                  top::SliceOp op) const {
+  auto out = op.getOutput();
+  if (module::isCalibratedType(out)) {
+    //For fuse_preprocess(crop image) and aligned use, it should be lowered to uint8.
+    auto qtype = module::getCalibratedType(out);
+    auto max = qtype.getMax();
+    auto min = qtype.getMin();
+    if (min == 0 && max == 255) {
+      lowering_common_int8<tpu::SliceOp>(rewriter, op, false, 2);
+      return;
+    }
+  }
   lowering_common_bf16<tpu::SliceOp>(rewriter, op, 2);
 }
 
