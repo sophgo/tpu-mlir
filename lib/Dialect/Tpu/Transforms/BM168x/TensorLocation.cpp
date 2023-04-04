5e79add04cedbf02973ea01a5fc19c895db74acd
man.lu <man.lu@sophgo.com>
2023-04-04 06:05:31 +0000

    TensorLocation
    - Refine TensorLocation
    - Fix bugs in encoding local/global memory information.
      "getGroupInfo" is the function to generate slice info.
    
    Change-Id: I353fec6792a1d32054d45f902f5cdea53f7a8358

--- lib/Dialect/Tpu/Transforms/BM168x/TensorLocation.cpp
+++ lib/Dialect/Tpu/Transforms/BM168x/TensorLocation.cpp
@@ -8,10 +8,12 @@
 //===----------------------------------------------------------------------===//
 
 #include "tpu_mlir/Dialect/Tpu/Transforms/BM168x/TensorLocation.hpp"
+#include "mlir/Dialect/Func/IR/FuncOps.h"
 #include "tpu_mlir/Backend/BM168x/BM168x.h"
 #include "tpu_mlir/Dialect/Tpu/IR/TpuOps.h"
 #include "tpu_mlir/Support/Module.h"
 #include <llvm/Support/JSON.h>
+#include <variant>
 
 namespace mlir {
 using namespace llvm;
@@ -33,7 +35,7 @@ inline llvm::raw_ostream &operator<<(llvm::raw_ostream &OS, const slice &S) {
   return OS;
 }
 
-inline std::string Slice(std::initializer_list<slice> slices) {
+inline std::string fmt_slice(std::initializer_list<slice> slices) {
   std::string stride;
   llvm::raw_string_ostream os(stride);
   os << "[";
@@ -42,28 +44,141 @@ inline std::string Slice(std::initializer_list<slice> slices) {
   return stride;
 }
 
-json::Object record_tensor(Value v, const group_type_t &group_type,
-                           const StringRef slice, const StringRef layout) {
-  auto v_spc = BM168x::value_to_spec(v, group_type);
-  std::string type, shape, S;
-  llvm::raw_string_ostream os(S);
-  v.getType().print(os);
-  type = S;
-  S.clear();
+template <typename T>
+inline std::string fmt_shape(ArrayRef<T> shape, StringRef dtype = {}) {
+  std::string shape_str;
+  llvm::raw_string_ostream os(shape_str);
   os << "<";
-  interleave(llvm::ArrayRef(v_spc.shape, v_spc.dims), os, "x");
+  interleave(shape, os, "x");
+  if (dtype.size() > 0)
+    os << "x" << dtype;
   os << ">";
-  shape = S;
+  return shape_str;
+}
+
+inline std::string get_dtype_str(int dtype) {
+  assert(dtype >= 0);
+  std::string dtype_str[] = {"f32",  "f16",  "si8",  "ui8", "si16", "ui16",
+                             "si32", "ui32", "bf16", "si4", "ui4"};
+  return dtype_str[dtype];
+}
+
+struct slice_index {
+  int64_t n_step;
+  int64_t h_step;
+  int64_t d_step;
+  int64_t w_step;
+};
+
+group_info_t getGroupInfo(Value v, const slice_index &slice_i) {
+  return LocalGenInterface::getGroupInfo(v, slice_i.n_step, slice_i.h_step,
+                                         slice_i.d_step, slice_i.w_step);
+}
+
+group_info_t getGroupInfo(Operation *op, const slice_index &slice_i) {
+  return LocalGenInterface::getGroupInfo(op, slice_i.n_step, slice_i.h_step,
+                                         slice_i.d_step, slice_i.w_step);
+}
+
+group_info_t getGroupInfo(const OpOperand &v, const slice_index &slice_i) {
+  if (auto op = v.get().getDefiningOp())
+    if (op->hasAttr(LocalGenInterface::kLayerGroupAttrName))
+      return getGroupInfo(op, slice_i);
+
+  // Computing the backward slice is time-consuming.
+  group_info_t ginfo = {0};
+  auto dst_op = v.getOwner();
+  auto dst_lg_op = cast<LocalGenInterface>(dst_op);
+  auto g_param = dst_op->getAttr(LocalGenInterface::kLayerGroupAttrName)
+                     .cast<tpu::LayerGroupAttr>();
+  int64_t nslice = g_param.getNSlice()[slice_i.n_step];
+  int64_t hslice = g_param.getHSlice()[slice_i.h_step];
+  int64_t dslice = g_param.getDSlice()[slice_i.d_step];
+  int64_t wslice = g_param.getWSlice()[slice_i.w_step];
+  dst_lg_op.BackwardN(ginfo.n_idx, ginfo.n_slice, slice_i.n_step, nslice);
+  dst_lg_op.BackwardH(ginfo.h_idx, ginfo.h_slice, slice_i.h_step, hslice);
+  dst_lg_op.BackwardD(ginfo.d_idx, ginfo.d_slice, slice_i.d_step, dslice);
+  dst_lg_op.BackwardW(ginfo.w_idx, ginfo.w_slice, slice_i.w_step, wslice);
+  return ginfo;
+}
+
+json::Object record_tensor(const std::variant<Value, OpOperand *> val_or_opd,
+                           const slice_index &slice_i,
+                           group_type_t group_type) {
+
+  group_info_t ginfo;
+  Value val;
+  if (auto v = std::get_if<Value>(&val_or_opd)) {
+    val = *v;
+    ginfo = getGroupInfo(*v, slice_i);
+  } else {
+    auto o = std::get<OpOperand *>(val_or_opd);
+    val = o->get();
+    ginfo = getGroupInfo(*o, slice_i);
+  };
+
+  auto v_spc = BM168x::value_to_spec(val, group_type);
+  auto name = module::getName(val);
+
+  std::string type;
+  llvm::raw_string_ostream os(type);
+  val.getType().print(os);
+
+  int64_t re_shape[5];
+  module::getNCDHW(val, re_shape[0], re_shape[1], re_shape[2], re_shape[3],
+                   re_shape[4], group_type);
+  auto reshape = fmt_shape(ArrayRef(re_shape));
+
+  int64_t mem_shape[] = {ginfo.n_slice * ginfo.d_slice, re_shape[1],
+                         ginfo.h_slice, ginfo.w_slice};
+
+  auto memory_type = fmt_shape(ArrayRef(mem_shape), get_dtype_str(v_spc.dtype));
+
   uint64_t address = v_spc.addr;
-  if (isa_and_nonnull<tpu::StoreOp>(v.getDefiningOp()))
-    address = module::getAddress(v);
+
+  auto slice = fmt_slice({{ginfo.n_idx, ginfo.n_slice},
+                          {-1, -1},
+                          {ginfo.d_idx, ginfo.d_slice},
+                          {ginfo.h_idx, ginfo.h_slice},
+                          {ginfo.w_idx, ginfo.w_slice}});
+
+  auto layout = ginfo.eu_align ? "eu_align" : "compact";
+
+  // global memory
+  auto op = val.getDefiningOp();
+  if (op == nullptr || !op->hasAttr(LocalGenInterface::kLayerGroupAttrName)) {
+    layout = "continuous";
+  }
+  if (isa_and_nonnull<tpu::StoreOp>(op)) {
+    address = module::getAddress(val);
+    layout = "continuous";
+  }
+
+  return json::Object{
+      {"name", name},     {"address", address}, {"memory_type", memory_type},
+      {"layout", layout}, {"type", type},       {"reshape", reshape},
+      {"slice", slice},
+  };
+}
+
+json::Object record_tensor(Value v, const group_type_t group_type) {
+  auto v_spc = BM168x::value_to_spec(v, group_type);
+  std::string type;
+  llvm::raw_string_ostream os(type);
+  v.getType().print(os);
+
+  auto memory_type =
+      fmt_shape(ArrayRef(v_spc.shape, v_spc.dims), get_dtype_str(v_spc.dtype));
+
+  uint64_t address = v_spc.addr;
 
   return json::Object{{"name", module::getName(v).str()},
-                      {"type", type},
-                      {"shape", shape},
-                      {"slice", slice},
                       {"address", address},
-                      {"layout", layout}};
+                      {"memory_type", memory_type},
+                      {"layout", "continuous"},
+                      {"type", type},
+                      {"reshape", ""},
+                      {"slice", "[...]"}};
 }
 
 void TensorLocationImpl::record_loc(Operation *op, const json::Array &operands,
@@ -73,9 +188,17 @@ void TensorLocationImpl::record_loc(Operation *op, const json::Array &operands,
   if (it != opToLineCol.end()) {
     line_num = it->second.first;
   }
+  int subnet_id = -1;
+  if (auto func = dyn_cast<func::FuncOp>(op->getParentOp()))
+    subnet_id = func->getAttrOfType<IntegerAttr>("id").getInt();
+  else if (auto lg = dyn_cast<tpu::GroupOp>(op->getParentOp())) {
+    auto func = cast<func::FuncOp>(lg->getParentOp());
+    subnet_id = func->getAttrOfType<IntegerAttr>("id").getInt();
+  }
 
   J.object([&] {
     J.attribute("file-line", line_num);
+    J.attribute("subnet_id", subnet_id);
     J.attribute("opcode", op->getName().getStringRef());
     J.attributeArray("bdc_gdma_id(before)", [&] {
       J.value(cmd_before[0]);
@@ -99,34 +222,25 @@ void TensorLocationImpl::record_loc(Operation *op, const json::Array &operands,
 void TensorLocationImpl::after_codegen_local(Operation *op, int64_t n_step,
                                              int64_t h_step, int64_t d_step,
                                              int64_t w_step,
-                                             const group_type_t &group_type,
+                                             group_type_t group_type,
                                              local_sec_info_t &sec_info) {
-  auto group_info = LocalGenInterface::getGroupInfo(op, n_step, h_step);
-  auto in_slice = Slice({{group_info.n_idx, sec_info.n_slice},
-                         {-1, -1},
-                         {sec_info.h_idx, sec_info.h_slice},
-                         {sec_info.w_idx, sec_info.w_slice}});
-
-  std::string layout = group_info.eu_align ? "en_align" : "";
+  auto slice_i = slice_index{
+      .n_step = n_step, .h_step = h_step, .d_step = d_step, .w_step = w_step};
 
   json::Array operands, results;
-  for (auto v : op->getOperands()) {
-    if (module::isNone(v)) {
-      operands.push_back(json::Object());
-    } else
-      operands.push_back(record_tensor(v, group_type, in_slice, layout));
-  }
 
-  auto out_slice = Slice({{group_info.n_idx, sec_info.out_n_slice},
-                          {-1, -1},
-                          {sec_info.out_h_idx, sec_info.out_h_slice},
-                          {sec_info.out_w_idx, sec_info.out_w_slice}});
+  for (auto &v : op->getOpOperands()) {
+    if (module::isNone(v.get()))
+      operands.push_back(json::Object());
+    else
+      operands.push_back(record_tensor(&v, slice_i, group_type));
+  }
 
   for (auto v : op->getResults()) {
     if (module::isNone(v)) {
       results.push_back(json::Object());
     } else
-      results.push_back(record_tensor(v, group_type, out_slice, layout));
+      results.push_back(record_tensor(v, slice_i, group_type));
   }
   record_loc(op, operands, results);
 }
@@ -138,14 +252,14 @@ void TensorLocationImpl::after_codegen_global(Operation *op) {
     if (module::isNone(v)) {
       operands.push_back(json::Object());
     } else
-      operands.push_back(record_tensor(v, GROUP_NORMAL, "", ""));
+      operands.push_back(record_tensor(v, GROUP_NORMAL));
   }
 
   for (auto v : op->getResults()) {
     if (module::isNone(v)) {
       results.push_back(json::Object());
     } else
-      results.push_back(record_tensor(v, GROUP_NORMAL, "", ""));
+      results.push_back(record_tensor(v, GROUP_NORMAL));
   }
   record_loc(op, operands, results);
 }
