371412e9dc1212dbb230acc704ae5db2544be981
pengchao.hu <pengchao.hu@sophgo.com>
2023-04-13 16:45:37 +0800

    add inference test for stable diffusion
    if model is too large, there are two choices:
    1) save all tensors to disk, use temporary memory to inference
    2) save part tensor to mem, other tensors use temporary memory
    
    Change-Id: Iadbdef9004178cd4a13f4ec7776e1a976da1cb2d

--- include/tpu_mlir/Support/ModuleInterpreter.h
+++ include/tpu_mlir/Support/ModuleInterpreter.h
@@ -31,22 +31,42 @@ using namespace mlir;
 namespace tpu_mlir {
 // Implementation class for module interpreter.
 class ModuleInterpreter {
-
 public:
+  enum class mem_mode_t {
+    // if mem size > 16GB, then use ALL_TENSOR_IN_DISK or PART_TENSOR_IN_MEM
+    // else use ALL_TENSOR_IN_MEM
+    ALL_TENSOR_IN_MEM,
+    ALL_TENSOR_IN_DISK,
+    PART_TENSOR_IN_MEM
+  };
   // Interpret the given MLIR module expressed in MLIR TPU IR dialect
   explicit ModuleInterpreter(ModuleOp module);
   virtual ~ModuleInterpreter();
   void allocate_resources();
   void invoke(bool express_type = true);
+  void invoke_to_disk(const std::string &filename, bool express_type = true);
   void fake_quant_weight();
   std::shared_ptr<std::vector<float>> invoke_at(std::string name);
   void invoke_from(const std::string op_name);
   void setTensor(const std::string &name, const void *data, size_t size,
                  bool is_integer = false);
-  std::shared_ptr<std::vector<float>> getTensor(const std::string &name, bool express_type = false);
-  bool getTensorQuantInfo(const std::string name, std::string &dtype, float &scale, int &zp);
+  std::shared_ptr<std::vector<float>> getTensor(const std::string &name,
+                                                bool express_type = false);
+  bool getTensorQuantInfo(const std::string name, std::string &dtype,
+                          float &scale, int &zp);
   llvm::ArrayRef<int64_t> getTensorShape(const std::string &name);
-  bool is_no_mem_op(Operation*op);
+  bool is_no_mem_op(Operation *op);
+
+private:
+  void allocate_part_tensor_in_mem();
+  void allocate_all_tensor_in_mem();
+  void allocate_all_tensor_in_disk();
+  bool check_op_in_mem(Operation *op);
+  void invoke_part_in_mem(bool express_type = true);
+  void invoke_all_in_mem(bool express_type = true);
+  void value_to_disk(const std::string &filename, const std::string &name,
+                     std::vector<float> &data, bool express_type = true);
+  void collect_tensor(Value v);
 
 public:
   std::vector<std::string> input_names;
@@ -58,6 +78,8 @@ public:
 private:
   ModuleOp module;
   int64_t num_infer_op;
+  mem_mode_t mem_mode;
+  int64_t total_count;
   std::map<std::string, Value> value_map;
   std::map<std::string, std::shared_ptr<InferenceParameter>> inference_map;
   std::map<std::string, std::shared_ptr<std::vector<float>>> mem_map;
