1d00b4346f06ede29104a211d2b06c5859b87c3d
wenjie.zhou <wenjie.zhou@sophgo.com>
2023-04-18 15:02:25 +0800

    [mlir-1684] add log
    Change-Id: Ic06c033ecbbfe73abb0a807e3dd3f4d15898d4aa

---
+++ lib/Conversion/TopToTpu/BM1684/Log.cpp
@@ -0,0 +1,31 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
+//
+// TPU-MLIR is licensed under the 2-Clause BSD License except for the
+// third-party components.
+//
+//===----------------------------------------------------------------------===//
+
+#include "tpu_mlir/Conversion/TopToTpu/LoweringBM1684.h"
+
+namespace tpu_mlir {
+namespace bm1684 {
+
+void LogLowering::LoweringF32(PatternRewriter &rewriter, top::LogOp op) const {
+    auto op_ = op.getOperation();
+    op_->setAttr("mode", tpu::ActiveModeAttr::get(op.getContext(),
+                                                  tpu::ActiveMode::LN));
+    lowering_common_f32<tpu::ActiveOp>(rewriter, op_);
+}
+
+void LogLowering::LoweringINT8(PatternRewriter &rewriter, top::LogOp op,
+                                 bool asymmetric) const {
+    Value table = create_lookup_table(op.getInput(), op.getOutput(), asymmetric,
+                                      [](double val) { return std::log(val); }, 32);
+    auto newType = getQuantInt8Type(op.getOutput(), asymmetric);
+    rewriter.replaceOpWithNewOp<tpu::LutOp>(op, newType, ValueRange{op.getInput(), table});
+}
+
+} // namespace bm1684
+} // namespace tpu_mlir
