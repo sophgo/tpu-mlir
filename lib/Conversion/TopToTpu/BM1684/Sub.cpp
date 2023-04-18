75567d975e412cb2412027982e07486790e7865b
bin.xiang01 <bin.xiang01@sophgo.com>
2023-04-18 16:17:48 +0800

    add SubConst support for BM1684
    Change-Id: I37f50c988504f1b02d2cd3e3da3cc6f90238711c

--- lib/Conversion/TopToTpu/BM1684/Sub.cpp
+++ lib/Conversion/TopToTpu/BM1684/Sub.cpp
@@ -29,13 +29,36 @@ void SubLowering::LoweringINT8(PatternRewriter &rewriter, top::SubOp op,
       coeff_v[idx++] = v.cast<FloatAttr>().getValueAsDouble();
     }
   }
-
   for (int i = 0; i < nInputs; i++) {
     auto input = op->getOperand(i);
-    operands.push_back(input);
     int64_t in_zp;
     double in_scale;
-    module::getScaleAndZeroPoint(input, in_scale, in_zp, asymmetric);
+    if (auto ConstOp = dyn_cast<top::WeightOp>(input.getDefiningOp())) {
+      auto constF32 = ConstOp.read<float>();
+      float fmax, fmin;
+      findMinMax(constF32->data(), constF32->size(), &fmin, &fmax);
+      bool ConstSign = (fmin < 0);
+      float fqmax = ConstSign ? 127 :255;
+      auto filter_type = input.getType().cast<RankedTensorType>();
+      auto new_type = RankedTensorType::get(filter_type.getShape(),rewriter.getIntegerType(8, ConstSign));
+      in_scale = fmax / fqmax;
+      if (ConstSign) {
+        auto constI8 = std::make_shared<std::vector<int8_t>>(constF32->size());
+        std::transform(constF32->begin(), constF32->end(), constI8->begin(),
+                       [&](const float cf32) { return to_int8(cf32 / in_scale); });
+        auto new_filter = top::WeightOp::create(ConstOp, "i8", *constI8, new_type);
+        operands.push_back(new_filter);
+      } else {
+        auto constU8 = std::make_shared<std::vector<uint8_t>>(constF32->size());
+        std::transform(constF32->begin(), constF32->end(), constU8->begin(),
+                       [&](const float cf32) { return to_uint8(cf32 / in_scale); });
+        auto new_filter = top::WeightOp::create(ConstOp, "U8", *constU8, new_type);
+        operands.push_back(new_filter);
+      }
+    } else {
+      operands.push_back(input);
+      module::getScaleAndZeroPoint(input, in_scale, in_zp, asymmetric);
+    }
     rshift_v[i] =
         calRightShiftNumUseCblas(coeff_v[i], in_scale, o_scale, BITS_INT8);
     float scale = 1.0 * (1 << rshift_v[i]) * in_scale / o_scale;
