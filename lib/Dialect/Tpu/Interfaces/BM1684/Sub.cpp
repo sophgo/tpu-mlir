806f502906fc2c2ab42da89c6138644815713d0d
heng.yang <heng.yang@sophgo.com>
2023-04-20 20:35:28 +0800

    support Sub dynamic
    Change-Id: Ic8560558639a89dc4225244317f8119ec54b17b2

--- lib/Dialect/Tpu/Interfaces/BM1684/Sub.cpp
+++ lib/Dialect/Tpu/Interfaces/BM1684/Sub.cpp
@@ -9,7 +9,8 @@
 
 #include "tpu_mlir/Backend/BM168x/BM1684.h"
 #include "tpu_mlir/Dialect/Tpu/IR/TpuOps.h"
-
+#include "tpu_mlir/Dialect/Tpu/Transforms/BM168x/DynamicLayer.hpp"
+#include "tpu_mlir/Support/MathUtils.h"
 #include "tpu_mlir/Support/Module.h"
 
 using namespace tpu_mlir::backend;
@@ -163,15 +164,66 @@ void tpu::SubOp::codegen_local_bm1684(int64_t n_step, int64_t h_step,
   }
 }
 
-uint32_t tpu::SubOp::dyn_codegen_global_bm1684(void* ir_layer_info) {
-  llvm_unreachable("Not Implemented");
-  return 0;
-}
-int64_t tpu::SubOp::get_fw_type_bm1684() {
-  return -1;
+// ======================================
+// Dynamic GlobalGenInterface
+// ======================================
+
+uint32_t tpu::SubOp::dyn_codegen_global_bm1684(void *ir_layer_info) {
+  int fw_ir_length = 0;
+  ir_layer_info_t *add_layer_info = (ir_layer_info_t *)ir_layer_info;
+  fw_broadcast_binary_layer_param_t fw_broadcast_binary_layer_param = {0};
+
+  dynamic_common_ir_layer_info(add_layer_info, getInputs()[0], getOutput());
+
+  assign_fw_param((void *)&fw_broadcast_binary_layer_param);
+  add_layer_info->fw_layer_param_u.fw_broadcast_binary_layer_param =
+      fw_broadcast_binary_layer_param;
+  fw_ir_length += sizeof(fw_broadcast_binary_layer_param_t);
+
+  return fw_ir_length;
 }
 
-int32_t tpu::SubOp::dyn_codegen_local_bm1684(void* ir_layer_info) {
-  llvm_unreachable("Not Implemented");
-  return 0;
+int64_t tpu::SubOp::get_fw_type_bm1684() { return FW_BMNET_BROADCAST_BINARY; }
+
+// ======================================
+// Dynamic LocalGenInterface
+// ======================================
+
+int32_t tpu::SubOp::dyn_codegen_local_bm1684(void *ir_layer_info) {
+  int fw_ir_length = 0;
+  ir_layer_info_t *add_layer_info = (ir_layer_info_t *)ir_layer_info;
+  fw_broadcast_binary_layer_param_t fw_broadcast_binary_layer_param = {0};
+
+  dynamic_common_ir_layer_info(add_layer_info, getInputs()[0], getOutput());
+
+  assign_fw_param((void *)&fw_broadcast_binary_layer_param);
+  add_layer_info->fw_layer_param_u.fw_broadcast_binary_layer_param =
+      fw_broadcast_binary_layer_param;
+  fw_ir_length += sizeof(fw_broadcast_binary_layer_param_t);
+
+  // get layer input and output
+  add_layer_info->ir_tensor_info_v.clear();
+  // input tensor
+  for (int i = 0; i < getInputs().size(); ++i) {
+    dynamic_push_back_local_tensor(add_layer_info->ir_tensor_info_v,
+                                   getInputs()[i]);
+  }
+  // output tensor
+  dynamic_push_back_local_tensor(add_layer_info->ir_tensor_info_v, getOutput());
+
+  if (add_layer_info->data_size != DSIZE_FP32) {
+    dynamic_push_back_local_buffer(add_layer_info->ir_tensor_info_v,
+                                   get_tensor_id(getInputs()[0]), getOutput());
+  }
+
+  // compute fw ir info length for input and output
+  fw_ir_length += (uint32_t)(getInputs().size() + 1) * (2 * sizeof(uint32_t));
+
+  if (add_layer_info->data_size != DSIZE_FP32)
+    fw_ir_length += sizeof(uint32_t);
+
+  // add fw ir length for output consumer number
+  fw_ir_length += sizeof(uint32_t);
+
+  return fw_ir_length;
 }
--- lib/Dialect/Tpu/Interfaces/Common/Sub.cpp
+++ lib/Dialect/Tpu/Interfaces/Common/Sub.cpp
@@ -11,7 +11,8 @@
 #include "tpu_mlir/Support/Dnnl/Dnnl.h"
 #include "tpu_mlir/Support/Float16.h"
 #include "tpu_mlir/Support/Module.h"
-
+#include "tpu_mlir/Dialect/Tpu/Transforms/BM168x/DynamicLayer.hpp"
+#include "tpu_mlir/Backend/BM168x/BM1684.h"
 #include "tpu_mlir/Support/MathUtils.h"
 
 LogicalResult tpu::SubOp::init(InferenceParameter &p) {
@@ -190,4 +191,48 @@ void tpu::SubOp::assign_sec_info(int64_t n_step, int64_t h_step, int64_t d_step,
   sec_info.out_h_slice = gi.h_slice;
   sec_info.out_w_idx = gi.w_idx;
   sec_info.out_w_slice = gi.w_slice;
+}
+
+void tpu::SubOp::assign_fw_param(void *param) {
+  fw_broadcast_binary_layer_param_t *fw_broadcast_binary_layer_param =
+      (fw_broadcast_binary_layer_param_t *)param;
+  fw_broadcast_binary_layer_param->binary_op = BINARY_SUB;
+  fw_broadcast_binary_layer_param->a_is_coeff =
+      module::isWeight(getInputs()[0]);
+  fw_broadcast_binary_layer_param->b_is_coeff =
+      module::isWeight(getInputs()[1]);
+  fw_broadcast_binary_layer_param->if_relu = getDoRelu() ? 1 : 0;
+  fw_broadcast_binary_layer_param->relu_upper_limit =
+      getReluLimit().convertToDouble();
+  fw_broadcast_binary_layer_param->buffer_addr = 0; // no use
+
+  auto input_num = getInputs().size();
+  assert(input_num == 2);
+  auto muls = module::getI32Array(getMultipliers(), input_num, 1);
+  auto rs = module::getI32Array(getRshifts(), input_num, 0);
+  for (int i = 0; i < input_num; ++i) {
+    fw_broadcast_binary_layer_param->scale[i] = (uint8_t)muls->at(i);
+    fw_broadcast_binary_layer_param->rshift_num[i] = (uint8_t)rs->at(i);
+    auto dtype = BM1684::getDataType(getInputs()[i]);
+    if (dtype == DTYPE_INT8)
+      fw_broadcast_binary_layer_param->opd_sign[i] = 1;
+    else if (dtype == DTYPE_UINT8)
+      fw_broadcast_binary_layer_param->opd_sign[i] = 0;
+    else if (dtype == DTYPE_INT16)
+      fw_broadcast_binary_layer_param->opd_sign[i] = 2;
+    else if (dtype == DTYPE_UINT16)
+      fw_broadcast_binary_layer_param->opd_sign[i] = 3;
+  }
+
+  int a_shape[MAX_SHAPE_DIMS], b_shape[MAX_SHAPE_DIMS];
+  module::getGlobalShape(getInputs()[0], a_shape);
+  module::getGlobalShape(getInputs()[1], b_shape);
+  fw_broadcast_binary_layer_param->a_dims =
+      module::getShape(getInputs()[0]).size();
+  memcpy(&(fw_broadcast_binary_layer_param->a_shape[0]), &a_shape[0],
+         MAX_SHAPE_DIMS * sizeof(int));
+  fw_broadcast_binary_layer_param->b_dims =
+      module::getShape(getInputs()[1]).size();
+  memcpy(&(fw_broadcast_binary_layer_param->b_shape[0]), &b_shape[0],
+         MAX_SHAPE_DIMS * sizeof(int));
 }
