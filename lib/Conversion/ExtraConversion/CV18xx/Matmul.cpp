dda6215ff3eb442516110acf433db467e479e070
shenglin.li <shenglin.li01@sophgo.com>
2023-04-20 16:48:43 +0800

    Support test_torch Attention.
    Change-Id: If2663b8add3e66070621f5982c44023cadeeb2c0

--- lib/Conversion/ExtraConversion/CV18xx/Matmul.cpp
+++ lib/Conversion/ExtraConversion/CV18xx/Matmul.cpp
@@ -17,10 +17,41 @@ LogicalResult ConvertMatMulWithRightTranspose::matchAndRewrite(
     top::MatMulOp op, PatternRewriter &rewriter) const {
   auto left = op.getInput();
   auto right = op.getRight();
-  auto out = module::getNextOp(op);
-
   std::vector<int64_t> lshape = module::getShape(left);
   std::vector<int64_t> rshape = module::getShape(right);
+  auto out = module::getNextOp(op);
+
+  //if right is weight and need transpose, do it here.
+  if (op.getRightTranspose() && isa<top::WeightOp>(right.getDefiningOp())) {
+    std::string filter_name = module::getName(right).str();
+    auto filterOp = dyn_cast<top::WeightOp>(right.getDefiningOp());
+    auto filter_f32 = filterOp.read<float>();
+    int64_t filter_dims = rshape.size();
+    auto p = op.parseParam();
+    int64_t K = p.K, N = p.N;
+    int64_t batch = std::accumulate(rshape.begin(), rshape.end() - 2,
+                                    1, std::multiplies<int64_t>());
+    assert(rshape[filter_dims - 2] == N && rshape[filter_dims - 1] == K && batch == p.batch);
+    //transpose filter of last two dims
+    std::vector<float> new_filter(filter_f32->size());
+    for (int64_t b = 0; b < batch; b++) {
+      for (int i = 0; i < N; i++) {
+        for (int j = 0; j < K; j++) {
+          int64_t idx1 = b * N * K + i * K + j;
+          int64_t idx2 = b * N * K + j * N + i;
+          new_filter[idx1] = filter_f32->at(idx2);
+        }
+      }
+    }
+    //swap last two dims to get new_rshape
+    std::swap(rshape[filter_dims - 2], rshape[filter_dims - 1]);
+    auto new_filter_type = RankedTensorType::get(rshape, rewriter.getF32Type());
+    auto new_filter_op = top::WeightOp::create(op, filter_name, new_filter, new_filter_type);
+    op.setOperand(1, new_filter_op);
+    op.setRightTranspose(false);
+    return success();
+  }
+
   if (lshape.size() != 4 || rshape.size() != 4) {
     return failure();
   }
@@ -72,6 +103,5 @@ LogicalResult ConvertMatMulWithRightTranspose::matchAndRewrite(
   }
   return match ? success() : failure();
 }
-
 } // namespace cv18xx
 } // namespace tpu_mlir
