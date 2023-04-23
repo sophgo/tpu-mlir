0683ad944438ccbaf0c6d3df75da7d236375f1b9
yifei.gao <yifei.gao@sophgo.com>
2023-04-23 14:28:00 +0800

    masked fill support broadcast
    Change-Id: I683f71a5c34d1670c13f54e46496a06a3f04e916

--- lib/Dialect/Tpu/Interfaces/Common/MaskedFill.cpp
+++ lib/Dialect/Tpu/Interfaces/Common/MaskedFill.cpp
@@ -21,14 +21,38 @@ void tpu::MaskedFillOp::deinit(InferenceParameter &p) {}
 
 LogicalResult tpu::MaskedFillOp::inference(InferenceParameter &p) {
   const auto num_element = module::getNumElements(getOutput());
+  const auto in_num_element = module::getNumElements(getOperand(0));
+  const auto brn_num_element = module::getNumElements(getOperand(1));
+  std::vector<int64_t> output_shape = module::getShape(getOutput());
+  std::vector<int64_t> input_shape = module::getShape(getOperand(0));
+  std::vector<int64_t> brn_shape = module::getShape(getOperand(1));
+  bool in_broadcast = false;
+  bool brn_broadcast = false;
+  if (in_num_element != num_element) {
+    assert(input_shape.size() == output_shape.size());
+    in_broadcast = true;
+  }
+  if (brn_num_element != num_element) {
+    assert(brn_shape.size() == output_shape.size());
+    brn_broadcast = true;
+  }
+
   const float const_val_ = getConstVal().convertToDouble();
-  const float* in = p.inputs[0];
-  const float* brn = p.inputs[1];
+  const float *in = p.inputs[0];
+  const float *brn = p.inputs[1];
 #pragma omp parallel for schedule(static, omp_schedule(num_element))
   for (int i = 0; i < num_element; ++i) {
-    const float tbrn = getInversed() ? const_val_ : brn[i];
-    const float fbrn = getInversed() ? brn[i] : const_val_;
-    p.outputs[0][i] = in[i] ? tbrn : fbrn;
+    int in_index = i;
+    int brn_index = i;
+    if (in_broadcast) {
+      in_index = getBcastIndex(i, output_shape, input_shape);
+    }
+    if (brn_broadcast) {
+      brn_index = getBcastIndex(i, output_shape, brn_shape);
+    }
+    const float tbrn = getInversed() ? const_val_ : brn[brn_index];
+    const float fbrn = getInversed() ? brn[brn_index] : const_val_;
+    p.outputs[0][i] = in[in_index] ? tbrn : fbrn;
   }
   return success();
 }
