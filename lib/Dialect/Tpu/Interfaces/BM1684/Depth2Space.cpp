95b1f8f1f630925289c63cce7e3a4f8df1eddb05
xueru.chen <xueru.chen@sophgo.com>
2023-04-27 18:28:05 +0800

    support int8 depth2space for bm1684
    Change-Id: I700a538285ac02940f0253e69eeb654aec7df530

--- lib/Dialect/Tpu/Interfaces/BM1684/Depth2Space.cpp
+++ lib/Dialect/Tpu/Interfaces/BM1684/Depth2Space.cpp
@@ -7,12 +7,10 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "tpu_mlir/Dialect/Tpu/IR/TpuOps.h"
 #include "tpu_mlir/Backend/BM168x/BM1684.h"
+#include "tpu_mlir/Dialect/Tpu/IR/TpuOps.h"
 
 #include "tpu_mlir/Support/Module.h"
-
-
 
 using namespace tpu_mlir::backend;
 
@@ -22,41 +20,28 @@ void tpu::Depth2SpaceOp::codegen_global_bm1684() {
   auto input_addr = module::getAddress(input);
   auto output_addr = module::getAddress(output);
   int input_dims = module::getShape(input).size();
-  auto input_dtype = BM1684::getDataType(input);
-  auto gdma_format = BM1684::GDMA_VALUE_FORMAT_FLOAT32;
-  // melloc
-  int *input_shape = new int[MAX_SHAPE_DIMS];
-  int *block_sizes = new int[MAX_SHAPE_DIMS];
-  // assign param and call func
+  int input_shape[MAX_SHAPE_DIMS] = {0};
   int64_t in, ic, ih, iw;
   module::getNCHW(getInput(), in, ic, ih, iw, false);
   input_shape[0] = (int32_t)in;
   input_shape[1] = (int32_t)ic;
   input_shape[2] = (int32_t)ih;
   input_shape[3] = (int32_t)iw;
-  block_sizes[0] = getBlockH();
-  block_sizes[1] = getBlockW();
-
-  assert((input_dtype != DTYPE_INT8 && input_dtype != DTYPE_UINT8) && "not support int8");
-
+  int block_sizes[2] = {(int)getBlockH(), (int)getBlockW()};
 
   BM1684::instance().dl_nodechip_depth2space_mlir(
-        input_addr, output_addr, input_shape, input_dims, block_sizes,
-        getInIs_NCHW(), getOutIs_NCHW(), getIsInversed(), getIs_CRD(), getSwapCr(),
-        gdma_format, (CMD_ID_NODE *)BM1684::instance().cmdid_node);
-  // release
-  delete[] input_shape;
-  delete[] block_sizes;
+      input_addr, output_addr, input_shape, input_dims, block_sizes,
+      getInIs_NCHW(), getOutIs_NCHW(), getIsInversed(), getIs_CRD(),
+      getSwapCr(), module::getDtypeSize(input),
+      (CMD_ID_NODE *)BM1684::instance().cmdid_node);
 }
 
 // ======================================
 // Dynamic GlobalGenInterface
 // ======================================
-uint32_t tpu::Depth2SpaceOp::dyn_codegen_global_bm1684(void* ir_layer_info) {
+uint32_t tpu::Depth2SpaceOp::dyn_codegen_global_bm1684(void *ir_layer_info) {
   llvm_unreachable("Not Implemented");
   return 0;
 }
 
-int64_t tpu::Depth2SpaceOp::get_fw_type_bm1684() {
-  return -1;
-}
+int64_t tpu::Depth2SpaceOp::get_fw_type_bm1684() { return -1; }
