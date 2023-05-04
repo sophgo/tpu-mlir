d7e406744c01db30b878934f6430fe0270e09759
heng.yang <heng.yang@sophgo.com>
2023-05-04 11:54:00 +0800

    [mlir 1684]fix interp torch
    Change-Id: Iae1d5b2f9cdbacd71c39fed128110925ee1f494d

--- lib/Dialect/Tpu/Interfaces/BM1684/Interp.cpp
+++ lib/Dialect/Tpu/Interfaces/BM1684/Interp.cpp
@@ -50,8 +50,8 @@ void tpu::InterpOp::codegen_global_bm1684() {
             half_pixel_centers = 1;
             align_corners = 0;
         } else if (getCoordMode() == tpu::ResizeCoordMode::pytorch_half_pixel){
-            platform_sp = PYTORCH_SUPPORT;
-            half_pixel_centers = 0;
+            platform_sp = PYTORCH_NEAREST;
+            half_pixel_centers = 1;
             align_corners = 0;
         } else{
             llvm_unreachable("BM1684 DO NOT Support Such Attribute.!!!!");
