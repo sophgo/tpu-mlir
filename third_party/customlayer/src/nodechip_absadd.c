d5b7044c1172ad4f633b2553dfe06340de6b7faf
zhibin.xin <zhibin.xin@sophgo.com>
2023-05-21 15:31:28 +0800

    add absadd, ceiladd custom op samples
    Change-Id: I4f25911c91263aa85589807377e81c8884e96271

---
+++ third_party/customlayer/src/nodechip_absadd.c
@@ -0,0 +1,21 @@
+#include "nodechip_absadd.h"
+#include "common.h"
+#include "tpu_kernel.h"
+
+void nodechip_absadd_f32_global(
+    global_addr_t input_global_addr,
+    global_addr_t output_global_addr,
+    const int *shape,
+    float b_val,
+    data_type_t dtype)
+{
+  local_addr_t local_in_addr = 0;
+  local_addr_t local_mid_addr = BANK_SIZE * 4;
+  local_addr_t local_out_addr = BANK_SIZE * 8;
+  dim4 _shape = {.n = shape[0], .c = shape[1], .h = shape[2], .w = shape[3]};
+  tpu_gdma_cpy_S2L(local_in_addr, input_global_addr, &_shape, NULL, NULL, dtype);
+  tpu_bdc_abs(local_mid_addr, local_in_addr, &_shape, NULL, NULL, dtype);
+  scalar_t C = {.f32 = b_val};
+  tpu_bdc_fp_add_C(local_out_addr, local_mid_addr, C, &_shape, NULL, NULL, dtype);
+  tpu_gdma_cpy_L2S(output_global_addr, local_out_addr, &_shape, NULL, NULL, dtype);
+}
