f4bbc4fe56b67b0852aec4ef23e8d64aa75464b3
zhibin.xin <zhibin.xin@sophgo.com>
2023-05-22 11:06:09 +0800

    main_entry update
    1. print only the last 100 lines of failed log
    2. put the script test log in the file as well
    3. print success cases first then failed
    
    Change-Id: Ifc40a930c60a3f1c4e33ca6df091bd0285e1f3e9

--- regression/main_entry.py
+++ regression/main_entry.py
@@ -20,6 +20,7 @@ import test_tflite
 import test_onnx
 import argparse
 import logging
+from utils.mlir_shell import _os_system_log
 
 
 class MAIN_ENTRY(object):
@@ -39,9 +40,9 @@ class MAIN_ENTRY(object):
             "tpulang":  (test_tpulang.TPULANG_IR_TESTER, test_tpulang.test_all, ["bm1684x"]),
         }
         # yapf: enable
-        self.model_cases = set()
         self.results = []
         self.time_cost = []
+        self.logger = logging.getLogger()
 
     def add_result(self, test_name: str, status: bool, error_cases: list = []):
         self.results.append({
@@ -55,14 +56,10 @@ class MAIN_ENTRY(object):
         # set the file for saving output stream
         log_filename = case_name + ".log"
 
-        logging.basicConfig(filename=log_filename,
-                            filemode='w',
-                            level=logging.DEBUG,
-                            format='%(message)s')
-
-        file_handler = logging.FileHandler(log_filename)
+        file_handler = logging.FileHandler(filename=log_filename, mode="w")
         file_handler.setLevel(logging.DEBUG)
         file_handler.setFormatter(logging.Formatter('%(message)s'))
+        self.logger.addHandler(file_handler)
 
         print(f"======= run_models.py {model_name} {chip} {self.test_type}=====")
         dir = os.path.expandvars(f"$REGRESSION_PATH/regression_out/{model_name}_{chip}")
@@ -81,20 +78,21 @@ class MAIN_ENTRY(object):
         })
         os.chdir(self.current_dir)
 
+        self.logger.removeHandler(file_handler)
         file_handler.close()
         return ret == 0
 
     def run_op_test(self, op_source, tester, test_all_func, chip):
         print(f"======= test_{op_source}.py ======")
-        dir = f"{op_source}_test_{chip}"
-        os.makedirs(dir, exist_ok=True)
+        case_name = f"{op_source}_test_{chip}"
+        os.makedirs(case_name, exist_ok=True)
         os.chdir(dir)
         if op_source == "tflite" or op_source == "tpulang":
             tester = tester(chip=chip)
         else:
             tester = tester(chip=chip, simple=self.is_basic)
         error_cases = test_all_func(tester)
-        self.add_result(f"test_{op_source}.py {chip}", not error_cases, error_cases)
+        self.add_result(case_name, not error_cases, error_cases)
         os.chdir(self.current_dir)
 
         return not error_cases
@@ -102,9 +100,24 @@ class MAIN_ENTRY(object):
     def run_script_test(self):
         # run scripts under $REGRESSION_OUT/script_test
         print("======= script test ======")
-        ret = os.system(os.path.expandvars("$REGRESSION_PATH/script_test/run.sh"))
-        self.add_result("script_test", ret == 0)
-        return ret == 0
+        case_name = "script_test"
+
+        file_handler = logging.FileHandler(filename=case_name + ".log", mode="w")
+        file_handler.setLevel(logging.DEBUG)
+        file_handler.setFormatter(logging.Formatter('%(message)s'))
+        self.logger.addHandler(file_handler)
+
+        success = True
+        try:
+            _os_system_log(os.path.expandvars("$REGRESSION_PATH/script_test/run.sh"))
+        except:
+            success = False
+
+        self.add_result(case_name, success)
+
+        self.logger.removeHandler(file_handler)
+        file_handler.close()
+        return success
 
     def run_all(self):
         start_time = time.time()
@@ -139,7 +152,6 @@ class MAIN_ENTRY(object):
             if self.disable_thread:
                 finished_list = list()
                 for model in cur_model_list:
-                    self.model_cases.add(f"{model}_{chip}")
                     self.run_regression_net(model, chip, finished_list)
                 self.results.extend(finished_list)
                 if self.is_basic:
@@ -151,7 +163,6 @@ class MAIN_ENTRY(object):
                 processes = []
                 finished_list = multiprocessing.Manager().list()
                 for model in cur_model_list:
-                    self.model_cases.add(f"{model}_{chip}")
                     p = multiprocessing.Process(target=self.run_regression_net,
                                                 args=(model, chip, finished_list))
                     processes.append(p)
@@ -194,22 +205,38 @@ if __name__ == "__main__":
     os.makedirs(dir, exist_ok=True)
     os.chdir(dir)
 
+    logging.basicConfig(filename="main.log",
+                        filemode='w',
+                        level=logging.DEBUG,
+                        format='%(message)s')
+
     main_entry = MAIN_ENTRY(args.test_type, args.disable_thread)
 
     exit_status = main_entry.run_all()
 
-    # print log of failed cases first
+    # print last 100 lines log of failed cases first
     for result in main_entry.results:
-        if result["name"] in main_entry.model_cases and result["status"] == "FAILED":
+        if result["name"].startswith(tuple(main_entry.op_test_types.keys())):
+            break
+        if result["status"] == "FAILED":
             with open(result["name"] + ".log", 'r') as file:
-                content = file.read()
+                lines = file.readlines()
+                last_100_lines = lines[-100:]
+                content = ''.join(last_100_lines)
                 print(content)
 
     for time in main_entry.time_cost:
         print(time)
+
     for result in main_entry.results:
-        print("{} {}".format(result["name"], result["status"]))
-        if result["error_cases"]:
-            print("Failed cases: ", result["error_cases"])
+        if result["status"] == "PASSED":
+            print("{} {}".format(result["name"], result["status"]))
+
+    for result in main_entry.results:
+        if result["status"] == "FAILED":
+            print("{} {}".format(result["name"], result["status"]))
+            if result["error_cases"]:
+                print("Failed cases: ", result["error_cases"])
+
     print("TEST {} {}".format(args.test_type, "PASSED" if not exit_status else "FAILED"))
     exit(exit_status)
