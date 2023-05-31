0b0746236e0beff3197b4da440259e6663fe2d6c
pengchao.hu <pengchao.hu@sophgo.com>
2023-05-31 11:09:22 +0800

    bayer2rgb
    Change-Id: I52ca341f7855801c385ae76044f1660d1829c53a

---
+++ python/samples/bayer2rgb/convert.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+mkdir -p workspace
+pushd workspace
+
+python3 ../model.py
+
+model_transform.py \
+    --model_name bayer2rgb \
+    --model_def bayer2rgb.pt \
+    --input_shapes [[1,1,1024,1024]] \
+    --test_input input.npz \
+    --test_result bayer2rgb_top_outputs.npz \
+    --mlir bayer2rgb.mlir
+
+mkdir -p dataset
+cp input.npz dataset/
+
+run_calibration.py bayer2rgb.mlir \
+  --dataset dataset/ \
+  --input_num 1 \
+  -o bayer2rgb_cali_table
+
+model_deploy.py \
+  --mlir bayer2rgb.mlir \
+  --quantize INT8 \
+  --calibration_table bayer2rgb_cali_table\
+  --chip bm1684x \
+  --quant_input \
+  --quant_output \
+  --test_input input.npz \
+  --test_reference bayer2rgb_top_outputs.npz \
+  --model bayer2rgb.bmodel
+
+popd
+
