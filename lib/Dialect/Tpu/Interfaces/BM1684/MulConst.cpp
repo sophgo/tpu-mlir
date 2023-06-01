c775798f2c4cd43f8aab3ccf7bcee7ff6a84b423
xueru.chen <xueru.chen@sophgo.com>
2023-06-01 14:29:19 +0800

    [BM1684]: fix local buffer size bug
    Change-Id: Ic5dca898fbaeba194fad565149df7f058681906e

--- lib/Dialect/Tpu/Interfaces/BM1684/MulConst.cpp
+++ lib/Dialect/Tpu/Interfaces/BM1684/MulConst.cpp
@@ -10,6 +10,7 @@
 #include "tpu_mlir/Backend/BM168x/BM1684.h"
 #include "tpu_mlir/Dialect/Tpu/IR/TpuOps.h"
 #include "tpu_mlir/Dialect/Tpu/Transforms/Codegen/Dynamic/DynamicLayer.hpp"
+#include "tpu_mlir/Support/MathUtils.h"
 #include "tpu_mlir/Support/Module.h"
 
 using namespace tpu_mlir::backend;
@@ -22,7 +23,8 @@ void tpu::MulConstOp::codegen_global_bm1684() {
     auto b0_rshift = getRshift();
     int is_signs[3] = {module::isSign(getInput()), 1,
                        module::isSign(getOutput())};
-    int is_int8s[3] = {1, 0, 1};
+    int is_int8s[3] = {module::getStorageType(getInput()).isInteger(8), 0,
+                       module::getStorageType(getOutput()).isInteger(8)};
     int b0_shape[4] = {(int)n, (int)c, (int)h, (int)w};
     int16_t b1_val = (int16_t)getConstVal().convertToDouble();
     BM1684::instance().dl_nodechip_const_binary_fix8b_forward_parallel(
@@ -45,9 +47,12 @@ int64_t tpu::MulConstOp::getBufferSize_bm1684(
   int64_t buffer_size = 0;
   auto dtype_i = BM168x::getDataType(getInput());
   if (dtype_i == DTYPE_INT8 || dtype_i == DTYPE_UINT8) {
-    if (getMultiplier() != 1 || getRshift() != 0) {
-      buffer_size = in_lmem_bytes * 2;
-    }
+    int64_t n, c, h, w;
+    module::getNCHW(getInput(), n, c, h, w);
+    auto EU_NUM = BM1684::eu_num(sizeof(int32_t));
+    buffer_size = ceiling_func(in_hslice, 2) *
+                  ceiling_func(c, BM1684::NPU_NUM) *
+                  align_up(in_hslice * w, EU_NUM) * sizeof(int);
   }
   return buffer_size;
 }
@@ -68,7 +73,8 @@ void tpu::MulConstOp::codegen_local_bm1684(int64_t n_step, int64_t h_step,
     auto b0_rshift = getRshift();
     int is_signs[3] = {module::isSign(getInput()), 1,
                        module::isSign(getOutput())};
-    int is_int8s[3] = {1, 0, 1};
+    int is_int8s[3] = {module::getStorageType(getInput()).isInteger(8), 0,
+                       module::getStorageType(getOutput()).isInteger(8)};
     BM1684::instance().dl_nodechip_const_binary_fix8b_forward_local(
         in_g_info.out_addr, out_g_info.out_addr, out_g_info.buffer_addr, b1_val,
         b0_shape, 4, BINARY_MUL, b0_mul, 0, b0_rshift, 0, 0, getDoRelu(),
