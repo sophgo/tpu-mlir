8941284306ff0ee89a52d56d7aa3e5d11f850ed9
shenglin.li <shenglin.li01@sophgo.com>
2023-06-07 17:02:52 +0800

    Align cv18xx detect samples and YOLODetection Func.
    Change-Id: I530aed88c1fce673656e03581848280b9fe6e2e0

--- python/samples/detect_pp_yoloe.py
+++ python/samples/detect_pp_yoloe.py
@@ -177,15 +177,6 @@ def postproc(outputs, imsize, top, left, anchors=ANCHORS):
     return scores, boxes_xyxy
 
 
-def refine_cvi_output(output):
-    new_output = {}
-    for k in output.keys():
-        if k.endswith("_f32"):
-            out = output[k]
-            n, c, h, w = out.shape[0], out.shape[1], out.shape[2], out.shape[3]
-            new_output[k] = out.reshape(n, c, h, w // 85, 85)
-    return new_output
-
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Inference Yolo v5 network.')
@@ -235,9 +226,10 @@ def main():
     elif args.model.endswith('.mlir'):
         output = mlir_inference(data, args.model, False)
     elif args.model.endswith(".bmodel") or args.model.endswith(".cvimodel"):
-        output = model_inference(data, args.model)
         if args.model.endswith(".cvimodel"):
-            output = refine_cvi_output(output)
+            raise RuntimeError("not support cvimodel now.")
+        output = model_inference(data, args.model)
+
     else:
         raise RuntimeError("not support modle file:{}".format(args.model))
     dets = output['multiclass_nms3_0.tmp_0']
