4481bf87f0d2be5cbe106a732571b97a347caf49
chen.chen <chen.chen@sophgo.com>
2023-07-27 13:46:15 +0800

    minor bug fix
    mobilebert_tf fails after my previous commit, this fixes it.
    
    Bug Cause:
    Segmentation fault when trying accessing a slice op's user, which has no
    user and should be erased.
    Recommend actively doing rewriter.eraseOp to avoid potential bugs.
    
    Caution:
    When modifying users of op, do not iterate on op->getUsers().
    Iterate on auto users = op->getUsers() is also buggy.
    Please cache the users into a vector and iterate on it.
    
    Change-Id: I3323a6dd65b1ea7dc6e71c9c6e13a66c3169bab9

--- lib/Dialect/Top/Canonicalize/Slice.cpp
+++ lib/Dialect/Top/Canonicalize/Slice.cpp
@@ -20,11 +20,23 @@ struct SplitSlicePattern : public OpRewritePattern<SliceOp> {
     if (op->hasOneUse()) {
       return failure();
     }
-    const auto users = op->getUsers();
-    for (const auto user: users) {
+    // ****** CAUTION ******
+    // Method 1:
+    // auto users = op->getUsers();
+    // for (auto user: users) {
+    //  *** remove users ***
+    // }
+    // Method 2:
+    // for (auto user: op->getUsers()) {
+    //  *** remove users ***
+    // }
+    // Both methods might cause problems! We should cache the users first!
+    std::vector<mlir::Operation *> users;
+    for (auto user: op->getUsers()) {
       if (!isa<SliceOp>(user)) {
         return failure();
       }
+      users.emplace_back(user);
     }
     const auto& opd = op->getOperand(0);
     const auto& res = op->getResult(0);
@@ -50,6 +62,7 @@ struct SplitSlicePattern : public OpRewritePattern<SliceOp> {
       user->eraseOperand(0);
       user->insertOperands(0, slice_result_var);
     }
+    rewriter.eraseOp(op);
     return success();
   }
 };
@@ -115,6 +128,7 @@ struct NoUseSlicePattern : public OpRewritePattern<SliceOp> {
       }
     }
     op.getOutput().replaceAllUsesWith(op.getInput());
+    rewriter.eraseOp(op);
     return success();
   }
 };
