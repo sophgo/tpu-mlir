4aba3121b2a852d0e4e8ef0c16ac760ceb1e7ca2
shanglin.guo <shanglin.guo@sophgo.com>
2023-08-03 20:22:13 +0800

    support AutoIncrease Op(++i in-place)
    1. bugfix: align with onnxruntime when no_loop
    2. support do_while/while/for
    3. support AutoIncrease Op(++i in-place)
    
    Change-Id: I7bf308f5553d9d6c0520393ceaf634d761a21840

---
+++ lib/Dialect/Tpu/Interfaces/BM1684X/AutoIncrease.cpp
@@ -0,0 +1,59 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
+//
+// TPU-MLIR is licensed under the 2-Clause BSD License except for the
+// third-party components.
+//
+//===----------------------------------------------------------------------===//
+
+#include "tpu_mlir/Dialect/Tpu/Transforms/Codegen/Dynamic/DynamicLayer.hpp"
+
+using namespace tpu_mlir::backend;
+
+// =========================================
+// GlobalGenInterface
+// =========================================
+
+// int8
+void tpu::AutoIncreaseOp::codegen_global_bm1684x() {
+  int64_t n, c, h, w;
+  module::getNCHW(getOutput(), n, c, h, w);
+  auto op = getOperation();
+  auto input_spec = BM168x::get_input_spec(op);
+  auto output_spec = BM168x::get_output_spec(op);
+  auto input_type = module::getStorageType(getInput());
+  constbinary_global_spec_t param = {0};
+  param.common.binary_type = BINARY_ADD;
+  param.common.B_const_val = getConstVal().convertToDouble();
+  param.common.inversed = 0;
+  param.common.scale_A = 1;
+  param.common.rshift_A = 0;
+  param.common.B_dtype =
+        input_type.isa<FloatType>() ? DTYPE_FP32 : DTYPE_INT32;
+
+  BM168x::call_global_func("backend_api_constbinary_global", &param,
+                           sizeof(param), input_spec->data(),
+                           output_spec->data());
+}
+
+// ======================================
+// Dynamic GlobalGenInterface
+// ======================================
+int64_t tpu::AutoIncreaseOp::dyn_codegen_global_bm1684x(void *buffer) {
+  if (!buffer)
+    return sizeof(constbinary_global_spec_t);
+  auto input_type = module::getStorageType(getInput());
+  constbinary_global_spec_t param = {0};
+  param.common.binary_type = BINARY_ADD;
+  param.common.B_const_val = getConstVal().convertToDouble();
+  param.common.inversed = 0;
+  param.common.scale_A = 1;
+  param.common.rshift_A = 0;
+  param.common.B_dtype =
+        input_type.isa<FloatType>() ? DTYPE_FP32 : DTYPE_INT32;
+
+  return BM168x::dynamic_spec_to_buffer(buffer, param);
+}
+
+int64_t tpu::AutoIncreaseOp::get_fw_type_bm1684x() { return FW_BMNET_CONST_BINARY; }
---
+++ lib/Dialect/Tpu/Interfaces/Common/AutoIncrease.cpp
@@ -0,0 +1,23 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
+//
+// TPU-MLIR is licensed under the 2-Clause BSD License except for the
+// third-party components.
+//
+//===----------------------------------------------------------------------===//
+
+
+#include "tpu_mlir/Support/MathUtils.h"
+
+LogicalResult tpu::AutoIncreaseOp::init(InferenceParameter &p) {
+  return success();
+}
+
+void tpu::AutoIncreaseOp::deinit(InferenceParameter &p) {
+}
+
+LogicalResult tpu::AutoIncreaseOp::inference(InferenceParameter &p) {
+  // assistanted op, don;t need to implement it
+  return success();
+}
