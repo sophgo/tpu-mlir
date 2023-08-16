d4cba6e083fcd7a9a2432266d8b81979f817c06b
shaoxiong.xiang <shaoxiong.xiang@sophgo.com>
2023-08-16 11:19:29 +0800

    [18xx] fix reduce accuracy issue
    Change-Id: Ide25766d133c523480c1943cd06ca1382c13db99

--- lib/Conversion/TopToTpu/CV18xx/Reduce.cpp
+++ lib/Conversion/TopToTpu/CV18xx/Reduce.cpp
@@ -6,12 +6,111 @@
 // third-party components.
 //
 //===----------------------------------------------------------------------===//
+#include "tpu_mlir/Backend/CV18xx/CV18xx.h"
 #include "tpu_mlir/Conversion/TopToTpu/LoweringCV18xx.h"
+
+using namespace tpu_mlir::backend;
 
 namespace tpu_mlir {
 namespace cv18xx {
+
+static bool valid_shape(int axis_dims, cvk_fmt_t fmt) {
+  assert(axis_dims > 0 && "axis_dims should > 0.");
+  int h, w;
+  CV18xx::size_to_hw(axis_dims, h, w);
+  auto in_shape = CV18xx::tl_shape_t4(1, (int)CV18xx::NPU_NUM, h, w);
+  auto out_shape = CV18xx::tl_shape_t4(1, (int)CV18xx::NPU_NUM, 1, 1);
+  auto input_size = CV18xx::lmem_tensor_to_size(in_shape, fmt, 1);
+  auto output_size = CV18xx::lmem_tensor_to_size(out_shape, fmt, 1);
+  return (uint64_t)(input_size) < ((CV18xx::LMEM_BYTES - 2 * output_size) / 2);
+}
+
+static bool splitReduce(PatternRewriter &rewriter, top::ReduceOp reduceOp) {
+  // for ppyoloe reduce 1x48x160x160 --> 1x48x1x1
+  if (reduceOp.getMode() == "ReduceL2") {
+    return false;
+  }
+  backend::Arch::init(0);
+  std::vector<int64_t> axes_v;
+  std::vector<std::vector<int64_t>> outputs_shape_v, new_axes_v;
+  auto axes = module::getI64Array(reduceOp.getAxes());
+  axes_v.assign(axes->begin(), axes->end());
+  int32_t start_axis = axes_v.at(0);
+  int32_t end_axis = axes_v.back() + 1;
+  auto input_shape = module::getShape(reduceOp.getInput()).vec();
+  auto num_axes = axes_v.size();
+
+  int32_t inner_dims =
+      std::accumulate(input_shape.begin() + end_axis, input_shape.end(), 1,
+                      std::multiplies<int64_t>());
+  if (num_axes == 1 || inner_dims > 1) {
+    // TODO
+    return false;
+  }
+  int32_t axis_dims = std::accumulate(input_shape.begin() + start_axis,
+                                      input_shape.begin() + end_axis, 1,
+                                      std::multiplies<int64_t>());
+
+  auto fmt = CV18xx::getDataType(reduceOp.getInput());
+  if (valid_shape(axis_dims, fmt)) {
+    return false;
+  }
+  // prevent accuracy issue
+  auto eltType = rewriter.getBF16Type();
+  fmt = CVK_FMT_BF16;
+  for (int32_t i = num_axes - 1; i > 0; i--) {
+    int32_t axis = axes_v.at(i);
+    axes_v.pop_back();
+    new_axes_v.push_back({axis});
+    input_shape[axis] = 1;
+    outputs_shape_v.push_back(input_shape);
+    end_axis = axes_v.back() + 1;
+    axis_dims = std::accumulate(input_shape.begin() + start_axis,
+                                input_shape.begin() + end_axis, 1,
+                                std::multiplies<int64_t>());
+    if (valid_shape(axis_dims, fmt)) {
+      new_axes_v.push_back(axes_v);
+      outputs_shape_v.push_back(module::getShape(reduceOp.getOutput()).vec());
+      break;
+    }
+  }
+
+  if (!valid_shape(axis_dims, fmt)) {
+    // TODO. Reshape the reduce op to valid
+    llvm_unreachable("reduce's axis_dims is too large.");
+  }
+
+  // creat Op
+  rewriter.setInsertionPointAfter(reduceOp);
+  std::vector<Value> operands;
+  std::vector<NamedAttribute> attrs;
+  // auto eltType = module::getElementType(reduceOp.getOutput());
+  auto noneOp = module::getNoneOp(reduceOp);
+  operands.push_back(reduceOp.getInput());
+  operands.push_back(noneOp);
+  operands.push_back(noneOp);
+  Value newValue = reduceOp.getOutput();
+  for (uint32_t i = 0; i < new_axes_v.size(); i++) {
+    auto newType = RankedTensorType::get(outputs_shape_v[i], eltType);
+    Location loc = reduceOp.getLoc();
+    if (i != new_axes_v.size() - 1) {
+      loc = module::getLocLike(reduceOp.getOutput(), std::to_string(i));
+    }
+    auto newOp = rewriter.create<tpu::ReduceOp>(loc, newType, operands,
+                                                reduceOp->getAttrs());
+    newOp->setAttr("axes", rewriter.getI64ArrayAttr(new_axes_v[i]));
+    newValue = newOp.getOutput();
+    operands[0] = newValue;
+  }
+  rewriter.replaceOp(reduceOp, {newValue});
+  return true;
+};
+
 void ReduceLowering::LoweringINT8(PatternRewriter &rewriter, top::ReduceOp op,
                                   bool asymmetric) const {
+  if (splitReduce(rewriter, op)) {
+    return;
+  }
   std::vector<Value> operands;
   std::vector<int64_t> rshift_v(1, 0);
   std::vector<int64_t> multiplier_v(1, 1);
@@ -62,6 +161,9 @@ void ReduceLowering::LoweringINT8(PatternRewriter &rewriter, top::ReduceOp op,
 
 void ReduceLowering::LoweringBF16(PatternRewriter &rewriter,
                                   top::ReduceOp op) const {
+  if (splitReduce(rewriter, op)) {
+    return;
+  }
   std::vector<Value> operands;
   auto mode = op.getMode();
   operands.push_back(op.getInput());
--- lib/Dialect/Tpu/Transforms/ChipOptimize/OptimizeCV18XX.cpp
+++ lib/Dialect/Tpu/Transforms/ChipOptimize/OptimizeCV18XX.cpp
@@ -8,8 +8,8 @@
 //===----------------------------------------------------------------------===//
 
 #include "Common.h"
-#include "tpu_mlir/Dialect/Tpu/IR/TpuOps.h"
 #include "tpu_mlir/Backend/CV18xx/CV18xx.h"
+#include "tpu_mlir/Dialect/Tpu/IR/TpuOps.h"
 #include "llvm/Support/Debug.h"
 
 #define DEBUG_TYPE "optimize_cv18xx"
@@ -125,97 +125,6 @@ public:
     return failure();
   }
 };
-static bool valid_shape(int axis_dims, cvk_fmt_t fmt) {
-  int h, w;
-  CV18xx::size_to_hw(axis_dims, h, w);
-  auto in_shape = CV18xx::tl_shape_t4(1, (int)CV18xx::NPU_NUM, h, w);
-  auto out_shape = CV18xx::tl_shape_t4(1, (int)CV18xx::NPU_NUM, 1, 1);
-  auto input_size = CV18xx::lmem_tensor_to_size(in_shape, fmt, 1);
-  auto output_size = CV18xx::lmem_tensor_to_size(out_shape, fmt, 1);
-  return (uint64_t)(input_size) < ((CV18xx::LMEM_BYTES - 2 * output_size) / 2);
-}
-
-class SplitReducePattern : public OpRewritePattern<tpu::ReduceOp> {
-public:
-  using OpRewritePattern::OpRewritePattern;
-  LogicalResult matchAndRewrite(tpu::ReduceOp reduceOp,
-                                PatternRewriter &rewriter) const override {
-    // for ppyoloe reduce 1x48x160x160 --> 1x48x1x1
-    if (reduceOp.getMode() == "ReduceL2") {
-      return failure();
-    }
-    std::vector<int64_t> axes_v;
-    std::vector<std::vector<int64_t>> outputs_shape_v, new_axes_v;
-    auto axes = module::getI64Array(reduceOp.getAxes());
-    axes_v.assign(axes->begin(), axes->end());
-    int32_t start_axis = axes_v.at(0);
-    int32_t end_axis = axes_v.back() + 1;
-    auto input_shape = module::getShape(reduceOp.getInput()).vec();
-    auto num_axes = axes_v.size();
-
-    int32_t inner_dims =
-        std::accumulate(input_shape.begin() + end_axis, input_shape.end(), 1,
-                        std::multiplies<int64_t>());
-    if (num_axes == 1 || inner_dims > 1) {
-      // TODO
-      return failure();
-    }
-    int32_t axis_dims = std::accumulate(input_shape.begin() + start_axis,
-                                        input_shape.begin() + end_axis, 1,
-                                        std::multiplies<int64_t>());
-    auto fmt = CV18xx::getDataType(reduceOp.getInput());
-    if (valid_shape(axis_dims, fmt)) {
-      return failure();
-    }
-
-    for (int32_t i = num_axes - 1; i > 0; i--) {
-      int32_t axis = axes_v.at(i);
-      axes_v.pop_back();
-      new_axes_v.push_back({axis});
-      input_shape[axis] = 1;
-      outputs_shape_v.push_back(input_shape);
-      end_axis = axes_v.back() + 1;
-      axis_dims = std::accumulate(input_shape.begin() + start_axis,
-                                  input_shape.begin() + end_axis, 1,
-                                  std::multiplies<int64_t>());
-      if (valid_shape(axis_dims, fmt)) {
-        new_axes_v.push_back(axes_v);
-        outputs_shape_v.push_back(module::getShape(reduceOp.getOutput()).vec());
-        break;
-      }
-    }
-
-    if (!valid_shape(axis_dims, fmt)) {
-      // TODO. Reshape the reduce op to valid
-      llvm_unreachable("reduce's axis_dims is too large.");
-    }
-
-    // creat Op
-    rewriter.setInsertionPointAfter(reduceOp);
-    std::vector<Value> operands;
-    std::vector<NamedAttribute> attrs;
-    auto eltType = module::getElementType(reduceOp.getOutput());
-    auto noneOp = module::getNoneOp(reduceOp);
-    operands.push_back(reduceOp.getInput());
-    operands.push_back(noneOp);
-    operands.push_back(noneOp);
-    Value newValue = reduceOp.getOutput();
-    for (uint32_t i = 0; i < new_axes_v.size(); i++) {
-      auto newType = RankedTensorType::get(outputs_shape_v[i], eltType);
-      Location loc = reduceOp.getLoc();
-      if (i != new_axes_v.size() - 1) {
-        loc = module::getLocLike(reduceOp.getOutput(), std::to_string(i));
-      }
-      auto newOp = rewriter.create<tpu::ReduceOp>(loc, newType, operands,
-                                                  reduceOp->getAttrs());
-      newOp->setAttr("axes", rewriter.getI64ArrayAttr(new_axes_v[i]));
-      newValue = newOp.getOutput();
-      operands[0] = newValue;
-    }
-    rewriter.replaceOp(reduceOp, {newValue});
-    return success();
-  }
-};
 
 class SplitReluLimitPattern : public RewritePattern {
 public:
@@ -269,8 +178,8 @@ using namespace cv18xx;
 void populateOptimizeCV18XXPatterns(RewritePatternSet *patterns) {
   auto ctx = patterns->getContext();
   patterns->add<FuseLeakReluPattern, MoveConvStrideToEltwiseOpPattern,
-                SplitReluLimitPattern, SplitReducePattern,
-                PermuteReorderPattern, PermutePadSwap>(ctx);
+                SplitReluLimitPattern, PermuteReorderPattern, PermutePadSwap>(
+      ctx);
 };
 } // namespace tpu
 
