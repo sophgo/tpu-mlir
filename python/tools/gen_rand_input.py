1acd4a30221771f4636a0397a3814221824c6e04
man.lu <man.lu@sophgo.com>
2023-09-07 14:36:15 +0800

    split regression
    - add skip bmodel validation
    
    Change-Id: I34b68fb9fe87b7e3e875057976d3b31d345f90a4

--- python/tools/gen_rand_input.py
+++ python/tools/gen_rand_input.py
@@ -1,3 +1,12 @@
+#!/usr/bin/env python3
+# ==============================================================================
+#
+# Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
+#
+# TPU-MLIR is licensed under the 2-Clause BSD License except for the
+# third-party components.
+#
+# ==============================================================================
 import abc
 import numpy as np
 import argparse
