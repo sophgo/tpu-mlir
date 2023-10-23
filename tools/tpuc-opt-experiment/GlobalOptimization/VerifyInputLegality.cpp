c5311f6d13decc592a12a72f23f6ede3986ad7d9
shanglin.guo <shanglin.guo@sophgo.com>
2023-10-23 21:01:10 +0800

    support interchange of inner loop, padOp tranform, tensor op collapse, fusion on linalg-on-tensor etc
    Change-Id: Iebb9981046f03902e7ce448a1456bd169ba99403

---
+++ tools/tpuc-opt-experiment/GlobalOptimization/VerifyInputLegality.cpp
@@ -0,0 +1,82 @@
+#include "Passes.h"
+#include "llvm/ADT/StringExtras.h"
+#include "llvm/Support/FormatVariadic.h"
+namespace  mlir
+{
+// make sure the tosa/stablehlo dialect have lowered to linalg-on-tensor before subgraph split pipeline
+class VerifyInputLegalityPass
+    : public PassWrapper<VerifyInputLegalityPass, OperationPass<ModuleOp>> {
+public:
+  StringRef getArgument() const override {
+    return "verify-input-legality";
+  }
+
+  StringRef getDescription() const override {
+    return "Checks the legality of the IR at the start of subgraph split transformation pipeline";
+  }
+
+  static void emitLegalizationErrors(Location loc,
+                                   const DenseSet<Operation *> &illegalOps) {
+    // Print op errors for each of the illegal ops that still remain.
+    llvm::MapVector<StringRef, int> opNameCounts;
+    for (Operation *illegalOp : illegalOps) {
+      StringRef opName = illegalOp->getName().getStringRef();
+      opNameCounts[opName]++;
+      illegalOp->emitOpError() << ": illegal op still exists";
+    }
+
+    std::vector<std::string> errorMessages;
+    errorMessages.reserve(opNameCounts.size());
+    for (const auto &opInfo : opNameCounts) {
+      errorMessages.push_back(
+          llvm::formatv("\t{0} (count: {1})", opInfo.first, opInfo.second));
+    }
+    emitError(loc) << "The following illegal operations still remain: \n"
+                 << llvm::join(errorMessages, "\n") << "\n";
+  }
+
+  LogicalResult verifyAllOperationsAreLegal(Operation *op,
+                                          const ConversionTarget &target) {
+    // We don't just use applyPartialConversion with no patterns because this pass
+    // shouldn't alter the IR at all (including via folding or canonicalizations
+    // that dialect conversion does automatically).
+    DenseSet<Operation *> illegalOps;
+    op->walk([&](Operation *op) {
+      if (!target.isLegal(op)) {
+        illegalOps.insert(op);
+      }
+    });
+    if (illegalOps.empty())
+      return success();
+    emitLegalizationErrors(op->getLoc(), illegalOps);
+    return failure();
+  }
+
+  void runOnOperation() override {
+    ConversionTarget target(getContext());
+    target.markUnknownOpDynamicallyLegal([](Operation *) { return true; });
+    target.addLegalOp<tosa::ApplyScaleOp>();
+    // We're already depending on the Tosa Dialect
+    target.addIllegalDialect<tosa::TosaDialect>();
+    // Avoid StableHLO dependency
+    target.addIllegalDialect("chlo");
+    target.addIllegalDialect("stablehlo");
+    target.addIllegalOp<UnrealizedConversionCastOp>();
+
+    if (failed(verifyAllOperationsAreLegal(getOperation(),
+                                                          target))) {
+      return signalPassFailure();
+    }
+  }
+};
+
+std::unique_ptr<OperationPass<ModuleOp>>
+createVerifyInputLegalityPass() {
+  return std::make_unique<VerifyInputLegalityPass>();
+}
+
+static PassRegistration<VerifyInputLegalityPass> pass([] {
+  return std::make_unique<VerifyInputLegalityPass>();
+});
+} // namespace  mlir
+
