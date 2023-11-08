0454ac7e76fb606eaf2cafb63a21e04b574e7c1a
jone.zhang <jone.zhang@sophgo.com>
2023-11-08 14:48:29 +0800

    [SG2260Dialect] support And/Or/Xor/Add op
    Change-Id: Ie2eb781fd5998f299c10282fc779446bdc409a9b

--- experimental/lib/Dialect/SG2260/IR/SG2260Ops.cpp
+++ experimental/lib/Dialect/SG2260/IR/SG2260Ops.cpp
@@ -65,24 +65,52 @@ void DMATensorBroadcastOp::getAsmResultNames(
   ::getAsmResultNames(setNameFn, getResults());
 }
 
+void AndOp::getAsmResultNames(
+    function_ref<void(Value, StringRef)> setNameFn) {
+  ::getAsmResultNames(setNameFn, getResults());
+}
+
+void XorOp::getAsmResultNames(
+    function_ref<void(Value, StringRef)> setNameFn) {
+  ::getAsmResultNames(setNameFn, getResults());
+}
+
+void OrOp::getAsmResultNames(
+    function_ref<void(Value, StringRef)> setNameFn) {
+  ::getAsmResultNames(setNameFn, getResults());
+}
+
+void AddOp::getAsmResultNames(
+    function_ref<void(Value, StringRef)> setNameFn) {
+  ::getAsmResultNames(setNameFn, getResults());
+}
+
 template <typename T>
 Attribute dummyPropertiesAsAttribute(::mlir::MLIRContext *ctx, T &property) {
   Builder b{ctx};
   return b.getUnitAttr();
 }
 
+typedef enum {
+  HW_INT8 = 0,
+  HW_FP16 = 1,
+  HW_FP32 = 2,
+  HW_INT16 = 3,
+  HW_INT32 = 4,
+  HW_BFP16 = 5,
+  HW_INT4 = 6,
+  HW_FP8 = 7,
+  DTYPE_UNKNOWN = -1,
+} HWType;
+
+typedef enum {
+  N_BYTE_ALIGN = 0,
+  CONTINUOUS = 1,
+  COMPACT = 2,
+  FREE = 3,
+} Layout;
 struct getValueInfo {
-  typedef enum {
-    HW_INT8 = 0,
-    HW_FP16 = 1,
-    HW_FP32 = 2,
-    HW_INT16 = 3,
-    HW_INT32 = 4,
-    HW_BFP16 = 5,
-    HW_INT4 = 6,
-    HW_FP8 = 7,
-    DTYPE_UNKNOWN = -1,
-  } HWType;
+
 
   getValueInfo(Value value) : value(value){};
 
@@ -111,6 +139,11 @@ struct getValueInfo {
     return cast<ShapedType>(value.getType()).getElementType();
   }
 
+  // TODO:
+  Layout getLayout() {
+    return {};
+  }
+
   bool getSign() { return !getDtype().isUnsignedInteger(); }
 
   HWType getPrec() {
@@ -137,6 +170,71 @@ struct getValueInfo {
 private:
   Value value;
 };
+
+typedef enum {
+  CONV = 0,
+  PD   = 1,
+  MM   = 2,
+  AR   = 3,
+  RQDQ = 4,
+  TRANS_BC = 5,
+  SG   = 6,
+  LAR  = 7,
+  SFU  = 9,
+  LIN  = 10,
+  SYS_TRWR = 12,
+  CMP  = 13,
+  VC   = 14,
+  SYS  = 15,
+} TSK_TYPE;
+
+typedef enum {
+  MUL = 0,
+  NOT = 1,
+  ADD = 2,
+  SUB = 3,
+  MAX = 4,
+  MIN = 5,
+  LOGIC_SHIFT = 6,
+  AND = 7,
+  OR  = 8,
+  XOR = 9,
+  SELECT_GREAT = 10,
+  SELECT_EQUAL = 11,
+  DIV = 12,
+  SELECT_LESS  = 13,
+  DATA_CONVERT = 14,
+  ADD_SATU = 15,
+  SUB_SATU = 16,
+  CLAMP = 17,
+  MAC = 18,
+  COPY = 19,
+  MUL_SATU = 20,
+  ARITH_SHIFT = 21,
+  ROTATE_SHIFT = 22,
+  MULHDR = 23,
+  ABS = 26,
+  FSUBABS = 27,
+  COPY_MB = 28,
+  GET_FRIST_ONE = 29,
+  GET_FRIST_ZERO = 30,
+} AR_TYPE;
+
+typedef enum {
+  PAD_CONSTANT    = 0,
+  PAD_REFLECTION  = 1,
+  PAD_REPLICATION = 2,
+  PAD_CIRCULAR    = 3
+} PAD_MODE;
+
+typedef enum {
+  MM_NORMAL = 1,
+  MM_WRQ = 2,
+  MM_WRQ_RELU = 3,
+  MM_NN = 4,
+  MM_NT = 5,
+  MM_TT = 6,
+} MM_OP;
 
 LogicalResult MatMulOp::verify() {
   auto &reg = getProperties().reg;
@@ -340,6 +438,160 @@ LogicalResult DMATensorBroadcastOp::verify() {
 
   auto reg = getProperties().reg;
   reg.cmd_special_function = dmaType::TENSOR::BROADCAST;
+  return success();
+}
+
+LogicalResult binary_op_verify(
+    ShortARRegDef &reg, getValueInfo& leftInfo, getValueInfo& rightInfo,
+    getValueInfo& outInfo) {
+  reg.cmd_short = true;
+  reg.tsk_typ = TSK_TYPE::AR;
+  reg.tsk_opd_num = 2;
+
+  // left
+  reg.opt_opd0_prec = leftInfo.getPrec();
+  reg.opt_opd0_sign = leftInfo.getSign();
+  reg.opd0_addr = leftInfo.getAddr();
+  if (leftInfo.isConst()) {
+    reg.opt_opd0_const = true;
+  } else {
+    reg.opt_opd0_const = false;
+    if (leftInfo.getLayout() == Layout::FREE) {
+      reg.opd0_str = false;
+      auto stride = leftInfo.getStride();
+      reg.opd0_n_str = stride[0];
+      reg.opd0_c_str = stride[1];
+      reg.opd0_h_str = stride[2];
+      reg.opd0_w_str = stride[3];
+    } else {
+      reg.opd0_str = true;
+    }
+  }
+
+  // right
+  reg.opt_opd1_prec = rightInfo.getPrec();
+  reg.opt_opd1_sign = rightInfo.getSign();
+  reg.opd1_addr = rightInfo.getAddr();
+  if (rightInfo.isConst()) {
+    reg.opt_opd1_const = true;
+  } else {
+    reg.opt_opd1_const = false;
+    if (rightInfo.getLayout() == Layout::FREE) {
+      reg.opd1_str = false;
+      auto stride = rightInfo.getStride();
+      reg.opd1_n_str = stride[0];
+      reg.opd1_c_str = stride[1];
+      reg.opd1_h_str = stride[2];
+      reg.opd1_w_str = stride[3];
+    } else {
+      reg.opd1_str = true;
+    }
+  }
+
+  // out
+  reg.opt_res0_prec = outInfo.getPrec();
+  reg.res0_addr = outInfo.getAddr();
+  auto shape = outInfo.getShape();
+  reg.res0_n = shape[0];
+  reg.res0_c = shape[1];
+  reg.res0_h = shape[2];
+  reg.res0_w = shape[3];
+  if (outInfo.getLayout() == Layout::FREE) {
+    reg.res0_str = false;
+    auto stride = outInfo.getStride();
+    reg.res0_n_str = stride[0];
+    reg.res0_c_str = stride[1];
+    reg.res0_h_str = stride[2];
+    reg.res0_w_str = stride[3];
+  } else {
+    reg.res0_str = true;
+  }
+  return success();
+}
+
+LogicalResult logical_binary_op_verify(
+    ShortARRegDef &reg, getValueInfo& leftInfo, getValueInfo& rightInfo,
+    getValueInfo& outInfo) {
+
+  assert(leftInfo.getPrec() == rightInfo.getPrec());
+  assert(leftInfo.getPrec() == outInfo.getPrec());
+  assert(leftInfo.getSign() == rightInfo.getSign());
+  assert(leftInfo.getSign() == outInfo.getSign());
+
+  binary_op_verify(reg, leftInfo, rightInfo, outInfo);
+  if (reg.opt_opd0_prec == HWType::HW_FP32) {
+    reg.opt_opd0_prec = HWType::HW_INT32;
+    reg.opt_opd1_prec = HWType::HW_INT32;
+    reg.opt_res0_prec = HWType::HW_INT32;
+  } else if (reg.opt_opd0_prec == HWType::HW_FP16 || reg.opt_opd0_prec == HWType::HW_BFP16) {
+    reg.opt_opd0_prec = HWType::HW_INT16;
+    reg.opt_opd1_prec = HWType::HW_INT16;
+    reg.opt_res0_prec = HWType::HW_INT16;
+  } else if (reg.opt_opd0_prec == HWType::HW_FP8) {
+    reg.opt_opd0_prec = HWType::HW_INT8;
+    reg.opt_opd1_prec = HWType::HW_INT8;
+    reg.opt_res0_prec = HWType::HW_INT8;
+  }
+  return success();
+}
+
+LogicalResult AndOp::verify() {
+  auto &reg = getProperties().reg;
+  reg.tsk_eu_typ = AR_TYPE::AND;
+  reg.cmd_id_dep = getDependency().getType().getId();
+
+  auto leftInfo = getValueInfo(getLhs());
+  auto rightInfo = getValueInfo(getRhs());
+  auto outInfo = getValueInfo(getResult());
+
+  return logical_binary_op_verify(reg, leftInfo, rightInfo, outInfo);
+}
+
+LogicalResult OrOp::verify() {
+  auto &reg = getProperties().reg;
+  reg.tsk_eu_typ = AR_TYPE::OR;
+  reg.cmd_id_dep = getDependency().getType().getId();
+
+  auto leftInfo = getValueInfo(getLhs());
+  auto rightInfo = getValueInfo(getRhs());
+  auto outInfo = getValueInfo(getResult());
+
+  return logical_binary_op_verify(reg, leftInfo, rightInfo, outInfo);
+}
+
+LogicalResult XorOp::verify() {
+  auto &reg = getProperties().reg;
+  reg.tsk_eu_typ = AR_TYPE::XOR;
+  reg.cmd_id_dep = getDependency().getType().getId();
+
+  auto leftInfo = getValueInfo(getLhs());
+  auto rightInfo = getValueInfo(getRhs());
+  auto outInfo = getValueInfo(getResult());
+
+  return logical_binary_op_verify(reg, leftInfo, rightInfo, outInfo);
+}
+
+LogicalResult AddOp::verify() {
+  auto &reg = getProperties().reg;
+  reg.tsk_eu_typ = AR_TYPE::ADD;
+  reg.cmd_id_dep = getDependency().getType().getId();
+
+  auto leftInfo = getValueInfo(getLhs());
+  auto rightInfo = getValueInfo(getRhs());
+  auto outInfo = getValueInfo(getResult());
+  binary_op_verify(reg, leftInfo, rightInfo, outInfo);
+  // shift
+  if (getShift()) {
+    auto shiftInfo = getValueInfo(getShift());
+    reg.opt_opd2_prec = shiftInfo.getPrec();
+    reg.opt_opd2_sign = shiftInfo.getSign();
+    reg.opd2_addr = shiftInfo.getAddr();
+    reg.opt_opd2_const = shiftInfo.isConst();
+    reg.opd2_n_str = (int)getRoundMode();
+  }
+
+  // TODO: type check
+
   return success();
 }
 
