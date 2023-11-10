526732496f9767b03698c4ba0ac31e50b5d3b353
jing.song <jing.song@sophgo.com>
2023-11-10 11:19:13 +0800

    feat: add TypeInterface for IndexPut to process cast
    Change-Id: I9d611c3f2b603eabf4a73101525de24509c8eb38

--- lib/Dialect/Tpu/Interfaces/Common/IndexPut.cpp
+++ lib/Dialect/Tpu/Interfaces/Common/IndexPut.cpp
@@ -37,3 +37,26 @@ LogicalResult tpu::IndexPutOp::inference(InferenceParameter &p) {
   return success();
 }
 
+mlir::Type tpu::IndexPutOp::type_verify(uint64_t opd_idx, TypeCastMode &mode) {
+  auto op = getOperation();
+  if (opd_idx == 1) {
+    // indices
+    auto opd = op->getOperand(1);
+    auto in_op = opd.getDefiningOp();
+    if (in_op != nullptr && isa<top::WeightOp, top::NoneOp>(in_op)) {
+      return do_nothing(mode);
+    }
+    auto stype = module::getStorageType(opd);
+    if (stype.isIntOrIndex()) {
+      return do_nothing(mode);
+    }
+    mode = TypeCastMode::DO_CAST;
+    auto bitwidth = stype.getIntOrFloatBitWidth();
+    if (module::isBM1684XFamily() || module::isSG2260Family()) {
+      // indices should be int32 in BM1684x
+      bitwidth = 32;
+    }
+    return Builder(op).getIntegerType(bitwidth);
+  }
+  return type_verify_case_same(op, opd_idx, mode);
+}
