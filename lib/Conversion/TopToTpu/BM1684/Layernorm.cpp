a706979385caf6c8cda3a18a6538b847f42ea79b
Zhou Wenjie <wenjie.zhou@sophgo.com>
2023-11-10 21:13:30 +0800

    [bm1684] support layernorm when axis=3
    Change-Id: I36996edf5d3fcae081d514322d0f5bdb1235d6bb

--- lib/Conversion/TopToTpu/BM1684/Layernorm.cpp
+++ lib/Conversion/TopToTpu/BM1684/Layernorm.cpp
@@ -28,6 +28,7 @@ void LayerNormLowering::LoweringF32(PatternRewriter &rewriter,
   opds.push_back(module::getNoneOp(op));
   opds.push_back(module::getNoneOp(op));
   auto name = module::getName(op.getOutput());
+  auto input_shape = module::getShape(op.getInput());
   bool has_weight = !module::isNone(weight);
   bool has_bias = !module::isNone(bias);
   auto axis = op.getAxis();
@@ -35,26 +36,64 @@ void LayerNormLowering::LoweringF32(PatternRewriter &rewriter,
   if (axis == 1) {
     group_num = 1;
   } else if (axis == 2) {
-    group_num = module::getShape(op.getInput())[1];
+    group_num = input_shape[1];
+  } else if (axis == 3) {
+    group_num = input_shape[1] * input_shape[2];
   } else {
     llvm_unreachable("Not Implemented");
   }
 
   std::vector<NamedAttribute> attrs;
-  // replace LayerNorm use GroupNorm
-  attrs.push_back(
-      rewriter.getNamedAttr("num_groups", rewriter.getI64IntegerAttr(group_num)));
-  attrs.push_back(rewriter.getNamedAttr("eps", op.getEpsAttr()));
   auto new_type = op.getOutput().getType();
-  auto group_norm_loc = (has_weight || has_bias)
-                            ? NameLoc::get(rewriter.getStringAttr(
-                                  name.str() + "_convert_to_group_norm"))
-                            : op.getLoc();
-  auto group_norm_op =
-      rewriter.create<tpu::GroupNormOp>(group_norm_loc, new_type, opds, attrs);
+  Value last_op_value = op.getOutput();
+  // replace LayerNorm use GroupNorm
+  if (axis != 3) {
+    attrs.push_back(
+        rewriter.getNamedAttr("num_groups", rewriter.getI64IntegerAttr(group_num)));
+    attrs.push_back(rewriter.getNamedAttr("eps", op.getEpsAttr()));
+    auto group_norm_loc = (has_weight || has_bias)
+                              ? NameLoc::get(rewriter.getStringAttr(
+                                    name.str() + "_convert_to_group_norm"))
+                              : op.getLoc();
+    auto group_norm_op =
+        rewriter.create<tpu::GroupNormOp>(group_norm_loc, new_type, opds, attrs);
+
+    last_op_value = group_norm_op.getOutput();
+  } else {
+    // Handle the case when axis is 3
+    SmallVector<Value, 2> opds_1;
+    opds_1.push_back(op.getOperand(0));
+    opds_1.push_back(module::getNoneOp(op));
+    std::vector<int64_t> shape_1 = {input_shape[0], group_num, 1, input_shape[3]};
+    auto type_1 = RankedTensorType::get(shape_1, rewriter.getF32Type());
+    auto reshape_1_loc = NameLoc::get(rewriter.getStringAttr(name.str() + "_reshape_1"));
+    auto reshape_1_op =
+        rewriter.create<tpu::ReshapeOp>(reshape_1_loc, type_1, opds_1);
+
+    opds[0] = reshape_1_op.getResult();
+    auto group_norm_type = reshape_1_op.getOutput().getType();
+    attrs.push_back(
+        rewriter.getNamedAttr("num_groups", rewriter.getI64IntegerAttr(group_num)));
+    attrs.push_back(rewriter.getNamedAttr("eps", op.getEpsAttr()));
+    auto group_norm_loc = NameLoc::get(rewriter.getStringAttr(name.str() + "_convert_to_group_norm"));
+    auto group_norm_op =
+        rewriter.create<tpu::GroupNormOp>(group_norm_loc, group_norm_type, opds, attrs);
+
+    SmallVector<Value, 2> opds_2;
+    opds_2.push_back(group_norm_op.getOutput());
+    opds_2.push_back(module::getNoneOp(op));
+    auto type_2 = RankedTensorType::get(input_shape, rewriter.getF32Type());
+    auto reshape_2_loc = (has_weight || has_bias)
+                              ? NameLoc::get(rewriter.getStringAttr(
+                                    name.str() + "_reshape_2"))
+                              : op.getLoc();
+    auto reshape_2_op =
+        rewriter.create<tpu::ReshapeOp>(reshape_2_loc, type_2, opds_2);
+
+    last_op_value = reshape_2_op.getOutput();
+  }
 
   attrs.clear();
-  auto last_op_value = group_norm_op.getOutput();
   // replace weight use Mul
   if (has_weight) {
     auto mul_loc =
