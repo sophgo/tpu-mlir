e09adbda400e291d51ebd3b3a198dded996befc0
tjthereal <taoyu.jiang@sophgo.com>
2023-11-13 12:53:41 +0800

    Matmul fp8 supported
    Change-Id: I4ba9acd66abc2c259ec1f131f9cc09aeee389b16

--- lib/Dialect/Tpu/Interfaces/Common/MatMul.cpp
+++ lib/Dialect/Tpu/Interfaces/Common/MatMul.cpp
@@ -9,7 +9,7 @@
 
 #include "tpu_mlir/Support/Dnnl/Dnnl.h"
 #include "tpu_mlir/Support/Float16.h"
-
+#include "tpu_mlir/Support/Float8.h"
 #include "tpu_mlir/Dialect/Tpu/Transforms/Codegen/Dynamic/DynamicLayer.hpp"
 
 // clang-format off
@@ -168,6 +168,15 @@ LogicalResult tpu::MatMulOp::inference(InferenceParameter &p) {
   if (out_type.isa<FloatType>()) {
     if (out_type.isBF16()) {
       BF16(p.outputs[0], p.outputs[0], num_elem);
+    } else if (out_type.isFloat8E4M3FN()) {
+      if (!getOutF8Scales().has_value())
+        llvm_unreachable("should have out scale for MatMul in f8 mode");
+      f64_array_t scales = module::getF64Array(getOutF8Scales().value());
+      [[maybe_unused]] auto scale_f = scales->at(0);
+      [[maybe_unused]] auto scale_f_reciprocal = 1 / scales->at(0);
+      F8E4M3(p.outputs[0], p.outputs[0], num_elem, scale_f_reciprocal);
+    } else if (out_type.isFloat8E5M2()) {
+      F8E5M2(p.outputs[0], p.outputs[0], num_elem, 1.0);
     } else if (out_type.isF16()) {
       F16(p.outputs[0], p.outputs[0], num_elem);
     }
