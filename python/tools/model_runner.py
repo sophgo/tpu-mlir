8f1a2de8ffe4b3b206a0d804afb8d77fefbd449e
man.lu <man.lu@sophgo.com>
2023-11-22 20:07:28 +0800

    Always run the BModel on the CModel, even if the BModel is large.
    - Skipping the execution of the BModel without consultation can lead to
      confusion and problems if someone requires the reference data produced by the
      CModel.
    
    Change-Id: Iba56c22e69c3dfed91a05aec07d6c8570579ed5a

--- python/tools/model_runner.py
+++ python/tools/model_runner.py
@@ -105,12 +105,13 @@ def model_inference(inputs: dict, model_file: str, dump_all = True) -> dict:
     pack_bmodel_context = (iter([None]) if is_cv18xx else pack_bmodel_context_generator(
         model_file, net))
 
-    next(pack_bmodel_context) # save input_data
+    next(pack_bmodel_context)  # save input_data
 
-    if size > 0x10000000:
-        print("Warning: {} is too large and will cost a long time. Please run in board".format(
-            model_file))
-        return {}
+    print("Warning: {} is too large and will cost a long time. ".format(model_file))
+    print(
+        'Add the "--skip_validation" argument when using "model_deploy.py"'
+        " to bypass running the cmodel if checking the correctness of the BModel is not required."
+    )
 
     if is_cv18xx:
         net.forward()
