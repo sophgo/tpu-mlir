e65374fc84c4e582634b23e2b2fdd21a4be1b03d
lianwei.qu <lianwei.qu@sophgo.com>
2023-12-01 20:45:02 +0800

    fix Torchcoverter sliceAxis canonicalize
    Change-Id: I00378240543ec9fe1e885cfaa52953b8169f4fc4

--- lib/Dialect/Top/Canonicalize/SliceAxis.cpp
+++ lib/Dialect/Top/Canonicalize/SliceAxis.cpp
@@ -77,10 +77,19 @@ struct SliceAxisToStridedSlice : public OpRewritePattern<SliceAxisOp> {
         rewriter.getNamedAttr("steps", rewriter.getI64ArrayAttr(steps)));
     attrs.push_back(
         rewriter.getNamedAttr("ends", rewriter.getI64ArrayAttr(ends)));
-    if (!module::isNone(op.getStart()) || !module::isNone(op.getEnd()) ||
-        !module::isNone(op.getStep())) {
+    if ((!module::isNone(op.getStart()) && !module::isWeight(op.getStart())) || 
+        (!module::isNone(op.getEnd()) && !module::isWeight(op.getEnd())) ||
+        (!module::isNone(op.getStep()) && !module::isWeight(op.getStep()))) {
       attrs.push_back(
           rewriter.getNamedAttr("axes", rewriter.getI64ArrayAttr(axis)));
+    }
+    else{
+      std::vector<int64_t> axes(dims, 0);
+      for (int i = 0; i < dims; i++) {
+        axes[i] = i;
+      }
+      attrs.push_back(
+          rewriter.getNamedAttr("axes", rewriter.getI64ArrayAttr(axes)));
     }
     rewriter.replaceOpWithNewOp<SliceOp>(op, op.getResult().getType(), operands,
                                          attrs);
