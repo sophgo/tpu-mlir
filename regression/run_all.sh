c9fb65c92634a194fb5a1f0e33b76634d2c226dd
zhibin.xin <zhibin.xin@sophgo.com>
2024-01-29 21:30:22 -0500

    fix daily test customlayer error
    Change-Id: If1a84d226b2a4f5c9c2bca02a0020cf884a3627d

--- regression/run_all.sh
+++ regression/run_all.sh
@@ -2,4 +2,11 @@
 set -e
 pip list
 # pip3 install onnxsim==0.4.17
+
+# compile for customlayer
+source $PROJECT_ROOT/third_party/customlayer/envsetup.sh
+rebuild_custom_backend
+rebuild_custom_firmware_cmodel bm1684x
+rebuild_custom_firmware_cmodel bm1688
+
 $REGRESSION_PATH/main_entry.py --test_type all
