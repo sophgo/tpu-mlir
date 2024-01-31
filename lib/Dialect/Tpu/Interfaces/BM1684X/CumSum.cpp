a190d51327e2167c8d96e462d04f2a544a0170fd
chang.zhao <chang.zhao@sophgo.com>
2024-01-31 20:36:19 +0800

    support dyn indices of onehot && scatterelements && cumsum
    Change-Id: I5217d3ff80d922c1558bb8b774bb674a0ae99ca8

--- lib/Dialect/Tpu/Interfaces/BM1684X/CumSum.cpp
+++ lib/Dialect/Tpu/Interfaces/BM1684X/CumSum.cpp
@@ -57,8 +57,35 @@ void tpu::CumSumOp::codegen_global_bm1684x() {
 }
 
 int64_t tpu::CumSumOp::dyn_codegen_global_bm1684x(void *buffer) {
-  llvm_unreachable("Not supported now");
-  return 0;
+  if (!buffer)
+    return sizeof(cumsum_global_param_t);
+  cumsum_global_param_t p = {0};
+  auto in_shape = module::getShape(getInput());
+  p.input_addr = module::getAddress(getInput());
+  p.output_addr = module::getAddress(getOutput());
+  int64_t shape_size = in_shape.size();
+  p.dim = getAxis();
+  p.N = in_shape[0];
+  p.C = 1;
+  p.H = 1;
+  p.W = 1;
+  switch (shape_size) {
+    case 2:
+      p.C = in_shape[1];
+      break;
+    case 3:
+      p.C = in_shape[1];
+      p.H = in_shape[2];
+      break;
+    case 4:
+      p.C = in_shape[1];
+      p.H = in_shape[2];
+      p.W = in_shape[3];
+    default:
+      break;
+  }
+  p.dtype = BM168x::getDataType(getInput());
+  return BM168x::dynamic_spec_to_buffer(buffer, p);
 }
 
 int64_t tpu::CumSumOp::get_fw_type_bm1684x() { return FW_BMNET_CUMSUM; }
