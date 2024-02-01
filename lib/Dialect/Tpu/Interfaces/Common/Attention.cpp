bf9c7de51741246bdbaca8580307b8ae9f7507a1
HOU Yu <yu.hou@sophgo.com>
2024-02-01 11:14:47 +0800

    fix attention accuracy problem
    Change-Id: I4f4567cd6575537c3acf173a4b833f9b03a593a3

--- lib/Dialect/Tpu/Interfaces/Common/Attention.cpp
+++ lib/Dialect/Tpu/Interfaces/Common/Attention.cpp
@@ -9,13 +9,14 @@
 
 #include "tpu_mlir/Support/Float16.h"
 
-#include "tpu_mlir/Support/MathUtils.h"
 #include "tpu_mlir/Support/Dnnl/Attention.h"
+#include "tpu_mlir/Support/MathUtils.h"
 
 LogicalResult tpu::AttentionOp::init(InferenceParameter &p) {
   auto attention = new Attention();
   auto in_shape = module::getShape(getInput());
-  auto key_shape = module::isNone(getKeys()) ? in_shape : module::getShape(getKeys());
+  auto key_shape =
+      module::isNone(getKeys()) ? in_shape : module::getShape(getKeys());
   auto queries_shape = module::getShape(getQueriesWeight());
   auto out_type = module::getStorageType(getOutput());
   int batch = in_shape[0];
@@ -31,19 +32,19 @@ LogicalResult tpu::AttentionOp::init(InferenceParameter &p) {
   float *q_weight = p.inputs[3];
   float *k_weight = p.inputs[5];
   float *v_weight = p.inputs[7];
-  float *q_bias = has_bias&0x01 ? p.inputs[4] : nullptr;
-  float *k_bias = has_bias&0x02 ? p.inputs[6] : nullptr;
-  float *v_bias = has_bias&0x04 ? p.inputs[8] : nullptr;
-  float *o_bias = has_bias&0x08 ? p.inputs[10] : nullptr;
+  float *q_bias = has_bias & 0x01 ? p.inputs[4] : nullptr;
+  float *k_bias = has_bias & 0x02 ? p.inputs[6] : nullptr;
+  float *v_bias = has_bias & 0x04 ? p.inputs[8] : nullptr;
+  float *o_bias = has_bias & 0x08 ? p.inputs[10] : nullptr;
   int type = out_type.isF16() ? 1 : 0;
   type = out_type.isBF16() ? 2 : type;
   type = out_type.isInteger(32) ? 3 : type;
 
   attention->setup(p.inputs[0], p.inputs[1], p.inputs[2], q_weight, q_bias,
-                   k_weight, k_bias, v_weight, v_bias, p.inputs[9],
-                   o_bias, p.inputs[11], p.inputs[12], p.outputs[0], quant_param->data(),
-                   batch, M_q, M_k, N_q, N_k,
-                   d, scale, 0, type);
+                   k_weight, k_bias, v_weight, v_bias, p.inputs[9], o_bias,
+                   p.inputs[11], p.inputs[12], p.outputs[0],
+                   quant_param->data(), batch, M_q, M_k, N_q, N_k, d, scale, 0,
+                   type);
   p.handle = (void *)attention;
   return success();
 }
@@ -82,7 +83,7 @@ LogicalResult tpu::AttentionOp::LocalGenSupport() {
 }
 
 LogicalResult tpu::AttentionOp::AllowDataSplit(int64_t axis,
-                                            group_type_t group_type) {
+                                               group_type_t group_type) {
   if (axis == 0) {
     return success();
   } else if (axis == 1) {
@@ -96,9 +97,16 @@ LogicalResult tpu::AttentionOp::AllowDataSplit(int64_t axis,
 mlir::Type tpu::AttentionOp::type_verify(uint64_t opd_idx, TypeCastMode &mode) {
   if (opd_idx == 0 || opd_idx == 1) {
     return type_verify_case_i32(getOperation(), opd_idx, mode);
-  }  else if (opd_idx == 11) { // mask should be f32
-    auto ftype = RankedTensorType::get(module::getShape(getMusk()), Builder(getOperation()).getF32Type());
-    return type_verify_case_type(getOperation(), opd_idx, ftype, mode);
+  } else if (opd_idx == 11) { // mask should be f32
+    auto out = getResult();
+    auto stype = module::getStorageType(out);
+    if (stype.isInteger(32)) {
+      auto ftype = RankedTensorType::get(module::getShape(getMusk()),
+                                         Builder(getOperation()).getI32Type());
+      return type_verify_case_type(getOperation(), opd_idx, ftype, mode);
+    } else {
+      type_verify_case_same(getOperation(), opd_idx, mode);
+    }
   }
   return type_verify_case_same(getOperation(), opd_idx, mode);
 }
