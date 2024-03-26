45a479443f7e0bb2513c72a216ebbe10c98987a6
haozhe.yang <haozhe.yang@sophgo.com>
2024-03-26 11:58:59 +0800

    fix cv186x num_core params
    Change-Id: I7b6ead338c83d914712d51079bd3038529c7e0d1

--- lib/Dialect/Top/Transforms/ProcessorAssign.cpp
+++ lib/Dialect/Top/Transforms/ProcessorAssign.cpp
@@ -29,7 +29,7 @@ public:
       // only one device
       num_device = 1;
     }
-    if (!(module::isBM1688() || module::isBM1690Family())) {
+    if (!(module::isBM1688() || module::isBM1690Family()) || chip_ == "cv186x") {
       // only one core
       num_core = 1;
     }
