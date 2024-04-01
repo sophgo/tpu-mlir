10784ca1c349b60a794fb52afa7fb1491a4208c7
heng.yang <heng.yang@sophgo.com>
2024-04-01 15:57:25 +0800

    [A2]no conv_multi_core
    Change-Id: I5260dac2e7b024eff762b80f8452136e8f320483

--- lib/Dialect/Tpu/Transforms/Codegen/BM168xCodegen.cpp
+++ lib/Dialect/Tpu/Transforms/Codegen/BM168xCodegen.cpp
@@ -935,20 +935,24 @@ void BMCodegen::codegen(FuncOp funcOp) {
       bool is_depthwise =
           attr.ic == attr.oc && attr.ic == attr.groups && attr.groups > 1;
       auto in_etype = module::getStorageType(Conv2dOp.getInput());
-      if (is_depthwise == false && in_etype.isIntOrIndex() == false &&
-          core_num != 1) {
-        setupMultiCoreCodegen();
-        codegenMultiCoreOp(op, bm168x, codegenGlobalLayer);
-        return WalkResult::skip();
+      if (!(module::isBM1688() && !(BM168x::getDataType(Conv2dOp.getInput()) == DTYPE_FP32 && module::getShape(Conv2dOp.getInput())[0] == 1))) {
+        if (is_depthwise == false && in_etype.isIntOrIndex() == false &&
+            core_num != 1) {
+          setupMultiCoreCodegen();
+          codegenMultiCoreOp(op, bm168x, codegenGlobalLayer);
+          return WalkResult::skip();
+        }
       }
     } else if (auto PermuteOp = dyn_cast<tpu::PermuteOp>(op)) {
       auto order = *(module::getI64Array(PermuteOp.getOrder()));
       auto in_shape = module::getShape(PermuteOp.getInput());
-      if (core_num != 1 && in_shape.size() == 4 && order[0] == 0 && order[1] == 3 &&
-          order[2] == 1 && order[3] == 2 && in_shape[3] == 3) {
-        setupMultiCoreCodegen();
-        codegenMultiCoreOp(op, bm168x, codegenGlobalLayer);
-        return WalkResult::skip();
+      if (!(module::isBM1688() && !(BM168x::getDataType(PermuteOp.getInput()) == DTYPE_FP32 && module::getShape(PermuteOp.getInput())[0] == 1))) {
+        if (core_num != 1 && in_shape.size() == 4 && order[0] == 0 && order[1] == 3 &&
+            order[2] == 1 && order[3] == 2 && in_shape[3] == 3) {
+          setupMultiCoreCodegen();
+          codegenMultiCoreOp(op, bm168x, codegenGlobalLayer);
+          return WalkResult::skip();
+        }
       }
     }
 
