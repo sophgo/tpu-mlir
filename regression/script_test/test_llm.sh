1442567a1d172819e2d9782140fec061dbc1f9aa
yi.chu <yi.chu@sophgo.com>
2024-05-02 17:36:41 +0800

    [Regression] add chatglm3 in script_test
    Change-Id: I56ef002eac0f1089d31678d7fb1a13978ced70bd

--- regression/script_test/test_llm.sh
+++ regression/script_test/test_llm.sh
@@ -166,3 +166,77 @@ model_deploy.py \
   --chip bm1684x \
   --num_device 2 \
   --model qwen_block_cache_0_dev2.bmodel
+
+# ===------------------------------------------------------------===
+# ChatGLM
+# ===------------------------------------------------------------===
+# block
+# chatglm3_0.5b
+model_transform.py \
+  --model_name chatglm3_block_0 \
+  --model_def ${NNMODELS_PATH}/llm_models/chatglm3_block_0.onnx \
+  --mlir chatglm3_block_0.mlir
+
+model_runner.py \
+  --input ${NNMODELS_PATH}/llm_models/chatglm3_block_0_input.npz \
+  --model chatglm3_block_0.mlir \
+  --output chatglm3_block_0_top_outputs.npz
+
+model_deploy.py \
+  --mlir chatglm3_block_0.mlir \
+  --quantize W4F16 \
+  --q_group_size 64 \
+  --chip bm1684x \
+  --quant_input \
+  --quant_output \
+  --debug \
+  --model chatglm3_block_0.bmodel
+
+model_runner.py \
+  --input ${NNMODELS_PATH}/llm_models/chatglm3_block_0_input.npz \
+  --model chatglm3_block_0_bm1684x_w4f16_tpu.mlir \
+  --output chatglm3_block_0_tpu_outputs.npz
+
+npz_tool.py compare \
+  chatglm3_block_0_top_outputs.npz \
+  chatglm3_block_0_tpu_outputs.npz \
+  --tolerance 0.98,0.90 -v
+
+# parallel
+model_deploy.py \
+  --mlir chatglm3_block_0.mlir \
+  --quantize F16 \
+  --chip bm1684x \
+  --num_device 8 \
+  --quant_input \
+  --quant_output \
+  --model chatglm3_block_0_dev8.bmodel
+
+# block cache
+# chatglm3_0.5b
+model_transform.py \
+  --model_name chatglm3_block_cache_0 \
+  --model_def ${NNMODELS_PATH}/llm_models/chatglm3_block_cache_0.onnx \
+  --mlir chatglm3_block_cache_0.mlir
+
+model_runner.py \
+  --input ${NNMODELS_PATH}/llm_models/chatglm3_block_cache_0_input.npz \
+  --model chatglm3_block_cache_0.mlir \
+  --output chatglm3_block_cache_0_top_outputs.npz
+
+model_deploy.py \
+  --mlir chatglm3_block_cache_0.mlir \
+  --quantize W8F16 \
+  --chip bm1684x \
+  --test_input ${NNMODELS_PATH}/llm_models/chatglm3_block_cache_0_input.npz \
+  --test_reference chatglm3_block_cache_0_top_outputs.npz \
+  --tolerance 0.98,0.90 \
+  --model chatglm3_block_cache_0_static.bmodel
+
+# parallel
+model_deploy.py \
+  --mlir chatglm3_block_cache_0.mlir \
+  --quantize W8F16 \
+  --chip bm1684x \
+  --num_device 2 \
+  --model chatglm3_block_cache_0_dev2.bmodel
