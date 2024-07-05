73e8a504cfd1dbf5582aae7aac59f684598ed8ba
yangyang.zhou <yangyang.zhou@sophgo.com>
2024-07-05 19:42:00 +0800

    support onnx Mod
    Change-Id: Ie69cfb7a08957e361ac20d4cc2ffe25869600371

---
+++ lib/Conversion/TopToTpu/BM1684X/Mod.cpp
@@ -0,0 +1,74 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
+//
+// TPU-MLIR is licensed under the 2-Clause BSD License except for the
+// third-party components.
+//
+//===----------------------------------------------------------------------===//
+
+#include "tpu_mlir/Conversion/TopToTpu/LoweringBM1684X.h"
+
+namespace tpu_mlir {
+namespace bm1684x {
+
+void ModLowering::LoweringF32(PatternRewriter &rewriter, top::ModOp op) const {
+
+  auto name = module::getName(op.getOutput());
+  auto type = op.getOutput().getType();
+  rewriter.setInsertionPointAfter(op);
+
+  std::vector<NamedAttribute> attrs;
+  //div
+  auto div_loc = NameLoc::get(rewriter.getStringAttr(name.str() + "_div"));
+  auto div_op = rewriter.create<tpu::DivOp>(div_loc, type, ValueRange{op.getInputs()}, attrs);
+
+  //floor
+  auto floor_loc = NameLoc::get(rewriter.getStringAttr(name.str() + "_floor"));
+  attrs.push_back(rewriter.getNamedAttr("mode", tpu::ActiveModeAttr::get(op.getContext(), tpu::ActiveMode::FLOOR)));
+  auto floor_op = rewriter.create<tpu::ActiveOp>(floor_loc, type, ValueRange(div_op.getOutput()), attrs);
+  attrs.clear();
+
+  //mul
+  auto mul_loc = NameLoc::get(rewriter.getStringAttr(name.str() + "_mul"));
+  std::vector<Value> mul_inputs = {op->getOperand(1), floor_op.getOutput()};
+  auto mul_op = rewriter.create<tpu::MulOp>(mul_loc, type, mul_inputs, attrs);
+
+  //sub
+  auto sub_loc = op.getLoc();
+  std::vector<Value> sub_inputs = {op->getOperand(0), mul_op.getOutput()};
+  auto sub_op = rewriter.create<tpu::SubOp>(sub_loc, type, sub_inputs, attrs);
+
+  op.replaceAllUsesWith(sub_op.getOperation());
+  rewriter.eraseOp(op);
+}
+
+void ModLowering::LoweringF16(PatternRewriter& rewriter, top::ModOp op) const {
+  LoweringF32(rewriter, op);
+}
+
+void ModLowering::LoweringBF16(PatternRewriter &rewriter, top::ModOp op) const {
+  LoweringF32(rewriter, op);
+}
+
+void ModLowering::LoweringINT8(PatternRewriter &rewriter, top::ModOp op,
+                               bool asymmetric) const {
+  LoweringF32(rewriter, op);
+}
+
+void ModLowering::LoweringINT4(PatternRewriter &rewriter, top::ModOp op,
+                               bool asymmetric) const {
+  LoweringINT8(rewriter, op, asymmetric);
+}
+
+void ModLowering::LoweringQuantized(PatternRewriter &rewriter, top::ModOp op) const {
+  UNREACHABLE_OP("Not Implemented", op);
+}
+
+void ModLowering::LoweringF8(PatternRewriter &rewriter, top::ModOp op) const {
+  UNREACHABLE_OP("Not Implemented", op);
+}
+
+} // namespace bm1684x
+} // namespace tpu_mlir
+
---
+++ lib/Dialect/Top/Interfaces/Mod.cpp
@@ -0,0 +1,41 @@
+//===----------------------------------------------------------------------===//
+//
+// Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
+//
+// TPU-MLIR is licensed under the 2-Clause BSD License except for the
+// third-party components.
+//
+//===----------------------------------------------------------------------===//
+
+#include "tpu_mlir/Dialect/Top/IR/TopOps.h"
+#include "tpu_mlir/Support/Dnnl/Dnnl.h"
+#include "tpu_mlir/Support/Module.h"
+
+int64_t top::ModOp::getFLOPs() {
+  return module::getNumElements(getOutput());
+}
+
+LogicalResult top::ModOp::init(InferenceParameter &p) {
+  return success();
+}
+
+LogicalResult top::ModOp::inference(InferenceParameter &p) {
+  // if (p.handle == nullptr) {
+  //   return failure();
+  // }
+  auto num_element = module::getNumElements(getOutput());
+  auto input2_size = module::getNumElements(getInputs()[1]);
+#pragma omp parallel for schedule(static, omp_schedule(num_element))
+  for (int64_t i = 0; i < num_element; i++) {
+    double divisor = (input2_size == 1) ? p.inputs[1][0] : p.inputs[1][i];
+    double mid = p.inputs[0][i] / divisor;
+    p.outputs[0][i] = p.inputs[0][i] - static_cast<int>(mid) * divisor;
+  }
+  return success();
+}
+
+void top::ModOp::deinit(InferenceParameter &p) {}
+
+void top::ModOp::shape_inference() {
+  broadcast_shape_inference(getOperation());
+}
