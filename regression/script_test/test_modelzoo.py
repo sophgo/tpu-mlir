d483e19d404081a508652c22fa1a95e58c100dca
haozhe.yang <haozhe.yang@sophgo.com>
2024-07-08 14:45:48 +0800

    add modelzoo regression
    Change-Id: I596a1ec28c6f3edbac81f91cc4b11c4998ea2858

---
+++ regression/script_test/test_modelzoo.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# Copyright (C) 2022 Sophgo Technologies Inc.  All rights reserved.
+#
+# TPU-MLIR is licensed under the 2-Clause BSD License except for the
+# third-party components.
+#
+# ==============================================================================
+from pprint import pprint
+import requests
+import os
+import json
+import time
+import json
+import traceback
+
+try:
+    ret = requests.post(
+        "http://172.28.142.50:8502/gerrit",
+        data=json.dumps(
+            {"env": os.environ.copy()},
+        ),
+    )
+    pprint(json.loads(ret.content))
+    for i in range(10):
+        ret = requests.post(
+            "http://172.28.142.50:8502/gerrit/status",
+            data=json.dumps({"env": os.environ.copy()}),
+        )
+        pprint(ret.content)
+        time.sleep(6)
+except Exception:
+    traceback.print_exc()
+    pass
