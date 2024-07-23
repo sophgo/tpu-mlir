15f8db728021f79fc6c1aee4497b187e84bde63b
zhibin.xin <zhibin.xin@sophgo.com>
2024-07-23 07:34:09 -0400

    limit layergroup support of reshapeOp
    Change-Id: Ifb8eefa355c25c6e923f95a3b734f35173f16eb2

--- lib/Dialect/Tpu/Interfaces/Common/Reshape.cpp
+++ lib/Dialect/Tpu/Interfaces/Common/Reshape.cpp
@@ -7,7 +7,6 @@
 //
 //===----------------------------------------------------------------------===//
 
-
 #include "tpu_mlir/Support/MathUtils.h"
 
 LogicalResult tpu::ReshapeOp::init(InferenceParameter &p) { return success(); }
@@ -25,9 +24,16 @@ LogicalResult tpu::ReshapeOp::LocalGenSupport() {
   if (module::isCV18xx() || module::isBM1684Family()) {
     return failure();
   }
-
   auto ishape = module::getShape(getInput());
   auto oshape = module::getShape(getOutput());
+  auto inputOp = getInput().getDefiningOp();
+  // complete Vit-B optimization: reference to
+  // 93774065ae20c7f2d1eb5f0ee83c8c94e23b4fe0
+  if (inputOp && isa<tpu::MatMulOp>(inputOp) && ishape.size() == 3 &&
+      ishape[1] == 197) {
+    return failure();
+  }
+
   if (ishape.size() < 2 || oshape.size() < 2 || ishape[0] != oshape[0] ||
       ishape[1] != oshape[1]) {
     return failure();
