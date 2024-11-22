1b7f9428526a016f14047179d7cb220d337940c6
zhibin.xin <zhibin.xin@sophgo.com>
2024-11-22 10:00:25 +0800

    update test8.sh for new tpu_profile
    Change-Id: Id0dda9dd59b26faaf3676cffd4081416b9ec8ef8

--- regression/script_test/test8.sh
+++ regression/script_test/test8.sh
@@ -5,5 +5,5 @@ set -ex
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 
-tpu_profile.py --mode perfAI --arch A2 ${REGRESSION_PATH}/profile profile_output --debug
+tpu_profile.py --mode time --arch BM1688 ${REGRESSION_PATH}/profile profile_output --debug
 
