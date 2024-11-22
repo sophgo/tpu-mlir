8814faadb48e604b7b0abbcaa080ccf2fa76aaf6
xieminghe1 <minghe.xie@sophgo.com>
2024-11-22 14:34:27 +0800

    add  packraw preprocess operator for mars
    Change-Id: I0bb9f03fd1586f73844319184fbf13b59472af6d

--- lib/Dialect/Tpu/Interfaces/CV18xx/PackRaw.cpp
+++ lib/Dialect/Tpu/Interfaces/CV18xx/PackRaw.cpp
@@ -20,5 +20,22 @@ using namespace tpu_mlir::backend;
 // =========================================
 
 void tpu::PackRawOp::codegen_global_cv18xx(int64_t layer_id) {
-  llvm_unreachable("to be implemented");
+  gaddr_t ga_input = module::getAddress(getInput());
+  gaddr_t ga_output = module::getAddress(getOutput());
+  int64_t n, c, h, w;
+  module::getNCHW(getInput(), n, c, h, w);
+  auto channel_order = module::getI64Array(getChannelOrder());
+  gaddr_t ga_table_high = module::getAddress(getHighTable());
+  gaddr_t ga_table_low = module::getAddress(getLowTable());
+  cvk_fmt_t fmt =
+      module::isUniformQuantized(getOutput()) ? CVK_FMT_I8 : CVK_FMT_BF16;
+  int chennel_order_cvi[4];
+  int start_h = 0;
+  int start_w = 0;
+  for (int i = 0; i < 4; i++) {
+    chennel_order_cvi[i] = channel_order->at(i);
+  }
+  cvi_backend_tg_bnr_preprocess_kernel(
+      layer_id, ga_input, ga_output, ga_table_high, ga_table_low, n, c, h, w,
+      start_h, start_w, chennel_order_cvi, fmt);
 }
