87c31f0d0918fa6471e9bb8ab864f0bb510f949d
pengchao.hu <pengchao.hu@sophgo.com>
2024-12-20 15:56:31 +0800

    add test for llm merge
    to avoid weight share uncorrect between prefill and decode
    
    Change-Id: I48ccb6612c154708674dfbebfd4e077a1df8b7eb

--- regression/script_test/test_llm0.sh
+++ regression/script_test/test_llm0.sh
@@ -4,101 +4,162 @@ set -ex
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-
 # ===------------------------------------------------------------===
-# Llama2
+# Qwen
 # ===------------------------------------------------------------===
-# block cache
-# llama2_7b
+# block
+# qwen_0.5b
 model_transform.py \
-  --model_name llama2_block_cache_0 \
-  --model_def ${NNMODELS_PATH}/llm_models/llama2_block_cache_0.onnx \
-  --mlir llama2_block_cache_0.mlir
+  --model_name qwen_block_0 \
+  --model_def ${NNMODELS_PATH}/llm_models/qwen_block_0.onnx \
+  --mlir qwen_block_0.mlir
 
 model_runner.py \
-  --input ${NNMODELS_PATH}/llm_models/llama2_block_cache_0_input.npz \
-  --model llama2_block_cache_0.mlir \
-  --output llama2_block_cache_0_top_outputs.npz
+  --input ${NNMODELS_PATH}/llm_models/qwen_block_0_input.npz \
+  --model qwen_block_0.mlir \
+  --output qwen_block_0_top_outputs.npz
 
 model_deploy.py \
-  --mlir llama2_block_cache_0.mlir \
-  --quantize W4F16 \
+  --mlir qwen_block_0.mlir \
+  --quantize W4BF16 \
   --q_group_size 64 \
   --chip bm1684x \
-  --quant_input \
-  --quant_output \
-  --addr_mode io_alone \
-  --test_input ${NNMODELS_PATH}/llm_models/llama2_block_cache_0_input.npz \
-  --test_reference llama2_block_cache_0_top_outputs.npz \
-  --tolerance 0.98,0.84 \
-  --model llama2_block_cache_0_bm1684x_static.bmodel
+  --debug \
+  --model qwen_block_0.bmodel
 
-model_deploy.py \
-  --mlir llama2_block_cache_0.mlir \
-  --quantize W4F16 \
-  --q_group_size 64 \
-  --chip bm1688 \
-  --quant_input \
-  --quant_output \
-  --addr_mode io_alone \
-  --test_input ${NNMODELS_PATH}/llm_models/llama2_block_cache_0_input.npz \
-  --test_reference llama2_block_cache_0_top_outputs.npz \
-  --tolerance 0.98,0.84 \
-  --model llama2_block_cache_0_bm1688_1core_static.bmodel
+model_runner.py \
+  --input ${NNMODELS_PATH}/llm_models/qwen_block_0_input.npz \
+  --model qwen_block_0_bm1684x_w4bf16_tpu.mlir \
+  --output qwen_block_0_tpu_outputs.npz
 
-model_deploy.py \
-  --mlir llama2_block_cache_0.mlir \
-  --quantize W4F16 \
-  --q_group_size 64 \
-  --chip bm1688 \
-  --num_core 2 \
-  --quant_input \
-  --quant_output \
-  --addr_mode io_alone \
-  --test_input ${NNMODELS_PATH}/llm_models/llama2_block_cache_0_input.npz \
-  --test_reference llama2_block_cache_0_top_outputs.npz \
-  --tolerance 0.98,0.84 \
-  --model llama2_block_cache_0_bm1688_2core_static.bmodel
-
-model_deploy.py \
-  --mlir llama2_block_cache_0.mlir \
-  --quantize W8F16 \
-  --chip bm1688 \
-  --num_core 2 \
-  --quant_input \
-  --quant_output \
-  --addr_mode io_alone \
-  --test_input ${NNMODELS_PATH}/llm_models/llama2_block_cache_0_input.npz \
-  --test_reference llama2_block_cache_0_top_outputs.npz \
-  --tolerance 0.98,0.84 \
-  --model llama2_block_cache_0_bm1688_2core_static.bmodel
+npz_tool.py compare \
+  qwen_block_0_top_outputs.npz \
+  qwen_block_0_tpu_outputs.npz \
+  --tolerance 0.98,0.90 -v
 
 # dynamic
 model_deploy.py \
-  --mlir llama2_block_cache_0.mlir \
-  --quantize W4F16 \
+  --mlir qwen_block_0.mlir \
+  --quantize W4BF16 \
   --q_group_size 64 \
   --chip bm1684x \
-  --quant_input \
-  --quant_output \
-  --addr_mode io_alone \
   --dynamic \
-  --test_input ${NNMODELS_PATH}/llm_models/llama2_block_cache_0_input.npz \
-  --test_reference llama2_block_cache_0_top_outputs.npz \
-  --tolerance 0.98,0.84 \
-  --model llama2_block_cache_0_dynamic.bmodel
+  --model qwen_block_0_dynamic.bmodel
+
+model_runner.py \
+  --input ${NNMODELS_PATH}/llm_models/qwen_block_0_input.npz \
+  --model qwen_block_0.bmodel \
+  --output qwen_block_0_model_outputs.npz
+
+npz_tool.py compare \
+  qwen_block_0_model_outputs.npz \
+  qwen_block_0_tpu_outputs.npz \
+  --tolerance 0.98,0.90 -v
 
 # parallel
 model_deploy.py \
-  --mlir llama2_block_cache_0.mlir \
-  --quantize W4F16 \
+  --mlir qwen_block_0.mlir \
+  --quantize W4BF16 \
   --q_group_size 64 \
   --chip bm1684x \
-  --quant_input \
-  --quant_output \
-  --addr_mode io_alone \
-  --num_device 6 \
-  --model llama2_block_cache_0_dev6.bmodel
+  --num_device 8 \
+  --model qwen_block_0_dev8.bmodel
 
-rm -rf *.npz
-rm -rf *.onnx
+# bf16
+model_deploy.py \
+  --mlir qwen_block_0.mlir \
+  --quantize BF16 \
+  --chip bm1684x \
+  --test_input ${NNMODELS_PATH}/llm_models/qwen_block_0_input.npz \
+  --test_reference qwen_block_0_top_outputs.npz \
+  --model qwen_block_0_bf16.bmodel
+
+# block cache
+# qwen_0.5b
+model_transform.py \
+  --model_name qwen_block_cache_0 \
+  --model_def ${NNMODELS_PATH}/llm_models/qwen_block_cache_0.onnx \
+  --mlir qwen_block_cache_0.mlir
+
+model_runner.py \
+  --input ${NNMODELS_PATH}/llm_models/qwen_block_cache_0_input.npz \
+  --model qwen_block_cache_0.mlir \
+  --output qwen_block_cache_0_top_outputs.npz
+
+model_deploy.py \
+  --mlir qwen_block_cache_0.mlir \
+  --quantize W4BF16 \
+  --q_group_size 64 \
+  --chip bm1684x \
+  --test_input ${NNMODELS_PATH}/llm_models/qwen_block_cache_0_input.npz \
+  --test_reference qwen_block_cache_0_top_outputs.npz \
+  --tolerance 0.98,0.90 \
+  --model qwen_block_cache_0.bmodel
+
+model_deploy.py \
+  --mlir qwen_block_cache_0.mlir \
+  --quantize W8BF16 \
+  --chip bm1684x \
+  --test_input ${NNMODELS_PATH}/llm_models/qwen_block_cache_0_input.npz \
+  --test_reference qwen_block_cache_0_top_outputs.npz \
+  --tolerance 0.98,0.90 \
+  --model qwen_block_cache_0_w8bf16.bmodel
+
+# dynamic
+model_deploy.py \
+  --mlir qwen_block_cache_0.mlir \
+  --quantize W8BF16 \
+  --chip bm1684x \
+  --dynamic \
+  --model qwen_block_cache_0_dynamic.bmodel
+
+# parallel
+model_deploy.py \
+  --mlir qwen_block_cache_0.mlir \
+  --quantize W8BF16 \
+  --chip bm1684x \
+  --num_device 2 \
+  --model qwen_block_cache_0_dev2.bmodel
+
+# bf16
+model_deploy.py \
+  --mlir qwen_block_cache_0.mlir \
+  --quantize BF16 \
+  --chip bm1684x \
+  --test_input ${NNMODELS_PATH}/llm_models/qwen_block_cache_0_input.npz \
+  --test_reference qwen_block_cache_0_top_outputs.npz \
+  --model qwen_block_cache_0_bf16.bmodel
+
+# combine prefill and decode, the size should be same
+
+check_merge() {
+  local a="$1"
+  local b="$2"
+
+  model_tool --combine $a $b -o llm_merge.bmodel
+
+  # Get the sizes of the files in bytes
+  size_a=$(stat -c%s $a)
+  size_b=$(stat -c%s $b)
+  size_m=$(stat -c%s llm_merge.bmodel)
+
+  # Calculate the size of size_m minus 2 MB (2 * 1024 * 1024 bytes)
+  size_m_minus=$((size_m - 2 * 1024 * 1024))
+
+  # Compare the sizes
+  if [ "$size_a" -le "$size_m_minus" ]; then
+    echo "Error: qwen merge size is uncorrect"
+    return 1
+  fi
+
+  if [ "$size_b" -le "$size_m_minus" ]; then
+    echo "Error: qwen merge size is uncorrect"
+    return 1
+  fi
+  return 0
+}
+
+check_merge qwen_block_0.bmodel qwen_block_cache_0.bmodel
+check_merge qwen_block_0_bf16.bmodel qwen_block_cache_0_bf16.bmodel
+
+rm -rf *.npz *.onnx *.bmodel
--- regression/script_test/test_llm1.sh
+++ regression/script_test/test_llm1.sh
@@ -4,112 +4,101 @@ set -ex
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
+
 # ===------------------------------------------------------------===
-# Qwen
+# Llama2
 # ===------------------------------------------------------------===
-# block
-# qwen_0.5b
-model_transform.py \
-  --model_name qwen_block_0 \
-  --model_def ${NNMODELS_PATH}/llm_models/qwen_block_0.onnx \
-  --mlir qwen_block_0.mlir
-
-model_runner.py \
-  --input ${NNMODELS_PATH}/llm_models/qwen_block_0_input.npz \
-  --model qwen_block_0.mlir \
-  --output qwen_block_0_top_outputs.npz
-
-model_deploy.py \
-  --mlir qwen_block_0.mlir \
-  --quantize W4BF16 \
-  --q_group_size 64 \
-  --chip bm1684x \
-  --quant_input \
-  --quant_output \
-  --debug \
-  --model qwen_block_0.bmodel
-
-model_runner.py \
-  --input ${NNMODELS_PATH}/llm_models/qwen_block_0_input.npz \
-  --model qwen_block_0_bm1684x_w4bf16_tpu.mlir \
-  --output qwen_block_0_tpu_outputs.npz
-
-npz_tool.py compare \
-  qwen_block_0_top_outputs.npz \
-  qwen_block_0_tpu_outputs.npz \
-  --tolerance 0.98,0.90 -v
-
-# dynamic
-model_deploy.py \
-  --mlir qwen_block_0.mlir \
-  --quantize W4BF16 \
-  --q_group_size 64 \
-  --chip bm1684x \
-  --dynamic \
-  --quant_input \
-  --quant_output \
-  --model qwen_block_0_dynamic.bmodel
-
-model_runner.py \
-  --input ${NNMODELS_PATH}/llm_models/qwen_block_0_input.npz \
-  --model qwen_block_0_dynamic.bmodel \
-  --output qwen_block_0_model_outputs.npz
-
-npz_tool.py compare \
-  qwen_block_0_model_outputs.npz \
-  qwen_block_0_tpu_outputs.npz \
-  --tolerance 0.98,0.90 -v
-
-# parallel
-model_deploy.py \
-  --mlir qwen_block_0.mlir \
-  --quantize W4BF16 \
-  --q_group_size 64 \
-  --chip bm1684x \
-  --num_device 8 \
-  --quant_input \
-  --quant_output \
-  --model qwen_block_0_dev8.bmodel
-
 # block cache
-# qwen_0.5b
+# llama2_7b
 model_transform.py \
-  --model_name qwen_block_cache_0 \
-  --model_def ${NNMODELS_PATH}/llm_models/qwen_block_cache_0.onnx \
-  --mlir qwen_block_cache_0.mlir
+  --model_name llama2_block_cache_0 \
+  --model_def ${NNMODELS_PATH}/llm_models/llama2_block_cache_0.onnx \
+  --mlir llama2_block_cache_0.mlir
 
 model_runner.py \
-  --input ${NNMODELS_PATH}/llm_models/qwen_block_cache_0_input.npz \
-  --model qwen_block_cache_0.mlir \
-  --output qwen_block_cache_0_top_outputs.npz
+  --input ${NNMODELS_PATH}/llm_models/llama2_block_cache_0_input.npz \
+  --model llama2_block_cache_0.mlir \
+  --output llama2_block_cache_0_top_outputs.npz
 
 model_deploy.py \
-  --mlir qwen_block_cache_0.mlir \
-  --quantize W8BF16 \
+  --mlir llama2_block_cache_0.mlir \
+  --quantize W4F16 \
+  --q_group_size 64 \
   --chip bm1684x \
-  --test_input ${NNMODELS_PATH}/llm_models/qwen_block_cache_0_input.npz \
-  --test_reference qwen_block_cache_0_top_outputs.npz \
-  --tolerance 0.98,0.90 \
-  --model qwen_block_cache_0_static.bmodel
+  --quant_input \
+  --quant_output \
+  --addr_mode io_alone \
+  --test_input ${NNMODELS_PATH}/llm_models/llama2_block_cache_0_input.npz \
+  --test_reference llama2_block_cache_0_top_outputs.npz \
+  --tolerance 0.98,0.84 \
+  --model llama2_block_cache_0_bm1684x_static.bmodel
+
+model_deploy.py \
+  --mlir llama2_block_cache_0.mlir \
+  --quantize W4F16 \
+  --q_group_size 64 \
+  --chip bm1688 \
+  --quant_input \
+  --quant_output \
+  --addr_mode io_alone \
+  --test_input ${NNMODELS_PATH}/llm_models/llama2_block_cache_0_input.npz \
+  --test_reference llama2_block_cache_0_top_outputs.npz \
+  --tolerance 0.98,0.84 \
+  --model llama2_block_cache_0_bm1688_1core_static.bmodel
+
+model_deploy.py \
+  --mlir llama2_block_cache_0.mlir \
+  --quantize W4F16 \
+  --q_group_size 64 \
+  --chip bm1688 \
+  --num_core 2 \
+  --quant_input \
+  --quant_output \
+  --addr_mode io_alone \
+  --test_input ${NNMODELS_PATH}/llm_models/llama2_block_cache_0_input.npz \
+  --test_reference llama2_block_cache_0_top_outputs.npz \
+  --tolerance 0.98,0.84 \
+  --model llama2_block_cache_0_bm1688_2core_static.bmodel
+
+model_deploy.py \
+  --mlir llama2_block_cache_0.mlir \
+  --quantize W8F16 \
+  --chip bm1688 \
+  --num_core 2 \
+  --quant_input \
+  --quant_output \
+  --addr_mode io_alone \
+  --test_input ${NNMODELS_PATH}/llm_models/llama2_block_cache_0_input.npz \
+  --test_reference llama2_block_cache_0_top_outputs.npz \
+  --tolerance 0.98,0.84 \
+  --model llama2_block_cache_0_bm1688_2core_static.bmodel
 
 # dynamic
 model_deploy.py \
-  --mlir qwen_block_cache_0.mlir \
-  --quantize W8BF16 \
+  --mlir llama2_block_cache_0.mlir \
+  --quantize W4F16 \
+  --q_group_size 64 \
   --chip bm1684x \
+  --quant_input \
+  --quant_output \
+  --addr_mode io_alone \
   --dynamic \
-  --test_input ${NNMODELS_PATH}/llm_models/qwen_block_cache_0_input.npz \
-  --test_reference qwen_block_cache_0_top_outputs.npz \
-  --tolerance 0.98,0.90 \
-  --model qwen_block_cache_0_dynamic.bmodel
+  --test_input ${NNMODELS_PATH}/llm_models/llama2_block_cache_0_input.npz \
+  --test_reference llama2_block_cache_0_top_outputs.npz \
+  --tolerance 0.98,0.84 \
+  --model llama2_block_cache_0_dynamic.bmodel
 
 # parallel
 model_deploy.py \
-  --mlir qwen_block_cache_0.mlir \
-  --quantize W8BF16 \
+  --mlir llama2_block_cache_0.mlir \
+  --quantize W4F16 \
+  --q_group_size 64 \
   --chip bm1684x \
-  --num_device 2 \
-  --model qwen_block_cache_0_dev2.bmodel
+  --quant_input \
+  --quant_output \
+  --addr_mode io_alone \
+  --num_device 6 \
+  --model llama2_block_cache_0_dev6.bmodel
 
 # ===------------------------------------------------------------===
 # ChatGLM
@@ -187,5 +176,5 @@ model_deploy.py \
   --quant_output \
   --model chatglm3_block_cache_0_dev2.bmodel
 
-rm -rf *.npz
-rm -rf *.onnx
+
+rm -rf *.npz *.bmodel *.onnx
