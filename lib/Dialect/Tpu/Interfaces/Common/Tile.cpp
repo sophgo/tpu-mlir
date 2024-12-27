c33108b0abf079fed3b42b232b49346dc88ea215
zhibin.xin <zhibin.xin@sophgo.com>
2024-12-27 08:31:32 -0500

    remove Tile coreparallel support
    Change-Id: Ic6cfed4003d3777bdb054f1bd4c1726161f87191

--- lib/Dialect/Tpu/Interfaces/Common/Tile.cpp
+++ lib/Dialect/Tpu/Interfaces/Common/Tile.cpp
@@ -86,28 +86,4 @@ void tpu::TileOp::assign_fw_param(void *param) {
   memcpy(param, &tile_param, sizeof(fw_tile_layer_param_t));
 }
 
-ArrayAttr tpu::TileOp::getIndexingMaps() {
-  auto shape = module::getShape(getInput());
-  MLIRContext *ctx = getContext();
-  auto num_dims = shape.size();
-  if(num_dims != 4)
-  {
-    return Builder(ctx).getAffineMapArrayAttr({});
-  }
-  auto tile = getTile().cast<ArrayAttr>();
-  std::vector<int> tile_vec;
-  for (int i = 0; i < num_dims; ++i) {
-    tile_vec.push_back(tile[i].cast<IntegerAttr>().getInt());
-  }
-  auto input_map = AffineMap::getMultiDimIdentityMap(num_dims, ctx);
-  auto empty_map = AffineMap::get(num_dims, 0, ctx);
-  auto n0 = mlir::getAffineConstantExpr(tile_vec[0], ctx);
-  auto n1 = mlir::getAffineConstantExpr(tile_vec[1], ctx);
-  auto n2 = mlir::getAffineConstantExpr(tile_vec[2], ctx);
-  auto n3 = mlir::getAffineConstantExpr(tile_vec[3], ctx);
-  auto output_map = AffineMap::get(num_dims, 0, {n0*input_map.getResult(0), n1*input_map.getResult(1), n2*input_map.getResult(2), n3*input_map.getResult(3)}, ctx);
-  SmallVector<AffineMap> indexingMaps{input_map, empty_map, output_map};
-  return Builder(ctx).getAffineMapArrayAttr(indexingMaps);
-}
-
 bool tpu::TileOp::support_multi_core() { return false; }
