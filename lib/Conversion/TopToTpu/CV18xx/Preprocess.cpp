0f243da31c6fc843c0610beb6fae56f632b59d9e
ziyang.wang <ziyang.wang@sophgo.com>
2025-01-10 16:36:46 +0800

    support cv18xx raw bayer format
    - packraw preprocess op only support bggr bayer format in cv18xx
    - support other bayer format (gbrg, rggb, grbg) by change channel order
    - fix preprocess op into wrong input cast pattern
    
    Change-Id: I348e086c370d3726101d491367f0ba9e03e7a9f6

--- lib/Conversion/TopToTpu/CV18xx/Preprocess.cpp
+++ lib/Conversion/TopToTpu/CV18xx/Preprocess.cpp
@@ -26,71 +26,98 @@ static double gen_high_table(double val) {
 void PreprocessLowering::LoweringINT8(PatternRewriter &rewriter,
                                       top::PreprocessOp op,
                                       bool asymmetric) const {
+  Value low_table = create_lookup_table(op.getInput(), op.getOutput(),
+                                        asymmetric, activate_f(gen_low_table));
+  Value high_table = create_lookup_table(
+      op.getInput(), op.getOutput(), asymmetric, activate_f(gen_high_table));
+  auto newType = getQuantBF16Type(op.getOutput());
+  double threshold = 0;
+  float white_level = 4095.;
+  float black_level = 112.;
+  std::vector<int64_t> channel_order;
   std::string pixel_format = op.getCustomizationFormat().str();
   if (pixel_format == "BGGR_RAW") {
-    Value low_table = create_lookup_table(
-        op.getInput(), op.getOutput(), asymmetric, activate_f(gen_low_table));
-    Value high_table = create_lookup_table(
-        op.getInput(), op.getOutput(), asymmetric, activate_f(gen_high_table));
-    auto newType = getQuantBF16Type(op.getOutput());
-    std::cout << pixel_format << std::endl;
-    double threshold = 0;
-    float white_level = 4095.;
-    float black_level = 112.;
-    std::vector<int64_t> channel_order;
     channel_order.push_back(3);
     channel_order.push_back(2);
     channel_order.push_back(0);
     channel_order.push_back(1);
-    std::vector<NamedAttribute> attrs;
-    attrs.emplace_back(rewriter.getNamedAttr(
-        "white_level", rewriter.getF64FloatAttr(white_level)));
-    attrs.emplace_back(rewriter.getNamedAttr(
-        "black_level", rewriter.getF64FloatAttr(black_level)));
-    attrs.emplace_back(rewriter.getNamedAttr(
-        "threshold", rewriter.getF64FloatAttr(threshold)));
-    attrs.emplace_back(rewriter.getNamedAttr(
-        "channel_order", rewriter.getI64ArrayAttr(channel_order)));
-    rewriter.replaceOpWithNewOp<tpu::PackRawOp>(
-        op, newType, ValueRange{op.getInput(), high_table, low_table}, attrs);
-
+  } else if (pixel_format == "GRBG_RAW") {
+    channel_order.push_back(1);
+    channel_order.push_back(0);
+    channel_order.push_back(2);
+    channel_order.push_back(3);
+  } else if (pixel_format == "RGGB_RAW") {
+    channel_order.push_back(0);
+    channel_order.push_back(1);
+    channel_order.push_back(3);
+    channel_order.push_back(2);
+  } else if (pixel_format == "GBRG_RAW") {
+    channel_order.push_back(2);
+    channel_order.push_back(3);
+    channel_order.push_back(1);
+    channel_order.push_back(0);
   } else {
-    lowering_common_int8<tpu::PreprocessOp>(rewriter, op, asymmetric);
+    llvm_unreachable("raw format not support current type");
   }
+  std::vector<NamedAttribute> attrs;
+  attrs.emplace_back(rewriter.getNamedAttr(
+      "white_level", rewriter.getF64FloatAttr(white_level)));
+  attrs.emplace_back(rewriter.getNamedAttr(
+      "black_level", rewriter.getF64FloatAttr(black_level)));
+  attrs.emplace_back(
+      rewriter.getNamedAttr("threshold", rewriter.getF64FloatAttr(threshold)));
+  attrs.emplace_back(rewriter.getNamedAttr(
+      "channel_order", rewriter.getI64ArrayAttr(channel_order)));
+  rewriter.replaceOpWithNewOp<tpu::PackRawOp>(
+      op, newType, ValueRange{op.getInput(), high_table, low_table}, attrs);
 }
 
 void PreprocessLowering::LoweringBF16(PatternRewriter &rewriter,
                                       top::PreprocessOp op) const {
+  Value low_table = create_lookup_table(op.getInput(), op.getOutput(), 0,
+                                        activate_f(gen_low_table));
+  Value high_table = create_lookup_table(op.getInput(), op.getOutput(), 0,
+                                         activate_f(gen_high_table));
+  auto newType = getQuantBF16Type(op.getOutput());
+  double threshold = 0;
+  float white_level = 4095.;
+  float black_level = 112.;
+  std::vector<int64_t> channel_order;
   std::string pixel_format = op.getCustomizationFormat().str();
   if (pixel_format == "BGGR_RAW") {
-    Value low_table = create_lookup_table(op.getInput(), op.getOutput(), 0,
-                                          activate_f(gen_low_table));
-    Value high_table = create_lookup_table(op.getInput(), op.getOutput(), 0,
-                                           activate_f(gen_high_table));
-    auto newType = getQuantBF16Type(op.getOutput());
-    std::cout << pixel_format << std::endl;
-    double threshold = 0;
-    float white_level = 4095.;
-    float black_level = 112.;
-    std::vector<int64_t> channel_order;
     channel_order.push_back(3);
     channel_order.push_back(2);
     channel_order.push_back(0);
     channel_order.push_back(1);
-    std::vector<NamedAttribute> attrs;
-    attrs.emplace_back(rewriter.getNamedAttr(
-        "white_level", rewriter.getF64FloatAttr(white_level)));
-    attrs.emplace_back(rewriter.getNamedAttr(
-        "black_level", rewriter.getF64FloatAttr(black_level)));
-    attrs.emplace_back(rewriter.getNamedAttr(
-        "threshold", rewriter.getF64FloatAttr(threshold)));
-    attrs.emplace_back(rewriter.getNamedAttr(
-        "channel_order", rewriter.getI64ArrayAttr(channel_order)));
-    rewriter.replaceOpWithNewOp<tpu::PackRawOp>(
-        op, newType, ValueRange{op.getInput(), high_table, low_table}, attrs);
+  } else if (pixel_format == "GRBG_RAW") {
+    channel_order.push_back(1);
+    channel_order.push_back(0);
+    channel_order.push_back(2);
+    channel_order.push_back(3);
+  } else if (pixel_format == "RGGB_RAW") {
+    channel_order.push_back(0);
+    channel_order.push_back(1);
+    channel_order.push_back(3);
+    channel_order.push_back(2);
+  } else if (pixel_format == "GBRG_RAW") {
+    channel_order.push_back(2);
+    channel_order.push_back(3);
+    channel_order.push_back(1);
+    channel_order.push_back(0);
   } else {
-    lowering_common_bf16<tpu::PreprocessOp>(rewriter, op);
+    llvm_unreachable("raw format not support current type");
   }
+  std::vector<NamedAttribute> attrs;
+  attrs.emplace_back(rewriter.getNamedAttr(
+      "white_level", rewriter.getF64FloatAttr(white_level)));
+  attrs.emplace_back(rewriter.getNamedAttr(
+      "black_level", rewriter.getF64FloatAttr(black_level)));
+  attrs.emplace_back(
+      rewriter.getNamedAttr("threshold", rewriter.getF64FloatAttr(threshold)));
+  attrs.emplace_back(rewriter.getNamedAttr(
+      "channel_order", rewriter.getI64ArrayAttr(channel_order)));
+  rewriter.replaceOpWithNewOp<tpu::PackRawOp>(
+      op, newType, ValueRange{op.getInput(), high_table, low_table}, attrs);
 }
 
 } // namespace cv18xx
