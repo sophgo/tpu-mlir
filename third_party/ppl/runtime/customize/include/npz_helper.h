0c04c7f8af58f6784856ab3776d61dd4f09ed3d8
liang.chen <liang.chen@sophgo.com>
2025-02-25 11:07:24 +0800

    update ppl
    fix bug in calc offset of c dim
    
    Change-Id: I8e0772fd68c7adcfd336e4dc16d221c844d3fd0b

--- third_party/ppl/runtime/customize/include/npz_helper.h
+++ third_party/ppl/runtime/customize/include/npz_helper.h
@@ -83,3 +83,47 @@ void load_npz(std::string file_path, std::string key, char *data, int dtype,
 
   printf("read %s success\n", file_path.c_str());
 }
+
+void npz_add(cnpy::npz_t &npz, std::string key, char *data, dim4 *shape,
+             int dtype) {
+  size_t element_size = shape->n * shape->c * shape->h * shape->w;
+  std::vector<float> float_val;
+  for (size_t i = 0; i < element_size; ++i) {
+    float value = 0.0l;
+    if (dtype == DT_FP32)
+      value = reinterpret_cast<float *>(data)[i];
+    else if (dtype == DT_INT8)
+      value = reinterpret_cast<char *>(data)[i];
+    else if (dtype == DT_UINT8)
+      value = reinterpret_cast<unsigned char *>(data)[i];
+    else if (dtype == DT_INT32)
+      value = reinterpret_cast<int *>(data)[i];
+    else if (dtype == DT_UINT32)
+      value = reinterpret_cast<u32 *>(data)[i];
+    else if (dtype == DT_UINT16)
+      value = reinterpret_cast<u16 *>(data)[i];
+    else if (dtype == DT_INT16)
+      value = reinterpret_cast<short *>(data)[i];
+    else if (dtype == DT_FP16)
+      value = fp16_to_fp32(reinterpret_cast<fp16 *>(data)[i]).fval;
+    else if (dtype == DT_BFP16)
+      value = bf16_to_fp32(reinterpret_cast<bf16 *>(data)[i]).fval;
+    else {
+      printf("dtype not supported:%d\n", dtype);
+      assert(0);
+    }
+    float_val.emplace_back(value);
+  }
+  std::vector<size_t> shape_vec = {
+      static_cast<size_t>(shape->n), static_cast<size_t>(shape->c),
+      static_cast<size_t>(shape->h), static_cast<size_t>(shape->w)};
+  cnpy::npz_add_array<float>(npz, key, float_val.data(), shape_vec);
+}
+
+void npz_save(std::string file_path, std::string file_name, std::string ext,
+              cnpy::npz_t &npz) {
+  std::stringstream out_path;
+  out_path << file_path << "/" << file_name << ext << ".npz";
+  cnpy::npz_save_all(out_path.str(), npz);
+  printf("save %s success\n", out_path.str().c_str());
+}
