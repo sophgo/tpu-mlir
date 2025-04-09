c0856c97fe59b8e021fdfc6af44c98ad9d41870d
ruiqing.li <ruiqing.li@sophgo.com>
2025-04-09 18:49:27 +0800

    [mars3] fix pass of conv2d
    fix mars3 regression
    
    Change-Id: Ie2ceea1b6cc6824482460e29ca5fe0ec4927b979

--- lib/Dialect/Tpu/Canonicalize/Conv2D.cpp
+++ lib/Dialect/Tpu/Canonicalize/Conv2D.cpp
@@ -18,40 +18,42 @@ public:
                                     PatternRewriter &rewriter) const override {
     auto input = op.getInput();
     auto filter = op.getFilter();
-    auto originalBias = op.getOperand(2);
+    auto bias = op.getBias();
+    auto rshitf = op.getRshift();
+    auto multiplier = op.getMultiplier();
     auto output = op.getOutput();
     auto name = module::getName(op.getOutput()).str();
 
     auto inputShape = module::getShape(input);
     auto filterShape = module::getShape(filter);
-    const int64_t cDim = 1;
     if (!(module::isMARS3() || module::isSGTPUV8()))
       return failure();
     // is depthwise conv
     int64_t groups = op.getGroup();
-    int64_t ic = inputShape[cDim];
-    int64_t oc = module::getShape(output)[cDim];
+    int64_t ic = inputShape[1];
+    int64_t oc = module::getShape(output)[1];
     if (!(groups == ic && ic == oc))
       return failure();
     if (op.getDoLeakyRelu())
       return failure();
-    if (inputShape.size() != 4 || inputShape[cDim] <= 512)
+    if (!(inputShape.size() == 4 && inputShape[1] >= 1024 &&
+          (inputShape[2] <= 8 || inputShape[3] <= 8)))
       return failure();
 
     // cut input tensor close to 512
     const int64_t expectedPart = 512;
     const int64_t splitNumber =
-        (inputShape[cDim] + expectedPart - 1) / expectedPart;
+        (inputShape[1] + expectedPart - 1) / expectedPart;
     const int64_t NPU_NUM = backend::Arch::NPU_NUM;
 
     auto splitAndAlign = [&](int64_t total, int split) -> std::vector<int64_t> {
       std::vector<int64_t> parts;
       int64_t base = total / split;
-      int64_t remainder = total % split;
+      int64_t part = align_up(base, NPU_NUM);
+      // int64_t remainder = total % split;
 
       for (int i = 0; i < split - 1; ++i) {
-        int64_t part = base + (i < remainder ? 1 : 0);
-        part = align_up(part, NPU_NUM);
+        // int64_t part = base + (i < remainder ? 1 : 0);
         total -= part;
         parts.push_back(part);
       }
@@ -59,7 +61,7 @@ public:
       return parts;
     };
 
-    auto inPerPart = splitAndAlign(inputShape[cDim], splitNumber);
+    auto inPerPart = splitAndAlign(inputShape[1], splitNumber);
     auto ocPerPart = splitAndAlign(filterShape[0], splitNumber);
 
     auto none = module::getNoneOp(op);
@@ -87,8 +89,7 @@ public:
                             {inputShape[0], inPerPartSum + inPerPart[i],
                              inputShape[2], inputShape[3]}));
       attrs.set("steps", rewriter.getI64ArrayAttr({1, 1, 1, 1}));
-      attrs.set("axes", rewriter.getI64ArrayAttr({}));
-      attrs.set("hasparamConvert_axes", rewriter.getI64ArrayAttr({}));
+      attrs.set("axes", rewriter.getI64ArrayAttr({1}));
 
       auto slice = rewriter.create<tpu::SliceOp>(loc, newType, operands, attrs);
       slicedInputs.push_back(slice.getOutput());
@@ -119,8 +120,7 @@ public:
                             {ocPerPartSum + ocPerPart[i], filterShape[1],
                              filterShape[2], filterShape[3]}));
       attrs.set("steps", rewriter.getI64ArrayAttr({1, 1, 1, 1}));
-      attrs.set("axes", rewriter.getI64ArrayAttr({}));
-      attrs.set("hasparamConvert_axes", rewriter.getI64ArrayAttr({}));
+      attrs.set("axes", rewriter.getI64ArrayAttr({1}));
 
       auto slice = rewriter.create<tpu::SliceOp>(loc, newType, operands, attrs);
       slicedFilters.push_back(slice.getOutput());
@@ -130,19 +130,23 @@ public:
     }
 
     // Bias切片
-    auto biasOp = op.getBias().getDefiningOp();
     std::vector<Value> slicedBiases;
-    if (!isa<top::NoneOp>(biasOp)) {
+    if (module::isNone(bias)) {
+      for (int i = 0; i < splitNumber; ++i) {
+        slicedBiases.push_back(none);
+      }
+    } else {
       ocPerPartSum = 0;
       for (int i = 0; i < splitNumber; ++i) {
         auto loc = NameLoc::get(
             rewriter.getStringAttr(name + "_slice_bias_" + std::to_string(i)));
-        auto newType = originalBias.getType().cast<RankedTensorType>().clone({
+
+        auto newType = bias.getType().cast<RankedTensorType>().clone({
             1, ocPerPart[i], 1, 1 // 输出通道匹配 ocPerPart
         });
 
         std::vector<Value> operands;
-        operands.push_back(originalBias);
+        operands.push_back(bias);
         operands.push_back(none);
         operands.push_back(none);
         operands.push_back(none);
@@ -153,6 +157,18 @@ public:
         attrs.set("ends", rewriter.getI64ArrayAttr(
                               {1, ocPerPartSum + ocPerPart[i], 1, 1}));
         attrs.set("steps", rewriter.getI64ArrayAttr({1, 1, 1, 1}));
+        attrs.set("axes", rewriter.getI64ArrayAttr({1}));
+
+        if (module::getShape(bias).size() == 1) {
+          newType = RankedTensorType::get(
+              {ocPerPart[i]},
+              bias.getType().cast<RankedTensorType>().getElementType());
+          attrs.set("offset", rewriter.getI64ArrayAttr({ocPerPartSum}));
+          attrs.set("ends",
+                    rewriter.getI64ArrayAttr({ocPerPartSum + ocPerPart[i]}));
+          attrs.set("steps", rewriter.getI64ArrayAttr({1}));
+          attrs.set("axes", rewriter.getI64ArrayAttr({0}));
+        }
 
         auto slice =
             rewriter.create<tpu::SliceOp>(loc, newType, operands, attrs);
@@ -161,18 +177,20 @@ public:
         ocPerPartSum += ocPerPart[i];
         rewriter.setInsertionPointAfterValue(slice.getOutput());
       }
-    } else {
-      for (int i = 0; i < splitNumber; ++i) {
-        slicedBiases.push_back(none);
-      }
     }
 
-    inPerPartSum = 0;
-    auto old_rshift = op.getRshift();
-    auto old_rshift_v = module::getI64Array(old_rshift.value());
-    auto old_multiplier = op.getMultiplier();
-    auto old_multiplier_v = module::getI64Array(old_multiplier.value());
+    i64_array_t old_rshift_v;
+    if (rshitf.has_value()) {
+      old_rshift_v = module::getI64Array(rshitf.value());
+    }
+
+    i64_array_t old_multiplier_v;
+    if (multiplier.has_value()) {
+      old_multiplier_v = module::getI64Array(multiplier.value());
+    }
+
     // 创建分组卷积
+    inPerPartSum = 0;
     std::vector<Value> convResults;
     for (int i = 0; i < splitNumber; ++i) {
       auto loc = NameLoc::get(
@@ -190,14 +208,14 @@ public:
       attrs.set("group", rewriter.getI64IntegerAttr(inPerPart[i]));
 
       // rshift also need to be sliced
-      if (old_rshift.has_value()) {
+      if (rshitf.has_value()) {
         std::vector<int64_t> new_rshift_v(old_rshift_v->begin() + inPerPartSum,
                                           old_rshift_v->begin() + inPerPartSum +
                                               inPerPart[i]);
         attrs.set("rshift", rewriter.getI64ArrayAttr(new_rshift_v));
       }
 
-      if (old_multiplier.has_value()) {
+      if (multiplier.has_value()) {
         std::vector<int64_t> new_multiplier_v(
             old_multiplier_v->begin() + inPerPartSum,
             old_multiplier_v->begin() + inPerPartSum + inPerPart[i]);
@@ -227,7 +245,7 @@ public:
     return success();
   }
 
-  bool shouldPrint(tpu::Conv2DOp op) const override { return true; }
+  bool shouldPrint(tpu::Conv2DOp op) const override { return false; }
 };
 
 void tpu::Conv2DOp::getCanonicalizationPatterns(RewritePatternSet &results,
