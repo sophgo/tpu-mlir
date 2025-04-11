6ba9bfa388e6c2b09aada4ad38b81b34d82fd3ee
zhibin.xin <zhibin.xin@sophgo.com>
2025-04-11 13:08:09 +0800

    Revise "Optimize the quantization function""
    1. This reverts commit d8a657b3368c6489d399bc20bb922815a178df0b.
    2. Only applicable to docker images starting from v3.4
    
    Change-Id: I2fac2a6815e1eb973de47b4a35ab94a17a2f971a

---
+++ python/calibration/utils.py
@@ -0,0 +1,39 @@
+import re
+
+FLOAT_MAP = {
+    "bm1684x": "F16",
+    "bm1684": "F32",
+    "cv183x": "BF16",
+    "cv182x": "BF16",
+    "cv181x": "BF16",
+    "cv180x": "BF16",
+    "bm1688": "F16",
+    "cv186x": "F16",
+    "bm1690": "F16",
+    "mars3":  "BF16"
+}
+
+chip_support_mix_fp_type = {
+    "bm1684x": ["F16", "F32"],
+    "bm1688": ["F16", "F32"],
+    "cv186x": ["F16", "F32"],
+    "bm1684": ["F32"],
+    "cv183x": ["BF16"],
+    "cv182x": ["BF16"],
+    "cv181x": ["BF16"],
+    "cv180x": ["BF16"],
+    "mars3":  ["BF16"]
+}
+
+def parse_method_list(input_str):
+    return input_str.split(',')
+
+def is_fuseop(op_name):
+    return re.match(r'^fused\[".*?"\]$', op_name)
+
+def split_fuseop(op_name):
+    if is_fuseop(op_name):
+        new_ops = re.findall(r'"([^"]+)"', op_name)
+        return new_ops[0]
+    else:
+        return op_name
