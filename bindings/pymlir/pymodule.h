10dfa66cf5b1f0fe5adf04652af759d5c5d4b3ae
hush-coder <422997952@qq.com>
2025-09-01 16:48:11 +0800

    feat: migrate Python bindings from pybind11 to nanobind

--- bindings/pymlir/pymodule.h
+++ bindings/pymlir/pymodule.h
@@ -15,11 +15,11 @@ public:
   ~py_module();
   void load(std::string filename);
 
-  py::dict getAllTensor();
+  nb::dict getAllTensor();
 
-  void before_invoke(py::function func);
+  void before_invoke(nb::object func);
 
-  void after_invoke(py::function func);
+  void after_invoke(nb::object func);
 
   void clear_hooks();
 
@@ -27,38 +27,38 @@ public:
 
   void set_tensor(
       std::string name,
-      py::array_t<float, py::array::c_style | py::array::forcecast> data,
+      nb::ndarray<> data,
       std::vector<int64_t> shape);
 
   void set_tensor_from_int(
       std::string name,
-      py::array_t<float, py::array::c_style | py::array::forcecast> data,
+      nb::ndarray<> data,
       std::vector<int64_t> shape);
 
   // Warning: using copy in python
-  py::array get_tensor(std::string name);
+  nb::ndarray<> get_tensor(std::string name);
 
   // Tip: not using copy in python, since independent mem
-  py::array get_fp32_tensor(std::string name);
+  nb::ndarray<> get_fp32_tensor(std::string name);
 
   struct quant_brief_info format_tensor_qinfo(std::string name);
 
   void invoke(bool fixed_to_float);
   void fake_quant_weight();
 
-  py::array invoke_at(const std::string name);
+  nb::ndarray<> invoke_at(const std::string name);
 
-  py::array backward_weight_at(
+  nb::ndarray<> backward_weight_at(
       const std::string name, const std::string weight_name,
-      py::array_t<float, py::array::c_style | py::array::forcecast> grd_dst);
+      nb::ndarray<> grd_dst);
 
   void invoke_from(const std::string name);
 
 public:
-  py::list all_tensor_names;
-  py::list all_weight_names;
-  py::list input_names;
-  py::list output_names;
+  nb::list all_tensor_names;
+  nb::list all_weight_names;
+  nb::list input_names;
+  nb::list output_names;
   static std::string gmem_mode_str_;
 
 private:
