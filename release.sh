8c15b0ce0895960764f2b27b464c9da57ff896f7
pengchao.hu <pengchao.hu@sophgo.com>
2026-07-07 10:09:48 +0800

    chore: remove regression from release
    - user get it from github
    
    Change-Id: I2beade11b7031f23ef0086cbc8702557a47ea945

--- release.sh
+++ release.sh
@@ -16,11 +16,6 @@ release_archive="./tpu-mlir_${mlir_version}"
 rm -rf ${release_archive}*
 cp -rf ${INSTALL_PATH} ${release_archive}
 
-mkdir -p ${release_archive}/regression/
-cp -rf ${PROJECT_ROOT}/regression/dataset ${release_archive}/regression/
-cp -rf ${PROJECT_ROOT}/regression/image ${release_archive}/regression/
-cp -rf ${PROJECT_ROOT}/regression/npz_input ${release_archive}/regression/
-cp -rf ${PROJECT_ROOT}/regression/model ${release_archive}/regression/
 cp -rf ${PROJECT_ROOT}/third_party/customlayer ${release_archive}
 cp -rf ${PROJECT_ROOT}/python/tools/soc_infer ${release_archive}/python/tools/
 cp ${PROJECT_ROOT}/requirements.txt ${release_archive}/
